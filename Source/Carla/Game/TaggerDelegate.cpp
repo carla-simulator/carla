0ba107abe0060b05dfc238a9684e83e3994e755e
nsubiron <nsubiron@cvc.uab.es>
2017-05-17 14:55:41 +0100

    Make tagger delegate less verbose

--- Source/Carla/Game/TaggerDelegate.cpp
+++ Source/Carla/Game/TaggerDelegate.cpp
@@ -18,12 +18,5 @@ void UTaggerDelegate::OnActorSpawned(AActor* InActor)
 {
   if (InActor != nullptr) {
     ATagger::TagActor(*InActor, bSemanticSegmentationEnabled);
-
-    TArray<ECityObjectLabel> Tags;
-    ATagger::GetTagsOfTaggedActor(*InActor, Tags);
-    UE_LOG(LogCarla, Warning, TEXT("Tagged recently spawned actor \"%s\""), *InActor->GetName());
-    for (auto Tag : Tags) {
-      UE_LOG(LogCarla, Warning, TEXT("  - %d"), (uint8)Tag);
-    }
   }
 }
