1f81f3539d2eb8a7e79d96180ac18df05eb9d2a1
felipecode <felipe.alcm@gmail.com>
2017-11-20 15:17:18 +0100

    load configuration

--- PythonClient/client_example.py
+++ PythonClient/client_example.py
@@ -69,13 +69,13 @@ def run_carla_client(host, port, autopilot_on, save_images_to_disk, image_filena
                 camera1.set_position(30, 0, 130)
                 settings.add_camera(camera1)
 
-            print('Requesting new episode...')
+                print('Loading the new settings ...')
 
-            # Now we request a new episode with these settings. The server
-            # replies with a scene description containing the available start
-            # spots for the player. Here instead of a CarlaSettings object we
-            # can also provide a CarlaSettings.ini file as string.
-            scene = client.request_new_episode(settings)
+                # Now we load these new settings. The server
+                # replies with a scene description containing the available start
+                # spots for the player. Here instead of a CarlaSettings object we
+                # can also provide a CarlaSettings.ini file as string.
+                scene = client.load_carla_settings(settings)
 
             # Choose one player start at random.
             number_of_player_starts = len(scene.player_start_spots)
