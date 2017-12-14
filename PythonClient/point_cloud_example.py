5121cfad0d9a6b7169fdb6849248bfb696eb7c1d
Marc Garcia Puig <mgarcia@cvc.uab.es>
2017-12-14 18:32:24 +0100

    Working client that saves the point cloud in world space.

--- PythonClient/point_cloud_example.py
+++ PythonClient/point_cloud_example.py
@@ -18,21 +18,23 @@ import os
 import random
 import sys
 import time
+from carla.carla_server_pb2 import Transform
 
 from carla.client import make_carla_client
 from carla.sensor import Camera
 from carla.settings import CarlaSettings
 from carla.tcp import TCPConnectionError
 from carla.util import print_over_same_line, StopWatch
-from carla.image_converter import depth_to_local_point_cloud
+from carla.image_converter import depth_to_local_point_cloud, depth_to_local_colored_point_cloud, to_rgb_array
 
 
 def run_carla_client(host, port, save_images_to_disk, image_filename_format):
     # Here we will run 3 episodes with 300 frames each.
-    number_of_frames = 300
+    number_of_frames = 3000
     output_folder = '_out'
-    camera_local_pos = [30,0,130]
-    camera_local_rotation = [0,0,0]
+    image_size = [800, 600]
+    camera_local_pos = [30, 0, 130]
+    camera_local_rotation = [0, 0, 0]
     fov = 70
 
     # We assume the CARLA server is already waiting for a client to connect at
@@ -42,7 +44,7 @@ def run_carla_client(host, port, save_images_to_disk, image_filename_format):
     # context manager makes sure the connection is always cleaned up on exit.
     with make_carla_client(host, port) as client:
         print('CarlaClient connected')
-
+        numpy.set_printoptions(precision=2)
         # Start a new episode.
         # Here we load the settings
         settings = CarlaSettings()
@@ -55,12 +57,13 @@ def run_carla_client(host, port, save_images_to_disk, image_filename_format):
         settings.randomize_seeds()
 
         camera1 = Camera('CameraDepth', PostProcessing='Depth', CameraFOV=fov)
-        camera1.set_image_size(800, 600)
+        camera1.set_image_size(*image_size)
         camera1.set_position(*camera_local_pos)
         camera1.set_rotation(*camera_local_rotation)
 
-        camera2 = Camera('CameraRGB', PostProcessing='SceneFinal', CameraFOV=fov)
-        camera2.set_image_size(800, 600)
+        camera2 = Camera(
+            'CameraRGB', PostProcessing='SceneFinal', CameraFOV=fov)
+        camera2.set_image_size(*image_size)
         camera2.set_position(*camera_local_pos)
         camera2.set_rotation(*camera_local_rotation)
 
@@ -70,9 +73,27 @@ def run_carla_client(host, port, save_images_to_disk, image_filename_format):
         scene = client.load_settings(settings)
 
         # Start at location index id '0'
+        # client.start_episode(0)
+        number_of_player_starts = len(scene.player_start_spots)
+        player_start = random.randint(0, max(0, number_of_player_starts - 1))
         client.start_episode(0)
 
-        car_to_camera
+        # Compute the camera transform matrix
+        camera_to_car = Transform()
+        camera_to_car.location.x = camera_local_pos[0]
+        camera_to_car.location.y = camera_local_pos[1]
+        camera_to_car.location.z = camera_local_pos[2]
+        camera_to_car.rotation.pitch = camera_local_rotation[0]
+        camera_to_car.rotation.yaw = camera_local_rotation[1]
+        camera_to_car.rotation.roll = camera_local_rotation[2]
+
+        camera_to_car_matrix = compute_transform_matrix(camera_to_car)
+
+        # Do the custom transformations
+        # Rotate -90 degrees in X (Roll)
+        rot_X = compute_transform_matrix_raw(roll=-90, yaw=90, sX=-1)
+
+        camera_to_car_matrix = camera_to_car_matrix * rot_X
 
         # Iterate every frame in the episode.
         for frame in range(0, number_of_frames):
@@ -81,69 +102,161 @@ def run_carla_client(host, port, save_images_to_disk, image_filename_format):
             # Read the data produced by the server this frame.
             measurements, sensor_data = client.read_data()
 
-            t = StopWatch()
-            transform = measurements.player_measurements.transform
-
-            yaw = numpy.radians(transform.rotation.yaw)
-            roll = numpy.radians(transform.rotation.roll)
-            pitch = numpy.radians(transform.rotation.pitch)
-
-            yaw_matrix = numpy.matrix([
-                [math.cos(yaw), -math.sin(yaw), 0],
-                [math.sin(yaw), math.cos(yaw), 0],
-                [0, 0, 1]
-            ])
-
-            pitch_matrix = numpy.matrix([
-                [math.cos(pitch), 0, -math.sin(pitch)],
-                [0, 1, 0],
-                [math.sin(pitch), 0, math.cos(pitch)]
-            ])
-
-            roll_matrix = numpy.matrix([
-                [1, 0, 0],
-                [0, math.cos(roll), math.sin(roll)],
-                [0, -math.sin(roll), math.cos(roll)]
-            ])
-
-            rotation_matrix = yaw_matrix * pitch_matrix * roll_matrix
-
-            translation = numpy.matrix([
-                [transform.location.x],
-                [transform.location.y],
-                [transform.location.z]
-            ])
-
-            camera_rel_position = numpy.matrix([[camera_local_pos[0]], [camera_local_pos[1]], [camera_local_pos[2]]])
-
-            camera_world_position = (rotation_matrix * camera_rel_position) + translation
-            t.stop()
-            print('Rotation transformation took {:.0f} ms'.format(t.milliseconds()))
-
             # Save the images to disk if requested.
             if save_images_to_disk:
                 for name, image in sensor_data.items():
                     image.save_to_disk(
                         image_filename_format.format(episode, name, frame))
 
+            # 2d to (camera) local 3d
             t = StopWatch()
-            points3D = depth_to_local_point_cloud(sensor_data['CameraDepth'])
+
+            # RGB image [[[r,g,b],..[r,g,b]],..[[r,g,b],..[r,g,b]]]
+            image_RGB = to_rgb_array(sensor_data['CameraRGB'])
+
+            # [[X0,Y0,Z0],..[Xn,Yn,Zn]]
+            # points_3d = depth_to_local_point_cloud(sensor_data['CameraDepth'])
+
+            # [[X0,Y0,Z0,r0,g0,b0],..[Xn,Yn,Zn,rn,gn,bn]]
+            points_3d = depth_to_local_colored_point_cloud(
+                sensor_data['CameraDepth'], image_RGB)
             t.stop()
             print('Transformation took {:.0f} ms'.format(t.milliseconds()))
 
+            # (camera) local 3d to world 3d
             t = StopWatch()
-            ply = '\n'.join([' '.join(map(str, i)) for i in points3D])
+
+            # Separating the 3d point and its corresponding color
+            color_list = points_3d[:, 3:6]
+            points_3d = points_3d[:, 0:3]
+
+            car_to_world = measurements.player_measurements.transform
+            car_to_world_matrix = compute_transform_matrix(
+                car_to_world) * camera_to_car_matrix
+
+            # We need the foramt [[X0,..Xn],[Z0,..Zn],[Z0,..Zn]]
+            points_3d = points_3d.transpose()
+
+            # Car to World transformation
+            points_3d = transform_points(
+                car_to_world_matrix, points_3d).transpose()
+
+            points_3d = numpy.concatenate((points_3d, color_list), axis=1)
+
+            t.stop()
+            print('Local to world transformation took {:.0f} ms'.format(
+                t.milliseconds()))
+
+            t = StopWatch()
+
+            # Save PLY to disk
+            print('Generaing PLY ...')
+            ply = '\n'.join(['{:.2f} {:.2f} {:.2f} {:.0f} {:.0f} {:.0f}'.format(
+                *p) for p in points_3d.tolist()])
+
+            # Create folder to save if does not exist
             if not os.path.isdir(output_folder):
                 os.makedirs(output_folder)
-            with open('{}/{:0>4}.ply'.format(output_folder, frame), 'w+') as f:
-                f.write('\n'.join([construct_ply_header(len(points3D)), ply]))
-            t.stop()
 
+            print('Saving PLY ...')
+            with open('{}/{:0>4}.ply'.format(output_folder, frame), 'w+') as f:
+                f.write(
+                    '\n'.join([construct_ply_header(len(points_3d), True), ply]))
+            t.stop()
             print('Saving to disk took {:.2f} s'.format(t.seconds()))
+
             print()
 
             client.send_control(
                 measurements.player_measurements.autopilot_control)
+
+
+def compute_transform_matrix(transform):
+    return compute_transform_matrix_raw(
+        transform.location.x,
+        transform.location.y,
+        transform.location.z,
+        transform.rotation.pitch,
+        transform.rotation.roll,
+        transform.rotation.yaw
+    )
+
+
+def compute_transform_matrix_raw(x=0, y=0, z=0, pitch=0, roll=0, yaw=0, sX=1, sY=1, sZ=1):
+    # Yaw
+    cy = math.cos(numpy.radians(yaw))
+    sy = math.sin(numpy.radians(yaw))
+    # Pitch
+    cr = math.cos(numpy.radians(roll))
+    sr = math.sin(numpy.radians(roll))
+    # Roll
+    cp = math.cos(numpy.radians(pitch))
+    sp = math.sin(numpy.radians(pitch))
+    # (4 X 4) Identity matrix
+    m = numpy.matrix(numpy.identity(4))
+    # Translation
+    m[0, 3] = x
+    m[1, 3] = y
+    m[2, 3] = z
+    # Rotation
+    m[0, 0] = sX * (cp * cy)
+    m[0, 1] = sY * (cy * sp*sr - sy * cr)
+    m[0, 2] = -sZ * (cy * sp * cr + sy * sr)
+    m[1, 0] = sX * (sy * cp)
+    m[1, 1] = sY * (sy * sp * sr + cy * cr)
+    m[1, 2] = sZ * (cy * sr - sy * sp * cr)
+    m[2, 0] = sX * (sp)
+    m[2, 1] = -sY * (cp * sr)
+    m[2, 2] = sZ * (cp * cr)
+
+    return m
+
+
+def transform_points(transform_matrix, points):
+    """
+    Expected points format [[X0..,Xn],[Y0..,Yn],[Z0..,Zn]]
+    """
+    # Add 0s row so [[X0..,Xn],[Y0..,Yn],[Z0..,Zn],[0,..0]]
+    points = numpy.append(points, numpy.ones((1, points.shape[1])), axis=0)
+    # Point transformation
+    points = transform_matrix * points
+    # Return all but last row
+    return points[0:3]
+
+
+def transform_points_old(transform, points):
+    yaw = numpy.radians(transform.rotation.yaw)
+    roll = numpy.radians(-transform.rotation.roll)
+    pitch = numpy.radians(-transform.rotation.pitch)
+
+    yaw_matrix = numpy.array([
+        [math.cos(yaw), -math.sin(yaw), 0],
+        [math.sin(yaw), math.cos(yaw), 0],
+        [0, 0, 1]
+    ])
+
+    pitch_matrix = numpy.array([
+        [math.cos(pitch), 0, -math.sin(pitch)],
+        [0, 1, 0],
+        [math.sin(pitch), 0, math.cos(pitch)]
+    ])
+
+    roll_matrix = numpy.array([
+        [1, 0, 0],
+        [0, math.cos(roll), math.sin(roll)],
+        [0, -math.sin(roll), math.cos(roll)]
+    ])
+
+    rotation_matrix = numpy.dot(
+        yaw_matrix, numpy.dot(pitch_matrix, roll_matrix))
+
+    translation = numpy.array([
+        [transform.location.x],
+        [transform.location.y],
+        [transform.location.z]
+    ])
+
+    return numpy.dot(rotation_matrix, points) + translation
 
 
 def construct_ply_header(points_number, colors=False):
@@ -160,12 +273,6 @@ def construct_ply_header(points_number, colors=False):
     if not colors:
         return '\n'.join(header[0:6] + [header[-1]]).format(points_number)
     return '\n'.join(header).format(points_number)
-
-'''
-property uchar diffuse_red
-property uchar diffuse_green
-property uchar diffuse_blue
-'''
 
 
 def main():
@@ -205,13 +312,11 @@ def main():
 
     while True:
         try:
-
             run_carla_client(
                 host=args.host,
                 port=args.port,
                 save_images_to_disk=args.images_to_disk,
                 image_filename_format='_images/episode_{:0>3d}/{:s}/image_{:0>5d}.png')
-
             print('Done.')
             return
 
@@ -228,4 +333,4 @@ if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
-        print('\nCancelled by user. Bye!')
+        print('\nClient stoped by user.')
