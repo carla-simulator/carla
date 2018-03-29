e734cb50f5f1d257f4f8b4e6f23786d08c1025df
felipecode <felipe.alcm@gmail.com>
2018-03-29 19:23:17 +0200

    Recording module is cool !

--- PythonClient/carla/benchmarks/recording.py
+++ PythonClient/carla/benchmarks/recording.py
@@ -9,10 +9,13 @@ class Recording(object):
                  , city_name
                  , name_to_save
                  , continue_experiment
-                 , save_images):
+                 , save_images
+                 , benchmark_details):
 
 
-        self._dict_stats = {'exp_id': -1,
+
+
+        self._dict_summary = {'exp_id': -1,
                             'rep': -1,
                             'weather': -1,
                             'start_point': -1,
@@ -41,20 +44,26 @@ class Recording(object):
                                    'brake': -1
                                    }
 
-        # Create the log files and get the names
-        self._suffix_name, self._full_name = self._create_log_record(name_to_save, self._experiments)
-        # Get the line for the experiment to be continued
-        self._line_on_file = self._continue_experiment(continue_experiment)
+        # Generate the full path for the log files
+        self._path = os.path.join('_benchmarks_results'
+                                  , name_to_save + '_' + benchmark_details
+                                  )
 
+        # Check for continuation of experiment, also returns the last line, used for test purposes
+        # If you don't want to continue it will create a new path name with a number
+        self._path, _ = self._continue_experiment(continue_experiment)
+
+        self._create_log_files()
 
         # A log with a date file: to show when was the last access and log what was tested,
-
         now = datetime.datetime.now()
-        self._internal_log_name = os.path.join(self._full_name, 'log_' + now.strftime("%Y%m%d%H%M"))
+        self._internal_log_name = os.path.join(self._path, 'log_' + now.strftime("%Y%m%d%H%M"))
+        open(self._internal_log_name, 'w').close()
 
+        # store the save images flag, and already store the format for image saving
         self._save_images = save_images
         self._image_filename_format = os.path.join(
-            self._full_name, '_images/episode_{:s}/{:s}/image_{:0>5d}.jpg')
+            self._path, '_images/episode_{:s}/{:s}/image_{:0>5d}.jpg')
 
 
 
@@ -79,7 +88,7 @@ class Recording(object):
             log.write('Start Experiment %d \n' % id_experiment)
 
     def log_end(self):
-        with open(self._recording._internal_log_name, 'a+') as log:
+        with open(self._internal_log_name, 'a+') as log:
             log.write('====== Finished Entire Benchmark ======')
 
 
@@ -90,28 +99,30 @@ class Recording(object):
     """
 
     def write_summary_results(self, experiment, pose, rep,
-                               path_distance, remaining_distance,
-                               final_time, time_out, result):
-        self._dict_stats['exp_id'] = experiment.id
-        self._dict_stats['rep'] = rep
-        self._dict_stats['weather'] = experiment.Conditions.WeatherId
-        self._dict_stats['start_point'] = pose[0]
-        self._dict_stats['end_point'] = pose[1]
-        self._dict_stats['result'] = result
-        self._dict_stats['initial_distance'] = path_distance
-        self._dict_stats['final_distance'] = remaining_distance
-        self._dict_stats['final_time'] = final_time
-        self._dict_stats['time_out'] = time_out
-
-        with open(os.path.join(self._full_name, self._suffix_name), 'a+') as ofd:
-            w = csv.DictWriter(ofd, self._dict_stats.keys())
-
-            w.writerow(self._dict_stats)
+                              path_distance, remaining_distance,
+                              final_time, time_out, result):
 
 
-    def write_details_results(self, experiment, rep, pose, reward_vec, control_vec):
-        with open(os.path.join(self._full_name,
-                               'details_' + self._suffix_name), 'a+') as rfd:
+        self._dict_summary['exp_id'] = experiment.id
+        self._dict_summary['rep'] = rep
+        self._dict_summary['weather'] = experiment.Conditions.WeatherId
+        self._dict_summary['start_point'] = pose[0]
+        self._dict_summary['end_point'] = pose[1]
+        self._dict_summary['result'] = result
+        self._dict_summary['initial_distance'] = path_distance
+        self._dict_summary['final_distance'] = remaining_distance
+        self._dict_summary['final_time'] = final_time
+        self._dict_summary['time_out'] = time_out
+
+        with open(os.path.join(self._path, 'summary.csv'), 'a+') as ofd:
+            w = csv.DictWriter(ofd, self._dict_summary.keys())
+
+            w.writerow(self._dict_summary)
+
+
+    def write_measurements_results(self, experiment, rep, pose, reward_vec, control_vec):
+
+        with open(os.path.join(self._path, 'measurements.csv'), 'a+') as rfd:
             rw = csv.DictWriter(rfd, self._dict_measurements.keys())
 
             for i in range(len(reward_vec)):
@@ -121,19 +132,19 @@ class Recording(object):
                 self._dict_measurements['end_point'] = pose[1]
                 self._dict_measurements['weather'] = experiment.Conditions.WeatherId
                 self._dict_measurements['collision_general'] = reward_vec[
-                    i].collision_other
+                    i].player_measurements.collision_other
                 self._dict_measurements['collision_pedestrians'] = reward_vec[
-                    i].collision_pedestrians
+                    i].player_measurements.collision_pedestrians
                 self._dict_measurements['collision_vehicles'] = reward_vec[
-                    i].collision_vehicles
+                    i].player_measurements.collision_vehicles
                 self._dict_measurements['intersection_otherlane'] = reward_vec[
-                    i].intersection_otherlane
+                    i].player_measurements.intersection_otherlane
                 self._dict_measurements['intersection_offroad'] = reward_vec[
-                    i].intersection_offroad
+                    i].player_measurements.intersection_offroad
                 self._dict_measurements['pos_x'] = reward_vec[
-                    i].transform.location.x
+                    i].player_measurements.transform.location.x
                 self._dict_measurements['pos_y'] = reward_vec[
-                    i].transform.location.y
+                    i].player_measurements.transform.location.y
                 self._dict_measurements['steer'] = control_vec[
                     i].steer
                 self._dict_measurements['throttle'] = control_vec[
@@ -144,49 +155,79 @@ class Recording(object):
                 rw.writerow(self._dict_measurements)
 
 
-    def _create_log_record(self, base_name, experiments):
-        """
-        This function creates the log files for the benchmark.
+    """
+        Internal methods for summary files creation and recovering
+    """
 
+    def _create_log_files(self):
         """
-        suffix_name = self._get_experiments_names(experiments)
-        full_name = os.path.join('_benchmarks_results',
-                                 base_name + '_'
-                                 + self._get_details() + '/')
+            Just create the log files and add the necessary header for it.
+        """
 
-        return suffix_name, full_name
+        if not self._experiment_exist():
+
+            os.mkdir(self._path)
+
+            with open(os.path.join(self._path, 'summary.csv'), 'w') as ofd:
+                w = csv.DictWriter(ofd, self._dict_summary.keys())
+                w.writeheader()
+
+            with open(os.path.join(self._path, 'measurements.csv'), 'w') as rfd:
+                rw = csv.DictWriter(rfd, self._dict_measurements.keys())
+                rw.writeheader()
 
 
     def _continue_experiment(self, continue_experiment):
+        """
+            Get the line on the file for the experiment.
+            If continue_experiment is false and experiment exist, generates a new file path
+
+        """
+        def get_non_existent_path(f_name_path):
+            """
+            Get the path to a filename which does not exist by incrementing path.
+            """
+            if not os.path.exists(f_name_path):
+                return f_name_path
+            filename, file_extension = os.path.splitext(f_name_path)
+            i = 1
+            new_f_name = "{}-{}{}".format(filename, i, file_extension)
+            while os.path.exists(new_f_name):
+                i += 1
+                new_f_name = "{}-{}{}".format(filename, i, file_extension)
+            return new_f_name
+
+        # start the new path as the same one as before
+        new_path = self._path
+
+        # if the experiment exist
         if self._experiment_exist():
 
+            # If you want to continue just get the last position
             if continue_experiment:
                 line_on_file = self._get_last_position()
 
             else:
-                # Ask question, to avoid mistaken override situations
-                answer = input_data("The experiment was already found in the files"
-                                    + ", Do you want to continue (y/n)? \n"
-                                    )
-                if answer == 'Yes' or answer == 'y':
-                    line_on_file = self._get_last_position()
-                else:
-                    line_on_file = 0
+                # Get a new non_conflicting path name
+                new_path = get_non_existent_path(new_path)
+                line_on_file = 1
 
         else:
-            line_on_file = 0
-
-        return line_on_file
+            line_on_file = 1
+        return new_path, line_on_file
 
 
     def save_images(self, sensor_data, episode_name, frame):
+        """
+            Save a image during the experiment
+        """
         if self._save_images:
             for name, image in sensor_data.items():
                 image.save_to_disk(self._image_filename_format.format(
                     episode_name, name, frame))
 
 
-    def get_pose_and_experiment(self,number_poses_task):
+    def get_pose_and_experiment(self, number_poses_task):
         """
             Based on the line in log file, return the current pose and experiment.
             If the line is zero, create new log files.
@@ -196,57 +237,31 @@ class Recording(object):
         """
             Warning: assumes that all tasks have the same size
         """
+        line_on_file = self._get_last_position()
 
-
-        if self._line_on_file == 0:
-            # The fixed name considering all the experiments being run
-            with open(os.path.join(self._full_name,
-                                   self._suffix_name), 'w') as ofd:
-
-                w = csv.DictWriter(ofd, self._dict_stats.keys())
-                w.writeheader()
-
-            with open(os.path.join(self._full_name,
-                                   'details_' + self._suffix_name), 'w') as rfd:
-
-                rw = csv.DictWriter(rfd, self._dict_measurements.keys())
-                rw.writeheader()
+        if line_on_file == 1:
             return 0, 0
         else:
-
-            return self._line_on_file / number_poses_task, self._line_on_file % number_poses_task
+            return line_on_file % number_poses_task -1, line_on_file / number_poses_task
 
 
 
     def _experiment_exist(self):
-        return os.path.exists(self._full_name)
+
+        return os.path.exists(self._path)
 
 
     def _get_last_position(self):
         """
-            Get the last position on the experiment file
+            Get the last position on the summary experiment file
             With this you are able to continue from there
-        :return int, position:
-        """
 
+        Returns:
+             int, position:
+        """
         # Try to open, if the file is not found
         try:
-            with open(os.path.join(self._full_name, self._suffix_name)) as f:
+            with open(os.path.join(self._path, 'summary.csv')) as f:
                 return sum(1 for _ in f)
         except IOError:
             return 0
-
-
-
-
-    @staticmethod
-    def _get_experiments_names(experiments):
-
-        name_cat = 'w'
-
-        for experiment in experiments:
-
-            name_cat += str(experiment.Conditions.WeatherId) + '.'
-
-        return name_cat
-
