080b7a1d5a28e2346a8ff35fbe5feb166d88b5d0
Marc Garcia Puig <marcgpuig@gmail.com>
2018-05-22 16:50:32 +0200

    Now the scripts are parametrized and check for errors, + some bug fixes

--- Setup.bat
+++ Setup.bat
@@ -9,6 +9,33 @@ set INSTALLATION_DIR=%LOCAL_PATH%Util\Build\
 set VERSION_FILE=%LOCAL_PATH%Util\ContentVersions.txt
 set CONTENT_DIR=%LOCAL_PATH%Unreal\CarlaUE4\Content
 
+:arg-parse
+if not "%1"=="" (
+    if "%1"=="-j" (
+        set NUMBER_OF_ASYNC_JOBS=%2
+        shift
+    )
+    if "%1"=="--boost-toolset" (
+        set TOOLSET=%2
+        shift
+    )
+    if "%1"=="-h" (
+    	goto help
+    )
+    if "%1"=="--help" (
+    	goto help
+    )
+    shift
+    goto :arg-parse
+)
+
+if [%TOOLSET%] == [] set TOOLSET=msvc-14.1
+if [%NUMBER_OF_ASYNC_JOBS%] == [] set NUMBER_OF_ASYNC_JOBS=1
+
+echo Asynchronous jobs:  %NUMBER_OF_ASYNC_JOBS%
+echo Boost toolset:      %TOOLSET%
+echo Install directory:  %INSTALLATION_DIR%
+
 if not exist "%CONTENT_DIR%" (
 	echo Creating %CONTENT_DIR% folder...
 	mkdir %CONTENT_DIR%
@@ -16,7 +43,9 @@ if not exist "%CONTENT_DIR%" (
 
 echo.
 echo  Installing Protobuf...
-call %INSTALLERS_DIR%install_proto.bat %INSTALLATION_DIR%
+call %INSTALLERS_DIR%install_proto.bat ^
+	--build-dir %INSTALLATION_DIR% ^
+	-j %NUMBER_OF_ASYNC_JOBS%
 
 if not defined install_proto (
 	echo.
@@ -26,7 +55,10 @@ if not defined install_proto (
 
 echo.
 echo  Installing Boost...
-call %INSTALLERS_DIR%install_boost.bat %INSTALLATION_DIR%
+call %INSTALLERS_DIR%install_boost.bat ^
+	--build-dir %INSTALLATION_DIR% ^
+	--toolset %TOOLSET% ^
+	-j %NUMBER_OF_ASYNC_JOBS%
 
 if not defined install_boost (
 	echo.
@@ -43,6 +75,8 @@ FOR /F "tokens=1 delims=:" %%i in (%VERSION_FILE%) do (
 	set ASSETS_VERSION=%%i
 )
 
+goto success
+
 :success
 	echo.
 	echo.
@@ -51,9 +85,11 @@ FOR /F "tokens=1 delims=:" %%i in (%VERSION_FILE%) do (
 	echo  #############################
 	echo.
 	echo  All the CARLA library dependences should be installed now.
+	echo  (You can remove all "*-src" folders in %INSTALLATION_DIR% directory)
+	echo.
 	echo  You only need the ASSET PACK with all the meshes and textures.
 	echo.
-	echo  This script provides the optimal assets version %ASSETS_VERSION%
+	echo  This script provides the assets for CARLA %ASSETS_VERSION%
 	echo  If you want another asset version, search it in %VERSION_FILE%.
 	echo  You can download the assets from here:
 	echo.
@@ -61,6 +97,21 @@ FOR /F "tokens=1 delims=:" %%i in (%VERSION_FILE%) do (
 	echo.
 	echo  Unzip it in the "%CONTENT_DIR%" folder.
 	echo  After that, please run the "Rebuild.bat".
+
+	goto eof
+
+:help
+	echo  --------
+	echo    HELP
+	echo  --------
+	echo.
+	echo  Commands:
+	echo    -h, --help          -^> Shows this dialog.
+	echo    -j ^<N^>              -^> N is the integer number of async jobs while compiling (default=1).
+	echo    --boost-toolset [T] -^> Toolset corresponding to your compiler ^(default=^*^):
+	echo                               Visual Studio 2013 -^> msvc-12.0
+	echo                               Visual Studio 2015 -^> msvc-14.0
+	echo                               Visual Studio 2017 -^> msvc-14.1 *
 
 	goto eof
 
--- Util/InstallersWin/install_boost.bat
+++ Util/InstallersWin/install_boost.bat
@@ -6,8 +6,29 @@ rem Just put it in `Util/Build` and run it.
 
 setlocal
 
+:arg-parse
+if not "%1"=="" (
+    if "%1"=="-j" (
+        set NUMBER_OF_ASYNC_JOBS=%2
+        shift
+    )
+    if "%1"=="--build-dir" (
+        set BUILD_DIR=%2
+        shift
+    )
+    if "%1"=="--toolset" (
+        set B_TOOLSET=%2
+        shift
+    )
+    shift
+    goto :arg-parse
+)
+
+if [%BUILD_DIR%] == [] set BUILD_DIR=.
+if [%B_TOOLSET%] == [] set B_TOOLSET=msvc-14.1
+if [%NUMBER_OF_ASYNC_JOBS%] == [] set NUMBER_OF_ASYNC_JOBS=1
+
 set LOCAL_PATH=%~dp0
-set BUILD_DIR=%1
 set FILE_N=---%~n0%~x0:
 
 set B_VERSION=boost-1.64.0
@@ -15,9 +36,11 @@ set B_SRC=boost-src
 set B_SRC_DIR=%BUILD_DIR%%B_SRC%
 set B_INSTALL=boost-install
 set B_INSTALL_DIR=%BUILD_DIR%%B_INSTALL%
+set B_LIB_DIR=%B_INSTALL_DIR%\lib
 
-set B_TOOLSET=msvc-14.1
-set B_LIB_DIR=%B_INSTALL_DIR%/lib
+if exist "%B_INSTALL_DIR%" (
+	goto already_build
+)
 
 if not exist "%B_SRC_DIR%" (
 	echo %FILE_N% Cloning Boost - version "%B_VERSION%"...
@@ -30,17 +53,21 @@ if not exist "%B_SRC_DIR%" (
 	echo %FILE_N% Not cloning boost because already exists a folder called "%B_SRC%".
 )
 
-if exist "%B_INSTALL_DIR%" (
-	goto already_build
+cd %B_SRC_DIR%
+if not exist "b2.exe" (
+	echo %FILE_N% Generating build...
+	call bootstrap.bat
 )
 
-cd %B_SRC_DIR%
-echo %FILE_N% Generating build...
-call bootstrap.bat
+if errorlevel 1 goto error_bootstrap
+
+echo %FILE_N% Packing headers...
+b2 headers
 
 echo %FILE_N% Building...
 b2 -j8 ^
 	headers ^
+	--layout=versioned ^
 	--with-system ^
 	--with-date_time ^
 	--build-dir=./build ^
@@ -50,30 +77,47 @@ b2 -j8 ^
 	variant=release ^
 	link=static ^
 	threading=multi ^
-	install ^
 	--prefix=%B_INSTALL_DIR% ^
 	--libdir=%B_LIB_DIR% ^
-	--includedir=%B_INSTALL_DIR%
-cd %BUILD_DIR%
+	--includedir=%B_INSTALL_DIR% ^
+	install
 
-move "%B_INSTALL_DIR%/boost-1_64/boost" "%B_INSTALL_DIR%/"
-rd /s /q "%B_INSTALL_DIR%/boost-1_64"
+if errorlevel 1 goto error_install
+
+cd %BUILD_DIR%
 
 rem Remove the downloaded protobuf source because is no more needed
 rem if you want to keep the source just delete the following command.
 rem @rd /s /q %B_SRC_DIR% 2>nul
 
+goto success
+
 :success
 	echo.
 	echo %FILE_N% Boost has been successfully installed in %B_INSTALL_DIR%!
-	goto eof
+	goto good_exit
 
 :already_build
-	echo %FILE_N% Library has already been built.
+	echo %FILE_N% A Boost installation already exists.
 	echo %FILE_N% Delete "%B_INSTALL_DIR%" if you want to force a rebuild.
-	goto eof
+	goto good_exit
 
-:eof
+:error_bootstrap
+	echo %FILE_N% [ERROR] An error ocurred while executing the "bootstrap.bat".
+	goto bad_exit
+
+:error_install
+	echo %FILE_N% [ERROR] An error ocurred while installing.
+	goto bad_exit
+
+:good_exit
 	echo %FILE_N% Exiting...
 	endlocal
 	set install_boost=done
+	goto:EOF
+
+:bad_exit
+	if exist "%B_INSTALL_DIR%" rd /s /q "B_INSTALL_DIR"
+	echo %FILE_N% Exiting with error...
+	endlocal
+	goto:EOF
