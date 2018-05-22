ad3c8c953ea940daf05dc133b391f791f053c02a
Marc Garcia Puig <marcgpuig@gmail.com>
2018-05-22 16:50:32 +0200

    Now the scripts are parametrized and check for errors, + some bug fixes

--- Rebuild.bat
+++ Rebuild.bat
@@ -3,16 +3,17 @@
 setlocal
 
 set LOCAL_PATH=%~dp0
+set FILE_N=---%~n0%~x0:
 
 set LIB_FOLDER=%LOCAL_PATH%Util\Build\
 set CARLA_FOLDER=%LOCAL_PATH%Unreal\CarlaUE4\
 
 if not exist "%LIB_FOLDER%" (
-	echo Creating %LIB_FOLDER% folder...
+	echo %FILE_N% Creating %LIB_FOLDER% folder...
 	mkdir %LIB_FOLDER%
 )
 
-echo Deleting intermediate folders...
+echo %FILE_N% Deleting intermediate folders...
 for %%G in (
 	"%CARLA_FOLDER%Binaries",
 	"%CARLA_FOLDER%Intermediate",
@@ -20,17 +21,29 @@ for %%G in (
 	"%CARLA_FOLDER%Plugins\Carla\Intermediate"
 ) do (
 	if exist %%G (
-		echo Deleting: %%G
+		echo %FILE_N% Deleting: %%G
 		rmdir /s/q %%G
 	)
 )
 
 echo.
-echo Building carlaserver...
+echo %FILE_N% Building carlaserver...
 make clean & make
 
+if errorlevel 1 goto compiling_error
+
 echo.
-echo Launch editor...
+echo %FILE_N% Launch editor...
 start %CARLA_FOLDER%CarlaUE4.uproject
 
-endlocal
+:success
+	goto eof
+
+:compiling_error
+	echo.
+	echo.
+	echo %FILE_N% Error while compiling carlaserver
+	goto eof
+
+:eof
+	endlocal
