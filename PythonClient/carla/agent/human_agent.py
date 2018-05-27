e555dd2cfea2fc5ad607d52313f05d9247e116c9
Felipe Codevilla <felipe.alcm@gmail.com>
2018-05-28 07:26:11 +1000

    first version for the new architecture

---
+++ PythonClient/carla/agent/human_agent.py
@@ -0,0 +1,45 @@
+
+
+from carla.agent.agent import Agent
+from carla.client import VehicleControl
+
+
+class HumanAgent(Agent):
+
+    """
+    Derivation of Agent Class for human control,
+
+    """
+
+    def _get_keyboard_control(self, keys):
+        """
+        Return a VehicleControl message based on the pressed keys. Return None
+        if a new episode was requested.
+        """
+        if keys[K_r]:
+            return None
+        control = VehicleControl()
+        if keys[K_LEFT] or keys[K_a]:
+            control.steer = -1.0
+        if keys[K_RIGHT] or keys[K_d]:
+            control.steer = 1.0
+        if keys[K_UP] or keys[K_w]:
+            control.throttle = 1.0
+        if keys[K_DOWN] or keys[K_s]:
+            control.brake = 1.0
+        if keys[K_SPACE]:
+            control.hand_brake = True
+        if keys[K_q]:
+            self._is_on_reverse = not self._is_on_reverse
+        if keys[K_p]:
+            self._enable_autopilot = not self._enable_autopilot
+        control.reverse = self._is_on_reverse
+        return control
+
+    def run_step(self, measurements, sensor_data, directions, target):
+        control = VehicleControl()
+        control.throttle = 0.9
+
+        return control
+
+
