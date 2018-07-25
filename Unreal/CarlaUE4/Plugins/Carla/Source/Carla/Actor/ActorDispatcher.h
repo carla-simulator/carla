6a1e4c3bb6478752009df2eb636c1718330c5961
nsubiron <nsubiron@cvc.uab.es>
2018-07-25 11:47:57 +0200

    Improvements to actor spawners

--- Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Actor/ActorDispatcher.h
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Actor/ActorDispatcher.h
@@ -14,7 +14,7 @@
 #include "Containers/Array.h"
 #include "Templates/Function.h"
 
-class IActorSpawner;
+class AActorSpawner;
 
 /// Actor in charge of binding ActorDefinitions to spawn functions, as well as
 /// keeping the registry of all the actors spawned.
@@ -33,7 +33,7 @@ public:
   /// Bind all the definitions of @a ActorSpawner to its spawn function.
   ///
   /// @warning Invalid definitions are ignored.
-  void Bind(IActorSpawner &ActorSpawner);
+  void Bind(AActorSpawner &ActorSpawner);
 
   /// Spawns an actor based on @a ActorDescription at @a Transform. To properly
   /// despawn an actor created with this function call DestroyActor.
@@ -63,6 +63,8 @@ private:
   TArray<FActorDefinition> Definitions;
 
   TArray<SpawnFunctionType> SpawnFunctions;
+
+  TArray<TSubclassOf<AActor>> Classes;
 
   FActorRegistry Registry;
 };
