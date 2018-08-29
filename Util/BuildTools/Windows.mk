2844d039524e5244f15b659d3db727a36825f663
iFuSiiOnzZ <andreisystem@gmail.com>
2018-08-29 14:13:20 +0200

    Updated documentation and small fixies in BuildCarlaUE4

--- Util/BuildTools/Windows.mk
+++ Util/BuildTools/Windows.mk
@@ -12,29 +12,29 @@ help:
 	@type "${CARLA_BUILD_TOOLS_FOLDER}\Linux.mk.help"
 
 launch: LibCarla
-	@${CARLA_BUILD_TOOLS_FOLDER}/BuildCarlaUE4.bat --build --ue-version 4.19
+	@"${CARLA_BUILD_TOOLS_FOLDER}/BuildCarlaUE4.bat" --build --ue-version 4.19
 
-launch-only:
-	@${CARLA_BUILD_TOOLS_FOLDER}/BuildCarlaUE4.bat --launch --ue-version 4.19
+launch-editor: LibCarla
+	@"${CARLA_BUILD_TOOLS_FOLDER}/BuildCarlaUE4.bat" --launch-editor
 
 package: PythonAPI
-	@${CARLA_BUILD_TOOLS_FOLDER}/Package.bat --ue-version 4.19
+	@"${CARLA_BUILD_TOOLS_FOLDER}/Package.bat" --ue-version 4.19
 
 docs:
 	@doxygen
 	@echo "Documentation index at ./Doxygen/html/index.html"
 
 clean:
-	@${CARLA_BUILD_TOOLS_FOLDER}/Package.bat --clean --ue-version 4.19
-	@${CARLA_BUILD_TOOLS_FOLDER}/BuildCarlaUE4.bat --clean
+	@"${CARLA_BUILD_TOOLS_FOLDER}/Package.bat" --clean --ue-version 4.19
+	@"${CARLA_BUILD_TOOLS_FOLDER}/BuildCarlaUE4.bat" --clean
 
-	@${CARLA_BUILD_TOOLS_FOLDER}/BuildPythonAPI.bat --clean
-	@${CARLA_BUILD_TOOLS_FOLDER}/BuildLibCarla.bat --clean
+	@"${CARLA_BUILD_TOOLS_FOLDER}/BuildPythonAPI.bat" --clean
+	@"${CARLA_BUILD_TOOLS_FOLDER}/BuildLibCarla.bat" --clean
 
 rebuild: setup
-	@${CARLA_BUILD_TOOLS_FOLDER}/BuildLibCarla.bat --rebuild
-	@${CARLA_BUILD_TOOLS_FOLDER}/BuildPythonAPI.bat --rebuild
-	@${CARLA_BUILD_TOOLS_FOLDER}/BuildCarlaUE4.bat --rebuild --ue-version 4.19
+	@${CARLA_BUILD_TOOLS_FOLDER}/BuildLibCarla.bat" --rebuild
+	@${CARLA_BUILD_TOOLS_FOLDER}/BuildPythonAPI.bat" --rebuild
+	@${CARLA_BUILD_TOOLS_FOLDER}/BuildCarlaUE4.bat" --rebuild --ue-version 4.19
 
 check: PythonAPI
 	@echo "Not implemented!"
@@ -43,18 +43,15 @@ benchmark: LibCarla
 	@echo "Not implemented!"
 
 CarlaUE4Editor: LibCarla
-	@${CARLA_BUILD_TOOLS_FOLDER}/BuildCarlaUE4.bat --build-editor --ue-version 4.19
+	@"${CARLA_BUILD_TOOLS_FOLDER}/BuildCarlaUE4.bat" --build-editor --ue-version 4.19
 
 .PHONY: PythonAPI
 PythonAPI: LibCarla
-	@${CARLA_BUILD_TOOLS_FOLDER}/BuildPythonAPI.bat --py3
+	@"${CARLA_BUILD_TOOLS_FOLDER}/BuildPythonAPI.bat" --py3
 
 .PHONY: LibCarla
 LibCarla: setup
-	@${CARLA_BUILD_TOOLS_FOLDER}\BuildLibCarla.bat --server --client
+	@"${CARLA_BUILD_TOOLS_FOLDER}/BuildLibCarla.bat" --server --client
 
 setup:
-	@${CARLA_BUILD_TOOLS_FOLDER}\Setup.bat --boost-toolset msvc-14.1
-
-pretty:
-	@echo "Not implemented!"
+	@"${CARLA_BUILD_TOOLS_FOLDER}/Setup.bat" --boost-toolset msvc-14.1
