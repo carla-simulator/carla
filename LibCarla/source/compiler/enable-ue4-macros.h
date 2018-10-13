5b611b84f39f75ef49ffb20e272f65a35f03c54b
iFuSiiOnzZ <andreisystem@gmail.com>
2018-10-13 11:29:48 +0200

    Enable UE4 macros only if have been disabled

--- LibCarla/source/compiler/enable-ue4-macros.h
+++ LibCarla/source/compiler/enable-ue4-macros.h
@@ -4,15 +4,19 @@
 // This work is licensed under the terms of the MIT license.
 // For a copy, see <https://opensource.org/licenses/MIT>.
 
-#pragma pop_macro("check")
-#pragma pop_macro("TEXT")
+#ifdef LIBCARLA_INCLUDED_DISABLE_UE4_MACROS_HEADER
+  #pragma pop_macro("check")
+  #pragma pop_macro("TEXT")
 
-#if defined(_MSC_VER)
-#  pragma warning(pop)
-#endif
+  #if defined(_MSC_VER)
+  #  pragma warning(pop)
+  #endif
 
-#if defined(__clang__)
-#  pragma clang diagnostic pop
+  #if defined(__clang__)
+  #  pragma clang diagnostic pop
+  #endif
+
+  #undef LIBCARLA_INCLUDED_DISABLE_UE4_MACROS_HEADER
 #endif
 
 #undef LIBCARLA_INCLUDED_FROM_UE4
