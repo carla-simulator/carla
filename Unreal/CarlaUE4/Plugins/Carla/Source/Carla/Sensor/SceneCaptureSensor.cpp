604111ec4ffc0e3573ec59f2457e913aa5d7f957
iFuSiiOnzZ <andreisystem@gmail.com>
2018-10-13 14:12:20 +0200

    Remove intrinsics functions errors by reodering the includes

--- Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Sensor/SceneCaptureSensor.cpp
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Sensor/SceneCaptureSensor.cpp
@@ -4,14 +4,14 @@
 // This work is licensed under the terms of the MIT license.
 // For a copy, see <https://opensource.org/licenses/MIT>.
 
-#include "Carla.h"
-#include "Carla/Sensor/SceneCaptureSensor.h"
-
 #include "Components/DrawFrustumComponent.h"
 #include "Components/SceneCaptureComponent2D.h"
 #include "Components/StaticMeshComponent.h"
 #include "Engine/TextureRenderTarget2D.h"
 #include "HighResScreenshot.h"
+
+#include "Carla.h"
+#include "Carla/Sensor/SceneCaptureSensor.h"
 
 static auto SCENE_CAPTURE_COUNTER = 0u;
 
