980ddbb70ffc48485137101517522aad17011d32
nsubiron <nsubiron@cvc.uab.es>
2018-10-15 16:00:54 +0200

    Better distro check in setup.py

--- PythonAPI/setup.py
+++ PythonAPI/setup.py
@@ -18,7 +18,7 @@ def get_libcarla_extensions():
     libraries = []
 
     if os.name == "posix":
-        if platform.dist()[0] == "Ubuntu":
+        if platform.dist()[0].lower() in ["ubuntu", "debian"]:
             pwd = os.path.dirname(os.path.realpath(__file__))
             pylib = "libboost_python%d%d.a" % (sys.version_info.major,
                                                sys.version_info.minor)
