21e2e1bd24703284a34d26cbc00f0bc45ee01150
nsubiron <nsubiron@cvc.uab.es>
2018-10-17 22:44:22 +0200

    Define common actor_id_type

---
+++ LibCarla/source/carla/rpc/ActorId.h
@@ -0,0 +1,20 @@
+// Copyright (c) 2017 Computer Vision Center (CVC) at the Universitat Autonoma
+// de Barcelona (UAB).
+//
+// This work is licensed under the terms of the MIT license.
+// For a copy, see <https://opensource.org/licenses/MIT>.
+
+#pragma once
+
+#include <cstdint>
+
+namespace carla {
+namespace rpc {
+
+  using actor_id_type = uint32_t;
+
+} // namespace rpc
+
+  using actor_id_type = rpc::actor_id_type;
+
+} // namespace carla
