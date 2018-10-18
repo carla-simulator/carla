3c1f23a3fa4b4626f734a22d04cc46e87d5e129f
nsubiron <nsubiron@cvc.uab.es>
2018-10-18 20:38:14 +0200

    Better logging when destroying actors

--- Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Actor/ActorDispatcher.cpp
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Actor/ActorDispatcher.cpp
@@ -70,10 +70,22 @@ TPair<EActorSpawnResultStatus, FActorView> FActorDispatcher::SpawnActor(
 
 bool FActorDispatcher::DestroyActor(AActor *Actor)
 {
-  if ((Actor != nullptr) && Actor->Destroy())
+  if (Actor == nullptr) {
+    UE_LOG(LogCarla, Error, TEXT("Trying to destroy nullptr actor"));
+    return false;
+  }
+  auto View = Registry.Find(Actor);
+  if (!View.IsValid()) {
+    UE_LOG(LogCarla, Error, TEXT("Trying to destroy actor that is not in the registry"));
+    return false;
+  }
+  const auto &Id = View.GetActorDescription()->Id;
+  UE_LOG(LogCarla, Log, TEXT("Destroying actor '%s'"), *Id);
+  if (Actor->Destroy())
   {
     Registry.Deregister(Actor);
     return true;
   }
+  UE_LOG(LogCarla, Log, TEXT("Failed to destroy actor '%s'!"), *Id);
   return false;
 }
