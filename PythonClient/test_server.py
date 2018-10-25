fe89c585d66eb5c099ac365ca63c9c7af7e12a90
GCH-NB-17-045\sham <muhammad.shaheer-munir@baselabs.de>
2018-10-25 14:01:23 +0200

    Adding new module osi_grpc_communication_module.
    Module uses the gRPC(google remote procedure call) to send the groundtruth data with OSI standardization(Open Simulation Interface).

--- PythonClient/test_server.py
+++ PythonClient/test_server.py
@@ -4,24 +4,24 @@ import time
 import grpc
 import sys
 import os
-sys.path.insert(0,os.path.abspath('proto_generated'))
-import osi_grpc_pb2_grpc
-import osi_grpc_pb2
+
+import osi_grpc.osi_grpc_pb2_grpc
 
 _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
 
-class ProcessGroundTruth(osi_grpc_pb2_grpc.GroundtruthdataServicer):
+class ProcessGroundTruth(osi_grpc.osi_grpc_pb2_grpc.GroundtruthdataServicer):
 
     def ProcessGroundTruth(self, request, context):
 
         print("client data received: " + str(request))
 
-        return osi_grpc_pb2.Empty(result='Client Data Sent')
+        return osi_grpc.osi_grpc_pb2.Empty(result='Client Data Sent')
+
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    osi_grpc_pb2_grpc.add_GroundtruthdataServicer_to_server(ProcessGroundTruth(), server)
+    osi_grpc.osi_grpc_pb2_grpc.add_GroundtruthdataServicer_to_server(ProcessGroundTruth(), server)
     server.add_insecure_port('[::]:63558')
     server.start()
     try:
