c9952328c7a8097d7d7c44f8739919748b24dc27
nsubiron <nsubiron@cvc.uab.es>
2018-10-29 15:00:46 +0100

    Improve spawn_npc.py

--- PythonAPI/spawn_npc.py
+++ PythonAPI/spawn_npc.py
@@ -43,7 +43,7 @@ def main():
         type=int,
         help='TCP port to listen to (default: 2000)')
     argparser.add_argument(
-        '--number-of-vehicles',
+        '-nv', '--number-of-vehicles',
         metavar='N',
         default=10,
         type=int,
@@ -63,28 +63,39 @@ def main():
         client = carla.Client(args.host, args.port)
         client.set_timeout(2.0)
         world = client.get_world()
-        spawn_points = world.get_map().get_spawn_points()
         blueprints = world.get_blueprint_library().filter('vehicle.*')
 
-        count = args.number_of_vehicles
-
-        while count > 0:
-
+        def try_spawn_random_vehicle_at(transform):
             blueprint = random.choice(blueprints)
             if blueprint.has_attribute('color'):
                 color = random.choice(blueprint.get_attribute('color').recommended_values)
                 blueprint.set_attribute('color', color)
-
-            transform = random.choice(spawn_points)
-
             vehicle = world.try_spawn_actor(blueprint, transform)
             if vehicle is not None:
                 actor_list.append(vehicle)
                 vehicle.set_autopilot()
-                count -= 1
                 print('spawned %r at %s' % (vehicle.type_id, transform.location))
+                return True
+            return False
 
+        # @todo Needs to be converted to list to be shuffled.
+        spawn_points = list(world.get_map().get_spawn_points())
+        random.shuffle(spawn_points)
+
+        print('found %d spawn points.' % len(spawn_points))
+
+        count = args.number_of_vehicles
+
+        for spawn_point in spawn_points:
+            if try_spawn_random_vehicle_at(spawn_point):
+                count -= 1
+            if count <= 0:
+                break
+
+        while count > 0:
             time.sleep(args.delay)
+            if try_spawn_random_vehicle_at(random.choice(spawn_points)):
+                count -= 1
 
         print('spawned %d vehicles, press Ctrl+C to exit.' % args.number_of_vehicles)
         signal.pause()
