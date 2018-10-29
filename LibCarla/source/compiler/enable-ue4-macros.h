5b2d4f9ed37c66da00692032e3d42d1853e8846f
nsubiron <nsubiron@cvc.uab.es>
2018-10-29 11:04:44 +0100

    Fix Windows compilation

--- LibCarla/source/compiler/enable-ue4-macros.h
+++ LibCarla/source/compiler/enable-ue4-macros.h
@@ -4,9 +4,11 @@
 // This work is licensed under the terms of the MIT license.
 // For a copy, see <https://opensource.org/licenses/MIT>.
 
-
 #if defined(_MSC_VER)
 #  pragma warning(pop)
+#  ifdef UpdateResource
+#    undef UpdateResource
+#  endif
 #endif
 
 #if defined(__clang__)
