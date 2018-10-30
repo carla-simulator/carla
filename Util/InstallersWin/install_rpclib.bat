6ec8696bbc8f59357665ddc9e10ac3c0d5499516
nsubiron <nsubiron@cvc.uab.es>
2018-10-30 15:07:53 +0100

    Upgrade rpclib to fix crash when client exits too fast

--- Util/InstallersWin/install_rpclib.bat
+++ Util/InstallersWin/install_rpclib.bat
@@ -33,7 +33,7 @@ if not "%1"=="" (
     goto :arg-parse
 )
 
-set RPC_VERSION=v2.2.1
+set RPC_VERSION=d1146b7
 set RPC_SRC=rpclib-src
 set RPC_SRC_DIR=%BUILD_DIR%%RPC_SRC%\
 set RPC_INSTALL=rpclib-install
@@ -47,8 +47,11 @@ if exist "%RPC_INSTALL_DIR%" (
 if not exist "%RPC_SRC_DIR%" (
     echo %FILE_N% Cloning rpclib - version "%RPC_VERSION%"...
 
-    call git clone --depth=1 -b %RPC_VERSION% https://github.com/rpclib/rpclib.git %RPC_SRC_DIR%
+    call git clone https://github.com/rpclib/rpclib.git %RPC_SRC_DIR%
     if %errorlevel% neq 0 goto error_git
+    pushd %RPC_SRC_DIR%
+    call git reset --hard d1146b7
+    popd
 ) else (
     echo %FILE_N% Not cloning rpclib because already exists a folder called "%RPC_SRC%".
 )
