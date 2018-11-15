3072fcb2be9171648db6b4c1026b605a29f04118
nsubiron <nsubiron@cvc.uab.es>
2018-11-15 13:01:27 +0100

    Add missing license notices

--- LibCarla/source/carla/opendrive/OpenDrive.cpp
+++ LibCarla/source/carla/opendrive/OpenDrive.cpp
@@ -1,3 +1,9 @@
+// Copyright (c) 2017 Computer Vision Center (CVC) at the Universitat Autonoma
+// de Barcelona (UAB).
+//
+// This work is licensed under the terms of the MIT license.
+// For a copy, see <https://opensource.org/licenses/MIT>.
+
 #include "OpenDrive.h"
 
 #include "../road/MapBuilder.h"
--- LibCarla/source/carla/opendrive/parser/OpenDriveParser.h
+++ LibCarla/source/carla/opendrive/parser/OpenDriveParser.h
@@ -1,3 +1,9 @@
+// Copyright (c) 2017 Computer Vision Center (CVC) at the Universitat Autonoma
+// de Barcelona (UAB).
+//
+// This work is licensed under the terms of the MIT license.
+// For a copy, see <https://opensource.org/licenses/MIT>.
+
 #pragma once
 
 #include "TrafficSignalsParser.h"
