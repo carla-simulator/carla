6123de0b8afe3e42bdb31c838396154467e08abe
nsubiron <nsubiron@cvc.uab.es>
2018-11-16 13:29:40 +0100

    Increase version and update changelog

--- PythonAPI/setup.py
+++ PythonAPI/setup.py
@@ -95,7 +95,7 @@ def get_libcarla_extensions():
 
 setup(
     name='carla',
-    version='0.9.0',
+    version='0.9.1',
     package_dir={'': 'source'},
     packages=['carla'],
     ext_modules=get_libcarla_extensions(),
