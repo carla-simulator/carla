9550c6c2369c6fd81f42d59fc9d54ebf9ce6c5d3
Marc Garcia Puig <marcgpuig@gmail.com>
2018-11-30 21:09:47 +0100

    Fixed an issue where the OpenDrive was generated twice

--- Unreal/CarlaUE4/Plugins/Carla/Source/Carla/OpenDriveActor.cpp
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/OpenDriveActor.cpp
@@ -39,8 +39,22 @@ void AOpenDriveActor::BeginDestroy()
 void AOpenDriveActor::OnConstruction(const FTransform &transform)
 {
   Super::OnConstruction(transform);
-  BuildRoutes();
+  
 }
+
+#if WITH_EDITOR
+void AOpenDriveActor::PostEditChangeProperty(struct FPropertyChangedEvent& e) {
+  Super::PostEditChangeProperty(e);
+  FName PropertyName = (e.Property != NULL) ? e.Property->GetFName() : NAME_None;
+
+  if (PropertyName == GET_MEMBER_NAME_CHECKED(AOpenDriveActor, bGenerateRoutes)) {
+    if (bGenerateRoutes) {
+      bGenerateRoutes = false;
+      BuildRoutes();
+    }
+  }
+}
+#endif
 
 void AOpenDriveActor::BuildRoutes()
 {
