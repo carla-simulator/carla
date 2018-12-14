bug-fix: acos in -1,1; queue reloaded with more elements; lateral PID with derivative term to decrease overshooting

PythonAPI/source/carla/navigation/controller.py
@@ -234,8 +234,8 @@ class PIDLateralController():
         w_vec = np.array([waypoint.transform.location.x -
                           v_begin.x, waypoint.transform.location.y -
                           v_begin.y, 0.0])
-        _dot = math.acos(np.dot(w_vec, v_vec) /
-                         (np.linalg.norm(w_vec) * np.linalg.norm(v_vec)))
+        _dot = math.acos(np.clip(np.dot(w_vec, v_vec) /
+                                 (np.linalg.norm(w_vec) * np.linalg.norm(v_vec)), -1.0, 1.0))
         _cross = np.cross(v_vec, w_vec)
         if _cross[2] < 0:
             _dot *= -1.0