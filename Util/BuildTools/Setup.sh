06c4a1c0d80a757715aec769ea7a22b37842623c
nsubiron <nsubiron@cvc.uab.es>
2019-01-09 19:50:27 +0100

    Upgrade Boost version to 1.69.0

--- Util/BuildTools/Setup.sh
+++ Util/BuildTools/Setup.sh
@@ -71,7 +71,7 @@ unset LLVM_BASENAME
 # -- Get boost includes --------------------------------------------------------
 # ==============================================================================
 
-BOOST_VERSION=1.68.0
+BOOST_VERSION=1.69.0
 BOOST_BASENAME="boost-${BOOST_VERSION}"
 
 BOOST_INCLUDE=${PWD}/${BOOST_BASENAME}-install/include
