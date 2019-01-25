4891c6de73176217a7003a607ad9561e6f47bca7
nsubiron <nsubiron@cvc.uab.es>
2019-01-26 00:48:11 +0100

    Iterate actor registry by values instead of pairs

--- Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Actor/ActorRegistry.h
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Actor/ActorRegistry.h
@@ -10,6 +10,10 @@
 
 #include "Containers/Map.h"
 
+#include <compiler/disable-ue4-macros.h>
+#include <carla/Iterator.h>
+#include <compiler/enable-ue4-macros.h>
+
 #include <unordered_map>
 
 /// A registry of all the Carla actors.
@@ -90,19 +94,16 @@ public:
   /// @{
 public:
 
-  using key_type = DatabaseType::key_type;
-  using mapped_type = DatabaseType::mapped_type;
-  using value_type = DatabaseType::value_type;
-  using const_iterator = DatabaseType::const_iterator;
+  using value_type = DatabaseType::mapped_type;
 
-  const_iterator begin() const noexcept
+  auto begin() const noexcept
   {
-    return ActorDatabase.begin();
+    return carla::iterator::make_map_values_iterator(ActorDatabase.begin());
   }
 
-  const_iterator end() const noexcept
+  auto end() const noexcept
   {
-    return ActorDatabase.end();
+    return carla::iterator::make_map_values_iterator(ActorDatabase.end());
   }
 
   /// @}
