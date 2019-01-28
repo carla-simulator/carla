d7d40a6414f3e641112bdee345c3938c26a9d098
Daniel Novillo Villarejo <dnovillocvc@gmail.com>
2019-01-28 11:15:01 +0100

    Basic sensor setup for ObstacleDetectionSensor

---
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Sensor/ObstacleDetectionSensor.h
@@ -0,0 +1,56 @@
+// Copyright (c) 2017 Computer Vision Center (CVC) at the Universitat Autonoma
+// de Barcelona (UAB).
+//
+// This work is licensed under the terms of the MIT license.
+// For a copy, see <https://opensource.org/licenses/MIT>.
+
+#pragma once
+
+#include "Carla/Sensor/Sensor.h"
+
+#include "ObstacleDetectionSensor.generated.h"
+
+class UCarlaEpisode;
+class UCarlaGameInstance;
+
+/// A sensor to register collisions.
+UCLASS()
+class CARLA_API AObstacleDetectionSensor : public ASensor
+{
+  GENERATED_BODY()
+
+  float Distance;
+
+  float HitRadius;
+
+public:
+
+  static FActorDefinition GetSensorDefinition();
+
+  AObstacleDetectionSensor(const FObjectInitializer& ObjectInitializer);
+
+  void SetOwner(AActor *NewOwner) override;
+
+  void Set(const FActorDescription &Description) override;
+
+
+  void BeginPlay() override;
+
+  void Tick(float DeltaSeconds) override;
+
+private:
+
+  UFUNCTION()
+  void OnObstacleDetectionEvent(
+      AActor *Actor,
+      AActor *OtherActor,
+      float Distance,
+      const FHitResult &Hit);
+
+  UPROPERTY()
+  const UCarlaEpisode *Episode = nullptr;
+
+  UPROPERTY()
+  const UCarlaGameInstance *GameInstance = nullptr;
+
+};
