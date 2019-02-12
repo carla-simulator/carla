0471116c7a88ac5387e026c5d969aa8b01f2cd0d
bernatx <bernatx@gmail.com>
2019-02-12 12:06:35 +0100

    More Pull Request fixes

--- PythonAPI/manual_control.py
+++ PythonAPI/manual_control.py
@@ -31,6 +31,11 @@ Use ARROWS or WASD keys for control.
 
     R            : toggle recording images to disk
 
+    CTRL + R     : toggle recording of simulation (replacing any previous)
+    CTRL + P     : start replaying last recorded simulation
+    CTRL + +     : increments the start time of the replay by 1 second (+SHIFT = 10 seconds)
+    CTRL + -     : decrements the start time of the replay by 1 second (+SHIFT = 10 seconds)
+
     F1           : toggle HUD
     H/?          : toggle help
     ESC          : quit
