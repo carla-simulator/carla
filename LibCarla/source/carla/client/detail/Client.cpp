8fb6197a7953277feaa4fb6d8a57d6622931a66f
nsubiron <nsubiron@cvc.uab.es>
2019-02-22 16:12:12 +0100

    Add a method for getting the list of available maps in Python API

--- LibCarla/source/carla/client/detail/Client.cpp
+++ LibCarla/source/carla/client/detail/Client.cpp
@@ -136,6 +136,10 @@ namespace detail {
     return _pimpl->CallAndWait<rpc::MapInfo>("get_map_info");
   }
 
+  std::vector<std::string> Client::GetAvailableMaps() {
+    return _pimpl->CallAndWait<std::vector<std::string>>("get_available_maps");
+  }
+
   std::vector<rpc::ActorDefinition> Client::GetActorDefinitions() {
     return _pimpl->CallAndWait<std::vector<rpc::ActorDefinition>>("get_actor_definitions");
   }
