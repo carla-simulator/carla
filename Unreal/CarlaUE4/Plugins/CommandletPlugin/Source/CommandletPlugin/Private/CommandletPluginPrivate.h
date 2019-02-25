bc9a713791849fc6e8ab9400cff097fc46fffcb5
Daniel <dnovillo@cvc.uab.es>
2019-02-25 18:24:54 +0100

    Map process now inside Carla Plugin

--- Unreal/CarlaUE4/Plugins/CommandletPlugin/Source/CommandletPlugin/Private/CommandletPluginPrivate.h
+++
@@ -1,9 +0,0 @@
-// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
-
-#pragma once
-
-#include "Logging/LogMacros.h"
-
-
-/** Declares a log category for this module. */
-DECLARE_LOG_CATEGORY_EXTERN(LogCommandletPlugin, Log, All);
