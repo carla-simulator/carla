7a9a9c16c7a73bdad22017fe2b3cd7a12f43e9d5
bernatx <bernatx@gmail.com>
2019-02-27 14:33:42 +0100

    Calling Recording functions without blocking Python

--- PythonAPI/source/libcarla/libcarla.cpp
+++ PythonAPI/source/libcarla/libcarla.cpp
@@ -18,6 +18,30 @@
       return self.fn(); \
     }
 
+// Convenient for requests with 1 argument.
+#define CALL_WITHOUT_GIL_1(cls, fn, T1_) +[](cls &self, T1_ t1) { \
+      carla::PythonUtil::ReleaseGIL unlock; \
+      return self.fn(std::forward<T1_>(t1)); \
+    }
+
+// Convenient for requests with 2 arguments.
+#define CALL_WITHOUT_GIL_2(cls, fn, T1_, T2_) +[](cls &self, T1_ t1, T2_ t2) { \
+      carla::PythonUtil::ReleaseGIL unlock; \
+      return self.fn(std::forward<T1_>(t1), std::forward<T2_>(t2)); \
+    }
+
+// Convenient for requests with 3 arguments.
+#define CALL_WITHOUT_GIL_3(cls, fn, T1_, T2_, T3_) +[](cls &self, T1_ t1, T2_ t2, T3_ t3) { \
+      carla::PythonUtil::ReleaseGIL unlock; \
+      return self.fn(std::forward<T1_>(t1), std::forward<T2_>(t2), std::forward<T3_>(t3)); \
+    }
+
+// Convenient for requests with 4 arguments.
+#define CALL_WITHOUT_GIL_4(cls, fn, T1_, T2_, T3_, T4_) +[](cls &self, T1_ t1, T2_ t2, T3_ t3, T4_ t4) { \
+      carla::PythonUtil::ReleaseGIL unlock; \
+      return self.fn(std::forward<T1_>(t1), std::forward<T2_>(t2), std::forward<T3_>(t3), std::forward<T4_>(t4)); \
+    }
+
 // Convenient for const requests without arguments.
 #define CONST_CALL_WITHOUT_GIL(cls, fn) CALL_WITHOUT_GIL(const cls, fn)
 
