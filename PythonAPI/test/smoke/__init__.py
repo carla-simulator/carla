929f8229f0cd9f00c879da764706130687f5de79
nsubiron <nsubiron@cvc.uab.es>
2019-03-08 14:41:23 +0100

    Add infrastructure for smoke tests in Jenkins

--- PythonAPI/test/smoke/__init__.py
+++ PythonAPI/test/smoke/__init__.py
@@ -9,9 +9,11 @@ import os
 import sys
 
 try:
-    sys.path.append(glob.glob('../dist/carla-*%d.%d-%s.egg' % (
+    sys.path.append(glob.glob('../../dist/carla-*%d.%d-%s.egg' % (
         sys.version_info.major,
         sys.version_info.minor,
         'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
 except IndexError:
     pass
+
+TESTING_ADDRESS=('localhost', 3654)
