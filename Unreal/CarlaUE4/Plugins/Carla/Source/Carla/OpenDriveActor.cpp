d14767da889d866bbe4a8aba5183d9b79e5bd7b2
Marc Garcia Puig <marcgpuig@gmail.com>
2019-03-13 12:34:26 +0100

    Fix unreal build while changing the WaypointAPI

--- Unreal/CarlaUE4/Plugins/Carla/Source/Carla/OpenDriveActor.cpp
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/OpenDriveActor.cpp
@@ -179,10 +179,10 @@ void AOpenDriveActor::BuildRoutes(FString MapName)
   using IdType = carla::road::element::id_type;
   using Waypoint = carla::road::element::Waypoint;
   using WaypointGen = carla::road::WaypointGenerator;
-  using TrafficGroup = carla::opendrive::types::TrafficLightGroup;
-  using TrafficLight = carla::opendrive::types::TrafficLight;
-  using TrafficBoxComponent = carla::opendrive::types::BoxComponent;
-  using TrafficSign = carla::opendrive::types::TrafficSign;
+  // using TrafficGroup = carla::opendrive::types::TrafficLightGroup;
+  // using TrafficLight = carla::opendrive::types::TrafficLight;
+  // using TrafficBoxComponent = carla::opendrive::types::BoxComponent;
+  // using TrafficSign = carla::opendrive::types::TrafficSign;
 
   std::string ParseError;
 
@@ -190,10 +190,7 @@ void AOpenDriveActor::BuildRoutes(FString MapName)
   // xodr file using the lavel name and the game content directory.
   const FString XodrContent = FOpenDrive::Load(MapName);
 
-  auto map_ptr = carla::opendrive::OpenDrive::Load(
-      TCHAR_TO_UTF8(*XodrContent),
-      XmlInputType::CONTENT,
-      &ParseError);
+  auto map_ptr = carla::opendrive::OpenDriveParser::Load(TCHAR_TO_UTF8(*XodrContent));
 
   if (ParseError.size())
   {
@@ -238,7 +235,8 @@ void AOpenDriveActor::BuildRoutes(FString MapName)
         // Add the identifier as visited
         AlreadyVisited.emplace_back(Identifier);
 
-        const float MaxDist = map.GetRoad(RoadId)->GetLength();
+        // const float MaxDist = map.GetRoad(RoadId)->GetLength();
+        const float MaxDist = 0; // workarround while changing the WaypointAPI
 
         std::vector<Waypoint> Waypoints;
         Waypoints.emplace_back(Successor);
@@ -280,118 +278,119 @@ void AOpenDriveActor::BuildRoutes(FString MapName)
     }
   }
 
-  const std::vector<TrafficGroup> TrafficLightGroup = map.GetTrafficGroups();
-  for (TrafficGroup CurrentTrafficLightGroup : TrafficLightGroup)
-  {
-    double RedTime = CurrentTrafficLightGroup.red_time;
-    double YellowTime = CurrentTrafficLightGroup.yellow_time;
-    double GreenTime = CurrentTrafficLightGroup.green_time;
-    FActorSpawnParameters SpawnParams;
-    FOutputDeviceNull ar;
-    AActor *SpawnedTrafficGroup = GetWorld()->SpawnActor<AActor>(TrafficGroupBlueprintClass,
-        FVector(0, 0, 0),
-        FRotator(0, 0, 0),
-        SpawnParams);
-    FString SetTrafficTimesCommand = FString::Printf(TEXT("SetTrafficTimes %f %f %f"),
-            RedTime, YellowTime, GreenTime);
-    SpawnedTrafficGroup->CallFunctionByNameWithArguments(*SetTrafficTimesCommand, ar, NULL, true);
-    for (TrafficLight CurrentTrafficLight : CurrentTrafficLightGroup.traffic_lights)
-    {
-      FVector TLPos =
-          FVector(CurrentTrafficLight.x_pos, CurrentTrafficLight.y_pos, CurrentTrafficLight.z_pos);
-      FRotator TLRot = FRotator(CurrentTrafficLight.x_rot,
-          CurrentTrafficLight.z_rot,
-          CurrentTrafficLight.y_rot);
-      AActor *SpawnedTrafficLight = GetWorld()->SpawnActor<AActor>(TrafficLightBlueprintClass,
-          TLPos,
-          TLRot,
-          SpawnParams);
-      FString AddTrafficLightCommand = FString::Printf(TEXT("AddTrafficLightPole %s"),
-            *SpawnedTrafficLight->GetName());
-      SpawnedTrafficGroup->CallFunctionByNameWithArguments(*AddTrafficLightCommand, ar, NULL, true);
-      PersistentTrafficLights.Push(SpawnedTrafficGroup);
-      SpawnedTrafficLight->CallFunctionByNameWithArguments(TEXT("InitData"), ar, NULL, true);
-      for (TrafficBoxComponent TfBoxComponent : CurrentTrafficLight.box_areas)
-      {
-        FVector TLBoxPos = FVector(TfBoxComponent.x_pos,
-            TfBoxComponent.y_pos,
-            TfBoxComponent.z_pos);
-        FRotator TLBoxRot = FRotator(TfBoxComponent.x_rot,
-            TfBoxComponent.z_rot,
-            TfBoxComponent.y_rot);
+  // const std::vector<TrafficGroup> TrafficLightGroup = map.GetTrafficGroups();
+  // for (TrafficGroup CurrentTrafficLightGroup : TrafficLightGroup)
+  // {
+  //   double RedTime = CurrentTrafficLightGroup.red_time;
+  //   double YellowTime = CurrentTrafficLightGroup.yellow_time;
+  //   double GreenTime = CurrentTrafficLightGroup.green_time;
+  //   FActorSpawnParameters SpawnParams;
+  //   FOutputDeviceNull ar;
+  //   AActor *SpawnedTrafficGroup = GetWorld()->SpawnActor<AActor>(TrafficGroupBlueprintClass,
+  //       FVector(0, 0, 0),
+  //       FRotator(0, 0, 0),
+  //       SpawnParams);
+  //   FString SetTrafficTimesCommand = FString::Printf(TEXT("SetTrafficTimes %f %f %f"),
+  //           RedTime, YellowTime, GreenTime);
+  //   SpawnedTrafficGroup->CallFunctionByNameWithArguments(*SetTrafficTimesCommand, ar, NULL, true);
+  //   for (TrafficLight CurrentTrafficLight : CurrentTrafficLightGroup.traffic_lights)
+  //   {
+  //     FVector TLPos =
+  //         FVector(CurrentTrafficLight.x_pos, CurrentTrafficLight.y_pos, CurrentTrafficLight.z_pos);
+  //     FRotator TLRot = FRotator(CurrentTrafficLight.x_rot,
+  //         CurrentTrafficLight.z_rot,
+  //         CurrentTrafficLight.y_rot);
+  //     AActor *SpawnedTrafficLight = GetWorld()->SpawnActor<AActor>(TrafficLightBlueprintClass,
+  //         TLPos,
+  //         TLRot,
+  //         SpawnParams);
+  //     FString AddTrafficLightCommand = FString::Printf(TEXT("AddTrafficLightPole %s"),
+  //           *SpawnedTrafficLight->GetName());
+  //     SpawnedTrafficGroup->CallFunctionByNameWithArguments(*AddTrafficLightCommand, ar, NULL, true);
+  //     PersistentTrafficLights.Push(SpawnedTrafficGroup);
+  //     SpawnedTrafficLight->CallFunctionByNameWithArguments(TEXT("InitData"), ar, NULL, true);
+  //     for (TrafficBoxComponent TfBoxComponent : CurrentTrafficLight.box_areas)
+  //     {
+  //       FVector TLBoxPos = FVector(TfBoxComponent.x_pos,
+  //           TfBoxComponent.y_pos,
+  //           TfBoxComponent.z_pos);
+  //       FRotator TLBoxRot = FRotator(TfBoxComponent.x_rot,
+  //           TfBoxComponent.z_rot,
+  //           TfBoxComponent.y_rot);
 
-        FString BoxCommand = FString::Printf(TEXT("SetBoxLocationAndRotation %f %f %f %f %f %f"),
-            TLBoxPos.X,
-            TLBoxPos.Y,
-            TLBoxPos.Z,
-            TLBoxRot.Pitch,
-            TLBoxRot.Roll,
-            TLBoxRot.Yaw);
-        SpawnedTrafficLight->CallFunctionByNameWithArguments(*BoxCommand, ar, NULL, true);
-        PersistentTrafficLights.Push(SpawnedTrafficLight);
-      }
-    }
-}
+  //       FString BoxCommand = FString::Printf(TEXT("SetBoxLocationAndRotation %f %f %f %f %f %f"),
+  //           TLBoxPos.X,
+  //           TLBoxPos.Y,
+  //           TLBoxPos.Z,
+  //           TLBoxRot.Pitch,
+  //           TLBoxRot.Roll,
+  //           TLBoxRot.Yaw);
+  //       SpawnedTrafficLight->CallFunctionByNameWithArguments(*BoxCommand, ar, NULL, true);
+  //       PersistentTrafficLights.Push(SpawnedTrafficLight);
+  //     }
+  //   }
+  // }
 
-const std::vector<TrafficSign> TrafficSigns = map.GetTrafficSigns();
-for (TrafficSign CurrentTrafficSign : TrafficSigns) {
-  //switch()
-  AActor* SignActor;
-  FOutputDeviceNull ar;
+  // const std::vector<TrafficSign> TrafficSigns = map.GetTrafficSigns();
+  // for (TrafficSign CurrentTrafficSign : TrafficSigns)
+  // {
+  //   //switch()
+  //   AActor* SignActor;
+  //   FOutputDeviceNull ar;
 
-  FVector TSLoc = FVector(CurrentTrafficSign.x_pos, CurrentTrafficSign.y_pos, CurrentTrafficSign.z_pos);
-  FRotator TSRot = FRotator(CurrentTrafficSign.x_rot, CurrentTrafficSign.z_rot, CurrentTrafficSign.y_rot);
-  FActorSpawnParameters SpawnParams;
-  switch(CurrentTrafficSign.speed) {
-    case 30:
-      SignActor = GetWorld()->SpawnActor<AActor>(TrafficSign30BlueprintClass,
-        TSLoc,
-        TSRot,
-        SpawnParams);
-      break;
-    case 60:
-      SignActor = GetWorld()->SpawnActor<AActor>(TrafficSign60BlueprintClass,
-        TSLoc,
-        TSRot,
-        SpawnParams);
-      break;
-    case 90:
-      SignActor = GetWorld()->SpawnActor<AActor>(TrafficSign90BlueprintClass,
-        TSLoc,
-        TSRot,
-        SpawnParams);
-      break;
-    case 100:
-      SignActor = GetWorld()->SpawnActor<AActor>(TrafficSign100BlueprintClass,
-        TSLoc,
-        TSRot,
-        SpawnParams);
-        break;
-    default:
-      FString errorMessage = "Traffic Sign not found. Posibilities: 30, 60, 90, 100";
-      UE_LOG(LogCarla, Warning, TEXT("%s"), *errorMessage);
-    break;
-  }
-  PersistentTrafficSigns.Push(SignActor);
-  for (TrafficBoxComponent TfBoxComponent : CurrentTrafficSign.box_areas)
-    {
-      FVector TLBoxPos = FVector(TfBoxComponent.x_pos,
-          TfBoxComponent.y_pos,
-          TfBoxComponent.z_pos);
-      FRotator TLBoxRot = FRotator(TfBoxComponent.x_rot,
-          TfBoxComponent.z_rot,
-          TfBoxComponent.y_rot);
+  //   FVector TSLoc = FVector(CurrentTrafficSign.x_pos, CurrentTrafficSign.y_pos, CurrentTrafficSign.z_pos);
+  //   FRotator TSRot = FRotator(CurrentTrafficSign.x_rot, CurrentTrafficSign.z_rot, CurrentTrafficSign.y_rot);
+  //   FActorSpawnParameters SpawnParams;
+  //   switch(CurrentTrafficSign.speed) {
+  //     case 30:
+  //       SignActor = GetWorld()->SpawnActor<AActor>(TrafficSign30BlueprintClass,
+  //         TSLoc,
+  //         TSRot,
+  //         SpawnParams);
+  //       break;
+  //     case 60:
+  //       SignActor = GetWorld()->SpawnActor<AActor>(TrafficSign60BlueprintClass,
+  //         TSLoc,
+  //         TSRot,
+  //         SpawnParams);
+  //       break;
+  //     case 90:
+  //       SignActor = GetWorld()->SpawnActor<AActor>(TrafficSign90BlueprintClass,
+  //         TSLoc,
+  //         TSRot,
+  //         SpawnParams);
+  //       break;
+  //     case 100:
+  //       SignActor = GetWorld()->SpawnActor<AActor>(TrafficSign100BlueprintClass,
+  //         TSLoc,
+  //         TSRot,
+  //         SpawnParams);
+  //         break;
+  //     default:
+  //       FString errorMessage = "Traffic Sign not found. Posibilities: 30, 60, 90, 100";
+  //       UE_LOG(LogCarla, Warning, TEXT("%s"), *errorMessage);
+  //     break;
+  //   }
+  //   PersistentTrafficSigns.Push(SignActor);
+  //   for (TrafficBoxComponent TfBoxComponent : CurrentTrafficSign.box_areas)
+  //   {
+  //     FVector TLBoxPos = FVector(TfBoxComponent.x_pos,
+  //         TfBoxComponent.y_pos,
+  //         TfBoxComponent.z_pos);
+  //     FRotator TLBoxRot = FRotator(TfBoxComponent.x_rot,
+  //         TfBoxComponent.z_rot,
+  //         TfBoxComponent.y_rot);
 
-      FString BoxCommand = FString::Printf(TEXT("SetBoxLocationAndRotation %f %f %f %f %f %f"),
-          TLBoxPos.X,
-          TLBoxPos.Y,
-          TLBoxPos.Z,
-          TLBoxRot.Pitch,
-          TLBoxRot.Roll,
-          TLBoxRot.Yaw);
-      SignActor->CallFunctionByNameWithArguments(*BoxCommand, ar, NULL, true);
-    }
-  }
+  //     FString BoxCommand = FString::Printf(TEXT("SetBoxLocationAndRotation %f %f %f %f %f %f"),
+  //         TLBoxPos.X,
+  //         TLBoxPos.Y,
+  //         TLBoxPos.Z,
+  //         TLBoxRot.Pitch,
+  //         TLBoxRot.Roll,
+  //         TLBoxRot.Yaw);
+  //     SignActor->CallFunctionByNameWithArguments(*BoxCommand, ar, NULL, true);
+  //   }
+  // }
 }
 
 void AOpenDriveActor::RemoveRoutes()
