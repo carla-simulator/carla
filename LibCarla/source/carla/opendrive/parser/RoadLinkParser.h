1b6f00c7339e5dcbb01a257064ece69b19b8ed93
bernatx <bernatx@gmail.com>
2019-03-14 14:47:20 +0100

    RoadLinkParser done

--- LibCarla/source/carla/opendrive/parser/RoadLinkParser.h
+++ LibCarla/source/carla/opendrive/parser/RoadLinkParser.h
@@ -6,6 +6,9 @@
 
 #pragma once
 
+#include <vector>
+#include <string>
+
 namespace pugi {
   class xml_document;
 } // namespace pugi
@@ -18,6 +21,60 @@ namespace road {
 
 namespace opendrive {
 namespace parser {
+
+  using RoadId = int;
+  using LaneId = int;
+
+  struct Polynomial {
+    double s;
+    double a, b, c, d;
+  };
+
+  // struct LaneRoadMark {
+  //   double s;
+  //   std::string type;
+  //   std::string weight;
+  //   std::string material;
+  //   std::string color;
+  //   std::string lane_change;
+  //   double width;
+  //   double height;
+  // };
+
+  struct Lane {
+    LaneId id;
+    std::string type;
+    bool level;
+    LaneId predecessor;
+    LaneId successor;
+    // std::vector<Polynomial> width;
+    // std::vector<Polynomial> borders;
+    // std::vector<LaneRoadMark> road_marks;
+  };
+
+  struct LaneSection {
+    double s;
+    double a, b, c, d;
+    std::vector<Lane> lanes;
+  };
+
+  struct RoadTypeSpeed {
+    double s;
+    std::string type;
+    double max;
+    std::string unit;
+  };
+
+  struct Road {
+    RoadId id;
+    std::string name;
+    double length;
+    RoadId junction_id;
+    RoadId predecessor;
+    RoadId successor;
+    std::vector<RoadTypeSpeed> speed;
+    std::vector<LaneSection> sections;
+  };
 
   class RoadLinkParser {
   public:
