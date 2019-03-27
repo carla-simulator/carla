b3bac35fbe75e3151b379f2aa84ba8fefe35fc71
Pasch, Frederik <frederik.pasch@intel.com>
2019-03-27 09:36:45 +0100

    LaneInvasionSensor stabilization  - Fix naming: Use 'LaneInvasionSensor'/'lane_invasion' instead of mixture with 'LaneDetector'/'lane_detector'  - Create server-side LaneInvasionSensor (to be able to access it via ROS bridge)

--- PythonAPI/manual_control.py
+++ PythonAPI/manual_control.py
@@ -605,7 +605,7 @@ class LaneInvasionSensor(object):
         self._parent = parent_actor
         self.hud = hud
         world = self._parent.get_world()
-        bp = world.get_blueprint_library().find('sensor.other.lane_detector')
+        bp = world.get_blueprint_library().find('sensor.other.lane_invasion')
         self.sensor = world.spawn_actor(bp, carla.Transform(), attach_to=self._parent)
         # We need to pass the lambda a weak reference to self to avoid circular
         # reference.
