7deb79b10005686b4936d1693cbaad33405bee17
nsubiron <nsubiron@cvc.uab.es>
2019-03-28 19:15:13 +0100

    Fix path to egg file

--- PythonAPI/util/performance_benchmark.py
+++ PythonAPI/util/performance_benchmark.py
@@ -44,7 +44,7 @@ import subprocess
 import threading
 
 try:
-    sys.path.append(glob.glob('**/carla-*%d.%d-%s.egg' % (
+    sys.path.append(glob.glob('../carla/dist/carla-*%d.%d-%s.egg' % (
         sys.version_info.major,
         sys.version_info.minor,
         'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
