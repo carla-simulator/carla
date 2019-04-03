2a50786c629f38996c0935dcf2505d75880f507a
nsubiron <nsubiron@cvc.uab.es>
2019-04-03 19:07:59 +0200

    Update spawn_npc.py to use batch commands

--- PythonAPI/examples/spawn_npc.py
+++ PythonAPI/examples/spawn_npc.py
@@ -23,8 +23,8 @@ except IndexError:
 import carla
 
 import argparse
+import logging
 import random
-import time
 
 
 def main():
@@ -59,6 +59,8 @@ def main():
         help='avoid spawning vehicles prone to accidents')
     args = argparser.parse_args()
 
+    logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
+
     actor_list = []
     client = carla.Client(args.host, args.port)
     client.set_timeout(2.0)
@@ -73,48 +75,47 @@ def main():
             blueprints = [x for x in blueprints if not x.id.endswith('isetta')]
             blueprints = [x for x in blueprints if not x.id.endswith('carlacola')]
 
-        def try_spawn_random_vehicle_at(transform):
+        spawn_points = world.get_map().get_spawn_points()
+        number_of_spawn_points = len(spawn_points)
+
+        if args.number_of_vehicles < number_of_spawn_points:
+            random.shuffle(spawn_points)
+        elif args.number_of_vehicles > number_of_spawn_points:
+            msg = 'requested %d vehicles, but could only find %d spawn points'
+            logging.warning(msg, args.number_of_vehicles, number_of_spawn_points)
+            args.number_of_vehicles = number_of_spawn_points
+
+        # @todo cannot import these directly.
+        SpawnActor = carla.command.SpawnActor
+        SetAutopilot = carla.command.SetAutopilot
+        FutureActor = carla.command.FutureActor
+
+        batch = []
+        for n, transform in enumerate(spawn_points):
+            if n >= args.number_of_vehicles:
+                break
             blueprint = random.choice(blueprints)
             if blueprint.has_attribute('color'):
                 color = random.choice(blueprint.get_attribute('color').recommended_values)
                 blueprint.set_attribute('color', color)
             blueprint.set_attribute('role_name', 'autopilot')
-            vehicle = world.try_spawn_actor(blueprint, transform)
-            if vehicle is not None:
-                actor_list.append(vehicle)
-                vehicle.set_autopilot()
-                print('spawned %r at %s' % (vehicle.type_id, transform.location))
-                return True
-            return False
+            batch.append(SpawnActor(blueprint, transform).then(SetAutopilot(FutureActor, True)))
 
-        # @todo Needs to be converted to list to be shuffled.
-        spawn_points = list(world.get_map().get_spawn_points())
-        random.shuffle(spawn_points)
+        for response in client.apply_batch_sync(batch):
+            if response.error:
+                logging.error(response.error)
+            else:
+                actor_list.append(response.actor_id)
 
-        print('found %d spawn points.' % len(spawn_points))
-
-        count = args.number_of_vehicles
-
-        for spawn_point in spawn_points:
-            if try_spawn_random_vehicle_at(spawn_point):
-                count -= 1
-            if count <= 0:
-                break
-
-        while count > 0:
-            time.sleep(args.delay)
-            if try_spawn_random_vehicle_at(random.choice(spawn_points)):
-                count -= 1
-
-        print('spawned %d vehicles, press Ctrl+C to exit.' % args.number_of_vehicles)
+        print('spawned %d vehicles, press Ctrl+C to exit.' % len(actor_list))
 
         while True:
-            time.sleep(10)
+            world.wait_for_tick()
 
     finally:
 
         print('\ndestroying %d actors' % len(actor_list))
-        client.apply_batch([carla.command.DestroyActor(x.id) for x in actor_list])
+        client.apply_batch([carla.command.DestroyActor(x) for x in actor_list])
 
 
 if __name__ == '__main__':
