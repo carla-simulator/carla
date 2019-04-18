54dd1e22fc380debc719f4f158eb04a09ef3b8e6
nsubiron <nsubiron@cvc.uab.es>
2019-04-18 12:25:11 +0200

    Merge both thread pool implementations into one

--- LibCarla/source/carla/streaming/Server.h
+++ LibCarla/source/carla/streaming/Server.h
@@ -6,7 +6,7 @@
 
 #pragma once
 
-#include "carla/streaming/detail/AsioThreadPool.h"
+#include "carla/ThreadPool.h"
 #include "carla/streaming/detail/tcp/Server.h"
 #include "carla/streaming/low_level/Server.h"
 
@@ -68,7 +68,7 @@ namespace streaming {
 
     // The order of these two arguments is very important.
 
-    detail::AsioThreadPool _service;
+    ThreadPool _service;
 
     underlying_server _server;
   };
