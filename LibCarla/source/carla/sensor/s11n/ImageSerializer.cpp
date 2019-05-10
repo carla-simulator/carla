e4fb2dec3eb58ff70848afbf53de29bee0e1e05a
wattsavolt <aidanclear@gmail.com>
2019-05-10 16:44:01 +0200

    Cosmetically enhance Deserialize function modifications

--- LibCarla/source/carla/sensor/s11n/ImageSerializer.cpp
+++ LibCarla/source/carla/sensor/s11n/ImageSerializer.cpp
@@ -13,12 +13,12 @@ namespace sensor {
 namespace s11n {
 
   SharedPtr<SensorData> ImageSerializer::Deserialize(RawData data) {
-    auto pixels = data.data();
+    auto image = SharedPtr<data::Image>(new data::Image{std::move(data)});
     // Set alpha of each pixel in the buffer to max to make it 100% opaque
-    for (size_t i = header_offset + 3; i < data.size(); i += 4) {
-      pixels[i] = 255;
+    for (auto &pixel : *image) {
+      pixel.a = 255u;
     }
-    return SharedPtr<data::Image>(new data::Image{std::move(data)});
+    return image;
   }
 
 } // namespace s11n
