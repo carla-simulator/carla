330ccdc0abb3959228da474a077030ed334c7f42
nsubiron <nsubiron@cvc.uab.es>
2019-05-21 19:00:38 +0200

    Move OpenDrive related classes to its own folder

--- Unreal/CarlaUE4/Plugins/Carla/Source/Carla/OpenDrive/OpenDrive.cpp
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/OpenDrive/OpenDrive.cpp
@@ -1,11 +1,11 @@
-// Copyright (c) 2017 Computer Vision Center (CVC) at the Universitat Autonoma
+// Copyright (c) 2019 Computer Vision Center (CVC) at the Universitat Autonoma
 // de Barcelona (UAB).
 //
 // This work is licensed under the terms of the MIT license.
 // For a copy, see <https://opensource.org/licenses/MIT>.
 
 #include "Carla.h"
-#include "Carla/Util/OpenDrive.h"
+#include "Carla/OpenDrive/OpenDrive.h"
 
 #include "Runtime/Core/Public/HAL/FileManagerGeneric.h"
 
