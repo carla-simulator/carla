9d2959bda99fe10fd6263123c3cc7494aa49e8cc
Aidan Clear <aclear@cvc.uab.es>
2019-05-28 13:25:42 +0200

    Improving visual quality of the rgb scene capture (#1549)
    * Adding TemporalAA
    * Setting alpha values of pixels in buffer to 255 to account for TemporalAA
    * Alpha now removed from pixels on the client side in the image deserialize function
    * Adding motion blur

--- Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Sensor/PixelReader.cpp
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Sensor/PixelReader.cpp
@@ -22,13 +22,15 @@
 // -- Local variables and types ------------------------------------------------
 // =============================================================================
 
-struct LockTexture {
+struct LockTexture
+{
   LockTexture(FRHITexture2D *InTexture, uint32 &Stride)
     : Texture(InTexture),
       Source(reinterpret_cast<const uint8 *>(
-          RHILockTexture2D(Texture, 0, RLM_ReadOnly, Stride, false))) {}
+            RHILockTexture2D(Texture, 0, RLM_ReadOnly, Stride, false))) {}
 
-  ~LockTexture() {
+  ~LockTexture()
+  {
     RHIUnlockTexture2D(Texture, 0, false);
   }
 
@@ -98,7 +100,8 @@ TUniquePtr<TImagePixelData<FColor>> FPixelReader::DumpPixels(
 {
   const FIntPoint DestSize(RenderTarget.GetSurfaceWidth(), RenderTarget.GetSurfaceHeight());
   TUniquePtr<TImagePixelData<FColor>> PixelData = MakeUnique<TImagePixelData<FColor>>(DestSize);
-  if (!WritePixelsToArray(RenderTarget, PixelData->Pixels)) {
+  if (!WritePixelsToArray(RenderTarget, PixelData->Pixels))
+  {
     return nullptr;
   }
   return PixelData;
@@ -112,14 +115,14 @@ TFuture<bool> FPixelReader::SavePixelsToDisk(
 }
 
 TFuture<bool> FPixelReader::SavePixelsToDisk(
-      TUniquePtr<TImagePixelData<FColor>> PixelData,
-      const FString &FilePath)
+    TUniquePtr<TImagePixelData<FColor>> PixelData,
+    const FString &FilePath)
 {
   TUniquePtr<FImageWriteTask> ImageTask = MakeUnique<FImageWriteTask>();
   ImageTask->PixelData = MoveTemp(PixelData);
   ImageTask->Filename = FilePath;
   ImageTask->Format = EImageFormat::PNG;
-  ImageTask->CompressionQuality = (int32)EImageCompressionQuality::Default;
+  ImageTask->CompressionQuality = (int32) EImageCompressionQuality::Default;
   ImageTask->bOverwriteFile = true;
   ImageTask->PixelPreProcessors.Add(TAsyncAlphaWrite<FColor>(255));
 
@@ -133,9 +136,9 @@ void FPixelReader::WritePixelsToBuffer(
     uint32 Offset,
     FRHICommandListImmediate &
 #if CARLA_WITH_VULKAN_SUPPORT == 1
-      InRHICmdList
+    InRHICmdList
 #endif // CARLA_WITH_VULKAN_SUPPORT
-  )
+    )
 {
   check(IsInRenderingThread());
 
