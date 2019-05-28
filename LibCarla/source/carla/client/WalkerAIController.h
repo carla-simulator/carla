0e0d808b34d52ac1d054c4ac5ba77ad90876cb61
nsubiron <nsubiron@cvc.uab.es>
2019-05-28 15:16:17 +0200

    Add empty WalkerAIController on client-side

---
+++ LibCarla/source/carla/client/WalkerAIController.h
@@ -0,0 +1,21 @@
+// Copyright (c) 2019 Computer Vision Center (CVC) at the Universitat Autonoma
+// de Barcelona (UAB).
+//
+// This work is licensed under the terms of the MIT license.
+// For a copy, see <https://opensource.org/licenses/MIT>.
+
+#pragma once
+
+#include "carla/client/Actor.h"
+
+namespace carla {
+namespace client {
+
+  class WalkerAIController : public Actor {
+  public:
+
+    explicit WalkerAIController(ActorInitializer init);
+  };
+
+} // namespace client
+} // namespace carla
