dee3f1c9f4579dbd9969d9312d80b1fe1a6556ee
Manish <manish.thani@gmail.com>
2019-05-31 16:41:53 +0200

    prepend static keyword for friction trigger in bp library

--- Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Actor/ActorBlueprintFunctionLibrary.cpp
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Actor/ActorBlueprintFunctionLibrary.cpp
@@ -495,7 +495,7 @@ void UActorBlueprintFunctionLibrary::MakeTriggerDefinition(
     bool &Success,
     FActorDefinition &Definition)
 {
-  FillIdAndTags(Definition, TEXT("trigger"), Id);
+  FillIdAndTags(Definition, TEXT("static"), TEXT("trigger"), Id);
   AddVariationsForTrigger(Definition);
   Success = CheckActorDefinition(Definition);
   check(Success);
