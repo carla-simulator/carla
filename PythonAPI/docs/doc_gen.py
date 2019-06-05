0281b69dae03d0ced245449cd62784af3f8dda0c
Marc Garcia Puig <marcgpuig@gmail.com>
2019-06-05 15:05:57 +0200

    Added inheritance in PythonAPI docs

--- PythonAPI/docs/doc_gen.py
+++ PythonAPI/docs/doc_gen.py
@@ -14,6 +14,7 @@ import re
 COLOR_METHOD = '#7fb800'
 COLOR_PARAM = '#00a6ed'
 COLOR_INSTANCE_VAR = '#f8805a'
+COLOR_NOTE = '#ED2F2F'
 
 QUERY = re.compile(r'([cC]arla(\.[a-zA-Z0-9_]+)+)')
 
@@ -46,31 +47,38 @@ class MarkdownFile:
 
     def list_popn(self):
         self.list_pop()
-        self._data = ''.join([self._data, '\n'])
+        self._data = join([self._data, '\n'])
 
     def list_depth(self):
         if self._data.strip()[-1:] != '\n' or self._list_depth == 0:
             return ''
-        return ''.join(['    ' * self._list_depth])
+        return join(['    ' * self._list_depth])
 
     def separator(self):
-        self._data = ''.join([self._data, '\n---\n'])
+        self._data = join([self._data, '\n---\n'])
 
     def new_line(self):
-        self._data = ''.join([self._data, self.endl])
+        self._data = join([self._data, self.endl])
 
     def text(self, buf):
-        self._data = ''.join([self._data, buf])
+        self._data = join([self._data, buf])
 
     def textn(self, buf):
-        self._data = ''.join([self._data, self.list_depth(), buf, self.endl])
+        self._data = join([self._data, self.list_depth(), buf, self.endl])
 
     def title(self, strongness, buf):
-        self._data = ''.join([
+        self._data = join([
             self._data, '\n', self.list_depth(), '#' * strongness, ' ', buf, '\n'])
 
+    def title_html(self, strongness, buf):
+        self._data = join([
+            self._data, '\n', self.list_depth(), '<h', str(strongness), '>', buf, '</h', str(strongness), '>\n'])
+
+    def note(self, buf):
+        self._data = join([self._data, buf])
+
     def code_block(self, buf, language=''):
-        return ''.join(['```', language, '\n', self.list_depth(), buf, '\n', self.list_depth(), '```\n'])
+        return join(['```', language, '\n', self.list_depth(), buf, '\n', self.list_depth(), '```\n'])
 
     def prettify_doc(self, doc):
         doc = doc.strip()
@@ -78,31 +86,31 @@ class MarkdownFile:
         return doc
 
 def italic(buf):
-    return ''.join(['_', buf, '_'])
+    return join(['_', buf, '_'])
 
 def bold(buf):
-    return ''.join(['**', buf, '**'])
+    return join(['**', buf, '**'])
 
 def code(buf):
-    return ''.join(['`', buf, '`'])
+    return join(['`', buf, '`'])
 
 def brackets(buf):
-    return ''.join(['[', buf, ']'])
+    return join(['[', buf, ']'])
 
 def parentheses(buf):
-    return ''.join(['(', buf, ')'])
+    return join(['(', buf, ')'])
 
 def small(buf):
-    return ''.join(['<sub><sup>', buf, '</sup></sub>'])
+    return join(['<sub><sup>', buf, '</sup></sub>'])
 
 def sub(buf):
-    return ''.join(['<sub>', buf, '</sub>'])
+    return join(['<sub>', buf, '</sub>'])
 
 def html_key(buf):
-    return ''.join(['<a name="', buf, '"></a>'])
+    return join(['<a name="', buf, '"></a>'])
 
 def color(col, buf):
-    return ''.join(['<font color="', col, '">', buf, '</font>'])
+    return join(['<font color="', col, '">', buf, '</font>'])
 
 def valid_dic_val(dic, value):
     return value in dic and dic[value]
@@ -158,12 +166,12 @@ def gen_stub_method_def(method):
     param = ''
     method_name = method['def_name']
     for p in method['params']:
-        p_type = ''.join([': ', str(p['type'])]) if 'type' in p else ''
-        default = ''.join([' = ', str(p['default'])]) if 'default' in p else ''
-        param = ''.join([param, p['param_name'], p_type, default, ', '])
+        p_type = join([': ', str(p['type'])]) if 'type' in p else ''
+        default = join([' = ', str(p['default'])]) if 'default' in p else ''
+        param = join([param, p['param_name'], p_type, default, ', '])
     param = param[:-2] # delete the last ', '
-    return_type = ''.join([' -> ', method['return']]) if 'return' in method else ''
-    return ''.join([method_name, parentheses(param), return_type])
+    return_type = join([' -> ', method['return']]) if 'return' in method else ''
+    return join([method_name, parentheses(param), return_type])
 
 
 def gen_doc_method_def(method, indx=False):
@@ -178,19 +186,19 @@ def gen_doc_method_def(method, indx=False):
     else:
         method_name = bold(color(COLOR_METHOD, method_name))
     for p in method['params']:
-        default = ''.join(['=', str(p['default'])]) if 'default' in p else ''
+        default = join(['=', str(p['default'])]) if 'default' in p else ''
         if indx:
-            param = ''.join([param, bold(p['param_name']), default, ', '])
+            param = join([param, bold(p['param_name']), default, ', '])
         else:
-            param = ''.join([param, '<font color="', COLOR_PARAM, '">', bold(p['param_name']), default, '</font>', ', '])
+            param = join([param, '<font color="', COLOR_PARAM, '">', bold(p['param_name']), default, '</font>', ', '])
     param = param[:-2] # delete the last ', '
-    return ''.join([method_name, parentheses(param)])
+    return join([method_name, parentheses(param)])
 
 
 def gen_inst_var_indx(inst_var, class_key):
     inst_var_name = inst_var['var_name']
-    inst_var_key = '.'.join([class_key, inst_var_name])
-    return ''.join([
+    inst_var_key = join([class_key, inst_var_name], '.')
+    return join([
         brackets(bold(inst_var_name)),
         parentheses(inst_var_key), ' ',
         sub(italic('Instance variable'))])
@@ -198,9 +206,9 @@ def gen_inst_var_indx(inst_var, class_key):
 
 def gen_method_indx(method, class_key):
     method_name = method['def_name']
-    method_key = '.'.join([class_key, method_name])
+    method_key = join([class_key, method_name], '.')
     method_def = gen_doc_method_def(method, True)
-    return ''.join([
+    return join([
         brackets(method_def),
         parentheses(method_key), ' ',
         sub(italic('Method'))])
@@ -227,36 +235,49 @@ def add_doc_method_param(md, param):
 
 def add_doc_method(md, method, class_key):
     method_name = method['def_name']
-    method_key = '.'.join([class_key, method_name])
+    method_key = join([class_key, method_name], '.')
     method_def = gen_doc_method_def(method)
-    md.list_pushn(''.join([html_key(method_key), method_def]))
+    md.list_pushn(join([html_key(method_key), method_def]))
     # method doc
     if 'doc' in method and method['doc'] is not '':
         md.textn(create_hyperlinks(md.prettify_doc(method['doc'])))
-    # ignore if only have the parameter self
-    if valid_dic_val(method, 'params') and \
-        len(method['params']) != 1 and \
-        method['params'][0] != 'self':
-        md.list_push(bold('Parameters:') + '\n')
-        for param in method['params']:
-            if param['param_name'] == 'self':
+        printed_title = False
+        for actual_param in method['params']:
+            is_self = valid_dic_val(actual_param, 'param_name') and actual_param['param_name'] == 'self'
+            have_doc = valid_dic_val(actual_param, 'doc')
+            have_type = valid_dic_val(actual_param, 'type')
+            if is_self or not have_doc and not have_type:
                 continue
-            add_doc_method_param(md, param)
-        md.list_pop()
+            # Print the 'Parameters' title once
+            if not printed_title:
+                printed_title = True
+                md.list_push(bold('Parameters:') + '\n')
+            add_doc_method_param(md, actual_param)
+        if printed_title:
+            md.list_pop()
+    # Return doc
     if valid_dic_val(method, 'return'):
         md.list_push(bold('Return:') + ' ')
         md.textn(italic(create_hyperlinks(method['return'])))
         md.list_pop()
-    # raises error doc
+
+    # Note doc
+    if valid_dic_val(method, 'note'):
+        md.list_push(bold('Note:') + ' ')
+        md.textn(color(COLOR_NOTE, italic(create_hyperlinks(method['note']))))
+        md.list_pop()
+
+    # Raises error doc
     if valid_dic_val(method, 'raises'):
         md.list_pushn(bold('Raises:') + ' ' + method['raises'])
         md.list_pop()
+
     md.list_pop()
 
 
 def add_doc_inst_var(md, inst_var, class_key):
     var_name = inst_var['var_name']
-    var_key = '.'.join([class_key, var_name])
+    var_key = join([class_key, var_name], '.')
     var_type = ''
     if valid_dic_val(inst_var, 'type'):
         var_type = ' ' + parentheses(italic(create_hyperlinks(inst_var['type'])))
@@ -306,11 +327,11 @@ class Documentation:
             if 'classes' in module and module['classes']:
                 for cl in sorted(module['classes']):
                     class_name = cl['class_name']
-                    class_key = '.'.join([module_key, class_name])
-                    md.list_pushn(
-                        brackets(bold(class_name)) +
-                        parentheses(class_key) + ' ' +
-                        sub(italic('Class')))
+                    class_key = join([module_key, class_name], '.')
+                    md.list_pushn(join([
+                        brackets(bold(class_name)),
+                        parentheses(class_key), ' ',
+                        sub(italic('Class'))]))
                     # Generate class instance variables overview (if any)
                     if 'instance_variables' in cl and cl['instance_variables']:
                         for inst_var in cl['instance_variables']:
@@ -335,22 +356,27 @@ class Documentation:
             if valid_dic_val(module, 'classes'):
                 for cl in sorted(module['classes']):
                     class_name = cl['class_name']
-                    class_key = '.'.join([module_key, class_name])
-                    md.title(2,
-                        html_key(class_key) +
-                        class_name + ' ' +
-                        small(italic('Class')))
+                    class_key = join([module_key, class_name], '.')
+                    inherits = ''
+                    if valid_dic_val(cl, 'parent'):
+                        inherits = small(parentheses(create_hyperlinks(cl['parent'])))
+                    md.title(2, join([
+                        module_name, '.',
+                        class_name,
+                        html_key(class_key),
+                        inherits, ' ',
+                        small(italic('class'))]))
                     # Class main doc
                     if valid_dic_val(cl, 'doc'):
                         md.textn(create_hyperlinks(md.prettify_doc(cl['doc'])))
                     # Generate instance variable doc (if any)
                     if valid_dic_val(cl, 'instance_variables'):
-                        md.title(3, 'Instance Variables')
+                        md.title_html(3, 'Instance Variables')
                         for inst_var in cl['instance_variables']:
                             add_doc_inst_var(md, inst_var, class_key)
                     # Generate method doc (if any)
                     if valid_dic_val(cl, 'methods'):
-                        md.title(3, 'Methods')
+                        md.title_html(3, 'Methods')
                         for method in cl['methods']:
                             add_doc_method(md, method, class_key)
                     md.separator()
@@ -358,7 +384,7 @@ class Documentation:
 
     def gen_markdown(self):
         """Generates the whole markdown file"""
-        return '\n'.join([self.gen_overview(), self.gen_body()]).strip()
+        return join([self.gen_overview(), self.gen_body()], '\n').strip()
 
 def main():
     """Main function"""
