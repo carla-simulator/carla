c821fc0e7eb3dc779e48df56b2c86e094ad5d142
nsubiron <nsubiron@cvc.uab.es>
2019-07-10 17:57:07 +0200

    Use client timeout when loading a map

--- LibCarla/source/carla/client/detail/Simulator.cpp
+++ LibCarla/source/carla/client/detail/Simulator.cpp
@@ -70,9 +70,10 @@ namespace detail {
   EpisodeProxy Simulator::LoadEpisode(std::string map_name) {
     const auto id = GetCurrentEpisode().GetId();
     _client.LoadEpisode(std::move(map_name));
-    for (auto i = 0u; i < 10u; ++i) { // 10 attempts (at most 20 seconds).
+    size_t number_of_attempts = _client.GetTimeout().milliseconds() / 10u;
+    for (auto i = 0u; i < number_of_attempts; ++i) {
       using namespace std::literals::chrono_literals;
-      _episode->WaitForState(2s); // Ignore time-outs.
+      _episode->WaitForState(10ms);
       auto episode = GetCurrentEpisode();
       if (episode.GetId() != id) {
         return episode;
