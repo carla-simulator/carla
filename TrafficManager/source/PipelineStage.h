93a4f1b3676f591dee769aae7947bcba01112674
Praveen Kumar <pravinblaze@hotmail.com>
2019-08-31 21:01:00 +0530

    WIP Testing LocalizationStage

--- TrafficManager/source/PipelineStage.h
+++ TrafficManager/source/PipelineStage.h
@@ -5,6 +5,7 @@
 #include <memory>
 #include <shared_mutex>
 #include <condition_variable>
+#include <atomic>
 
 #include "Messenger.h"
 #include "StageEnum.h"
@@ -17,22 +18,29 @@ namespace traffic_manager {
 
   private:
 
-    std::unique_ptr<std::thread> data_reciever;
-    std::unique_ptr<std::thread> data_sender;
-    std::vector<std::unique_ptr<std::thread>> action_threads;
+    std::shared_ptr<std::thread> data_receiver;
+    std::shared_ptr<std::thread> data_sender;
+    std::vector<std::shared_ptr<std::thread>> action_threads;
 
   protected:
 
     const int pool_size;
-    bool run_threads =false;
-    bool run_stage =true;
+    std::atomic<int> action_counter;
+    std::atomic<bool> run_stage;
+    std::atomic<bool> run_threads;
+    std::atomic<bool> run_receiver;
+    std::atomic<bool> run_sender;
 
-    std::shared_timed_mutex wait_for_start_mutex;
-    std::condition_variable_any wake_up_notifier;
+    std::shared_timed_mutex wait_for_action_mutex;
+    std::mutex wait_receiver_mutex;
+    std::mutex wait_sender_mutex;
+    std::condition_variable_any wake_action_notifier;
+    std::condition_variable wake_receiver_notifier;
+    std::condition_variable wake_sender_notifier;
 
     /// Implement this method with the logic to recieve data from
     /// Previous stage(s) and distribute to Action() threads
-    virtual void DataReciever()=0;
+    virtual void DataReceiver()=0;
 
     /// Implement this method with logic to gather results from action
     /// Threads and send to next stage(s)
--- TrafficManager/source/Test.cpp
+++ TrafficManager/source/Test.cpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <iostream>
 #include <signal.h>
 #include <atomic>
@@ -8,15 +10,7 @@
 
 #include "SharedData.h"
 #include "InMemoryMap.h"
-#include "SyncQueue.h"
-#include "PipelineStage.h"
-#include "FeederCallable.h"
-#include "LocalizationCallable.h"
-#include "MotionPlannerCallable.h"
-#include "TrafficLightStateCallable.h"
-#include "BatchControlCallable.h"
-#include "CollisionCallable.h"
-#include "Pipeline.h"
+#include "LocalizationStage.h"
 
 void test_dense_topology(const carla::client::World &);
 
@@ -49,8 +43,8 @@ int main(int argc, char *argv[]) {
 
   // test_dense_topology(world);
   // test_in_memory_map(world_map);
-  // test_pipeline_stages(vehicle_list, world_map, client_conn);
-  test_lane_change(world);
+  test_pipeline_stages(vehicle_list, world_map, client_conn);
+  // test_lane_change(world);
   // test_pipeline(world, client_conn, 0);
 
   return 0;
@@ -61,115 +55,91 @@ void test_pipeline_stages(
     carla::SharedPtr<carla::client::Map> world_map,
     carla::client::Client &client_conn) {
 
-  traffic_manager::SyncQueue<traffic_manager::PipelineMessage> feeder_queue(20);
-  traffic_manager::SyncQueue<traffic_manager::PipelineMessage> localization_queue(20);
-  traffic_manager::SyncQueue<traffic_manager::PipelineMessage> pid_queue(20);
-  traffic_manager::SyncQueue<traffic_manager::PipelineMessage> tl_queue(20);
-  traffic_manager::SyncQueue<traffic_manager::PipelineMessage> collision_queue(20);
-  traffic_manager::SyncQueue<traffic_manager::PipelineMessage> batch_control_queue(20);
-
-  traffic_manager::SharedData shared_data;
+  std::vector<carla::SharedPtr<carla::client::Actor>> registered_actors;
   for (auto it = actor_list->begin(); it != actor_list->end(); it++) {
-    shared_data.registered_actors.push_back(*it);
+    registered_actors.push_back(*it);
   }
 
   auto dao = traffic_manager::CarlaDataAccessLayer(world_map);
   auto topology = dao.getTopology();
-  auto local_map = std::make_shared<traffic_manager::InMemoryMap>(topology);
-  local_map->setUp(1.0);
-  shared_data.local_map = local_map;
-  shared_data.client = &client_conn;
-  auto debug_helper = client_conn.GetWorld().MakeDebugHelper();
-  shared_data.debug = &debug_helper;
+  auto local_map = traffic_manager::InMemoryMap(topology);
+  local_map.setUp(1.0);
 
-  traffic_manager::Feedercallable feeder_callable(NULL, &feeder_queue, &shared_data);
-  traffic_manager::PipelineStage feeder_stage(1, feeder_callable);
-  feeder_stage.start();
+  auto motion_control_messenger = std::make_shared<traffic_manager::MessengerType>();
+  traffic_manager::LocalizationStage localization_stage(
+    8, motion_control_messenger, registered_actors, local_map
+  );
 
-  traffic_manager::LocalizationCallable actor_localization_callable(&feeder_queue,
-      &localization_queue,
-      &shared_data);
-  traffic_manager::PipelineStage actor_localization_stage(8, actor_localization_callable);
-  actor_localization_stage.start();
+  while (motion_control_messenger->GetState() == 0);
 
-  traffic_manager::CollisionCallable collision_callable(&localization_queue, &collision_queue, &shared_data);
-  traffic_manager::PipelineStage collision_stage(8, collision_callable);
-  collision_stage.start();
-
-  traffic_manager::TrafficLightStateCallable tl_state_callable(&collision_queue, &tl_queue, &shared_data);
-  traffic_manager::PipelineStage tl_state_stage(8, tl_state_callable);
-  tl_state_stage.start();
-
-  float target_velocity = 7.0;
-  traffic_manager::MotionPlannerCallable actor_pid_callable(
-      target_velocity, &tl_queue, &pid_queue, &shared_data,
-      {0.1f, 0.15f, 0.01f}, {10.0f, 0.0f, 0.1f});
-  traffic_manager::PipelineStage actor_pid_stage(8, actor_pid_callable);
-  actor_pid_stage.start();
-
-  traffic_manager::BatchControlCallable batch_control_callable(&pid_queue, &batch_control_queue,
-      &shared_data);
-  traffic_manager::PipelineStage batch_control_stage(1, batch_control_callable);
-  batch_control_stage.start();
-
-  std::cout << "All stage pipeline started !" << std::endl;
-
+  int messenger_state;
+  long count = 0;
+  auto last_time = std::chrono::system_clock::now();
   while (true) {
-    sleep(1);
-  }
-}
+    auto dummy = motion_control_messenger->RecieveData(messenger_state);
 
-void test_pipeline(
-    carla::client::World &world,
-    carla::client::Client &client_conn,
-    int target_traffic_amount) {
+    auto current_time = std::chrono::system_clock::now();
+    std::chrono::duration<double> diff = current_time - last_time;
 
-  struct sigaction sa;
-  memset(&sa, 0, sizeof(sa));
-  sa.sa_handler = got_signal;
-  sigfillset(&sa.sa_mask);
-  sigaction(SIGINT, &sa, NULL);
-
-  traffic_manager::SharedData shared_data;
-  auto world_map = world.GetMap();
-  auto debug_helper = client_conn.GetWorld().MakeDebugHelper();
-  auto dao = traffic_manager::CarlaDataAccessLayer(world_map);
-  auto topology = dao.getTopology();
-  auto local_map = std::make_shared<traffic_manager::InMemoryMap>(topology);
-  local_map->setUp(1.0);
-  shared_data.local_map = local_map;
-  shared_data.client = &client_conn;
-  shared_data.debug = &debug_helper;
-
-  auto core_count = traffic_manager::read_core_count();
-  std::cout << "Found " << core_count << " CPU cores" << std::endl;
-  shared_data.registered_actors = traffic_manager::spawn_traffic(world, core_count, target_traffic_amount);
-
-  traffic_manager::Pipeline pipeline(
-      {0.1f, 0.15f, 0.01f},
-      {10.0f, 0.01f, 0.1f},
-      7.0f,
-      std::ceil(core_count / 4),
-      shared_data
-      );
-  pipeline.setup();
-  pipeline.start();
-
-  std::cout << "Started " << 2 + 4 * std::ceil(core_count / 4) << " pipeline threads" << std::endl;
-
-  while (true) {
-    sleep(1);
-    if (quit.load()) {
-      break;
+    if (diff.count() > 1.0) {
+      last_time = current_time;
+      std::cout << "Updates processed per second " << count * registered_actors.size() << std::endl;
+      count = 0;
     }
   }
-
-  pipeline.stop();
-  sleep(1);
-  shared_data.destroy();
-
-  std::terminate();
 }
+
+// void test_pipeline(
+//     carla::client::World &world,
+//     carla::client::Client &client_conn,
+//     int target_traffic_amount) {
+
+//   struct sigaction sa;
+//   memset(&sa, 0, sizeof(sa));
+//   sa.sa_handler = got_signal;
+//   sigfillset(&sa.sa_mask);
+//   sigaction(SIGINT, &sa, NULL);
+
+//   traffic_manager::SharedData shared_data;
+//   auto world_map = world.GetMap();
+//   auto debug_helper = client_conn.GetWorld().MakeDebugHelper();
+//   auto dao = traffic_manager::CarlaDataAccessLayer(world_map);
+//   auto topology = dao.getTopology();
+//   auto local_map = std::make_shared<traffic_manager::InMemoryMap>(topology);
+//   local_map->setUp(1.0);
+//   shared_data.local_map = local_map;
+//   shared_data.client = &client_conn;
+//   shared_data.debug = &debug_helper;
+
+//   auto core_count = traffic_manager::read_core_count();
+//   std::cout << "Found " << core_count << " CPU cores" << std::endl;
+//   shared_data.registered_actors = traffic_manager::spawn_traffic(world, core_count, target_traffic_amount);
+
+//   traffic_manager::Pipeline pipeline(
+//       {0.1f, 0.15f, 0.01f},
+//       {10.0f, 0.01f, 0.1f},
+//       7.0f,
+//       std::ceil(core_count / 4),
+//       shared_data
+//       );
+//   pipeline.setup();
+//   pipeline.start();
+
+//   std::cout << "Started " << 2 + 4 * std::ceil(core_count / 4) << " pipeline threads" << std::endl;
+
+//   while (true) {
+//     sleep(1);
+//     if (quit.load()) {
+//       break;
+//     }
+//   }
+
+//   pipeline.stop();
+//   sleep(1);
+//   shared_data.destroy();
+
+//   std::terminate();
+// }
 
 void test_in_memory_map(carla::SharedPtr<carla::client::Map> world_map) {
   auto dao = traffic_manager::CarlaDataAccessLayer(world_map);
