b0f390fab437fc3715867c781a925c78638b0e32
Jacopo Bartiromo <32928804+jackbart94@users.noreply.github.com>
2019-09-18 16:24:39 +0200

    Add files via upload

--- PythonAPI/carla/agents/tools/misc.py
+++ PythonAPI/carla/agents/tools/misc.py
@@ -9,11 +9,8 @@
 """ Module with auxiliary functions. """
 
 import math
-
 import numpy as np
-
 import carla
-
 
 def draw_waypoints(world, waypoints, z=0.5):
     """
@@ -24,13 +21,12 @@ def draw_waypoints(world, waypoints, z=0.5):
     :param z: height in meters
     :return:
     """
-    for w in waypoints:
-        t = w.transform
-        begin = t.location + carla.Location(z=z)
-        angle = math.radians(t.rotation.yaw)
+    for wpt in waypoints:
+        wpt_t = wpt.transform
+        begin = wpt_t.location + carla.Location(z=z)
+        angle = math.radians(wpt_t.rotation.yaw)
         end = begin + carla.Location(x=math.cos(angle), y=math.sin(angle))
         world.debug.draw_arrow(begin, end, arrow_size=0.3, life_time=1.0)
-
 
 def get_speed(vehicle):
     """
@@ -39,17 +35,30 @@ def get_speed(vehicle):
     :return: speed as a float in Kmh
     """
     vel = vehicle.get_velocity()
+
     return 3.6 * math.sqrt(vel.x ** 2 + vel.y ** 2 + vel.z ** 2)
 
-
-def is_within_distance_ahead(target_location, current_location, orientation, max_distance):
+def get_accel(vehicle):
     """
-    Check if a target object is within a certain distance in front of a reference object.
+    Compute acceleration of a vehicle in Kmh
+    :param vehicle: the vehicle for which acceleration is calculated
+    :return: acceleration as a float in Kmh
+    """
+    acc = vehicle.get_acceleration()
+
+    return 3.6 * math.sqrt(acc.x ** 2 + acc.y ** 2 + acc.z ** 2)
+
+def is_within_distance(target_location, current_location, orientation, max_distance, d_angle_th, d_angle_th_low=0):
+    """
+    Check if a target object is within a certain distance from a reference object.
+    A vehicle in front would be something around 0 deg, while one behind around 180 deg.
 
     :param target_location: location of the target object
     :param current_location: location of the reference object
     :param orientation: orientation of the reference object
     :param max_distance: maximum allowed distance
+    :param d_angle_th: upper thereshold for angle
+    :param d_angle_th_low: low thereshold for angle (optional, default is 0)
     :return: True if target object is within max_distance ahead of the reference object
     """
     target_vector = np.array([target_location.x - current_location.x, target_location.y - current_location.y])
@@ -66,8 +75,7 @@ def is_within_distance_ahead(target_location, current_location, orientation, max
         [math.cos(math.radians(orientation)), math.sin(math.radians(orientation))])
     d_angle = math.degrees(math.acos(np.dot(forward_vector, target_vector) / norm_target))
 
-    return d_angle < 90.0
-
+    return d_angle_th_low < d_angle < d_angle_th
 
 def compute_magnitude_angle(target_location, current_location, orientation):
     """
@@ -86,19 +94,22 @@ def compute_magnitude_angle(target_location, current_location, orientation):
 
     return (norm_target, d_angle)
 
-
 def distance_vehicle(waypoint, vehicle_transform):
+    """
+    Returns the 2D distance from a waypoint to a vehicle
+    :param waypoint: actual waypoint
+    :param vehicle_transform: transform of the target vehicle
+    """
     loc = vehicle_transform.location
-    dx = waypoint.transform.location.x - loc.x
-    dy = waypoint.transform.location.y - loc.y
+    x = waypoint.transform.location.x - loc.x
+    y = waypoint.transform.location.y - loc.y
 
-    return math.sqrt(dx * dx + dy * dy)
-
+    return math.sqrt(x * x + y * y)
 
 def vector(location_1, location_2):
     """
     Returns the unit vector from location_1 to location_2
-    location_1, location_2:   carla.Location objects
+    :param location_1, location_2: carla.Location objects
     """
     x = location_2.x - location_1.x
     y = location_2.y - location_1.y
@@ -106,3 +117,20 @@ def vector(location_1, location_2):
     norm = np.linalg.norm([x, y, z]) + np.finfo(float).eps
 
     return [x / norm, y / norm, z / norm]
+
+def compute_distance(location_1, location_2):
+    """
+    Euclidean distance between 3D points
+    :param location_1, location_2: 3D points
+    """
+    x = location_2.x - location_1.x
+    y = location_2.y - location_1.y
+    z = location_2.z - location_1.z
+    norm = np.linalg.norm([x, y, z]) + np.finfo(float).eps
+    return norm
+
+def positive(num):
+    """
+    Return the given number if positive, else 0
+    """
+    return num if num > 0.0 else 0.0
