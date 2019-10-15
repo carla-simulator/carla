43d465cf3711940aaf345d351d1880fa420ca64e
Jacopo Bartiromo <jackbart94@gmail.com>
2019-10-15 15:41:10 +0200

    new .cpp for wrapper and update libcarla.cpp

--- PythonAPI/carla/source/libcarla/TrafficManager.cpp
+++ PythonAPI/carla/source/libcarla/TrafficManager.cpp
@@ -6,22 +6,25 @@
 
 #include "Pipeline.h"
 
-namespace cc = carla::client;
-using namespace boost::python;
-using ActorList = std::vector<carla::SharedPtr<cc::Actor>>;
-using Parameters = std::vector<float>;
-
-BOOST_PYTHON_MODULE(traffic_manager) {
-
+namespace carla {
+namespace trafficmanager {
+void export_trafficmanager() {
+    using namespace boost::python;
+    namespace cc = carla::client;
+    using ActorList = std::vector<carla::SharedPtr<cc::Actor>>;
+    using Parameters = std::vector<float>;
     class_<ActorList>("actor_list").def(vector_indexing_suite<ActorList>());
 
     class_<Parameters>("parameters").def(vector_indexing_suite<Parameters>());
 
     class_<traffic_manager::Pipeline, boost::noncopyable>(
         "traffic_manager",
-        init<Parameters, Parameters, Parameters, float, float, cc::Client&>())
+        .def(init<Parameters, Parameters, Parameters, float, float, cc::Client&>()))
         .def("register_vehicles", &traffic_manager::Pipeline::RegisterVehicles)
         .def("unregister_vehicles", &traffic_manager::Pipeline::UnregisterVehicles)
         .def("start", &traffic_manager::Pipeline::Start)
         .def("stop", &traffic_manager::Pipeline::Stop);
+
+}
+}
 }
