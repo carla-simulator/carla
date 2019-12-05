b6eed8277406bba89ffe09ee388ad75ca2885751
Jacopo Bartiromo <jackbart94@gmail.com>
2019-12-05 18:56:57 +0100

    review fixes

--- PythonAPI/examples/spawn_npc.py
+++ PythonAPI/examples/spawn_npc.py
@@ -25,7 +25,15 @@ import carla
 import argparse
 import logging
 import random
+import textwrap
 
+def list_options(client):
+    maps = [m.replace('/Game/Carla/Maps/', '') for m in client.get_available_maps()]
+    indent = 4 * ' '
+    def wrap(text):
+        return '\n'.join(textwrap.wrap(text, initial_indent=indent, subsequent_indent=indent))
+    print('available maps:\n')
+    print(wrap(', '.join(sorted(maps))) + '.\n')
 
 def main():
     argparser = argparse.ArgumentParser(
@@ -55,9 +63,15 @@ def main():
         help='number of walkers (default: 50)')
     argparser.add_argument(
         '-m', '--map',
-        default='Town03',
-        type=str,
-        help='Map to be loaded')
+        help='load a new map, use --list to see available maps')
+    argparser.add_argument(
+        '-r', '--reload-map',
+        action='store_true',
+        help='reload current map')
+    argparser.add_argument(
+        '-l', '--list',
+        action='store_true',
+        help='list available options')
     argparser.add_argument(
         '--safe',
         action='store_true',
@@ -81,126 +95,136 @@ def main():
     all_id = []
     client = carla.Client(args.host, args.port)
     client.set_timeout(2.0)
+    if args.list:
+        list_options(client)
 
-    try:
+    else:
+        try:
 
-        world = client.load_world(args.map)
-        blueprints = world.get_blueprint_library().filter(args.filterv)
-        blueprintsWalkers = world.get_blueprint_library().filter(args.filterw)
-
-        if args.safe:
-            blueprints = [x for x in blueprints if int(x.get_attribute('number_of_wheels')) == 4]
-            blueprints = [x for x in blueprints if not x.id.endswith('isetta')]
-            blueprints = [x for x in blueprints if not x.id.endswith('carlacola')]
-
-        spawn_points = world.get_map().get_spawn_points()
-        number_of_spawn_points = len(spawn_points)
-
-        if args.number_of_vehicles < number_of_spawn_points:
-            random.shuffle(spawn_points)
-        elif args.number_of_vehicles > number_of_spawn_points:
-            msg = 'requested %d vehicles, but could only find %d spawn points'
-            logging.warning(msg, args.number_of_vehicles, number_of_spawn_points)
-            args.number_of_vehicles = number_of_spawn_points
-
-        # @todo cannot import these directly.
-        SpawnActor = carla.command.SpawnActor
-        SetAutopilot = carla.command.SetAutopilot
-        FutureActor = carla.command.FutureActor
-
-        # --------------
-        # Spawn vehicles
-        # --------------
-        batch = []
-        for n, transform in enumerate(spawn_points):
-            if n >= args.number_of_vehicles:
-                break
-            blueprint = random.choice(blueprints)
-            if blueprint.has_attribute('color'):
-                color = random.choice(blueprint.get_attribute('color').recommended_values)
-                blueprint.set_attribute('color', color)
-            if blueprint.has_attribute('driver_id'):
-                driver_id = random.choice(blueprint.get_attribute('driver_id').recommended_values)
-                blueprint.set_attribute('driver_id', driver_id)
-            blueprint.set_attribute('role_name', 'autopilot')
-            batch.append(SpawnActor(blueprint, transform).then(SetAutopilot(FutureActor, True)))
-
-        for response in client.apply_batch_sync(batch):
-            if response.error:
-                logging.error(response.error)
+            if args.map is not None:
+                print('load map %r.' % args.map)
+                world = client.load_world(args.map)
+            elif args.reload_map:
+                print('reload map.')
+                world = client.reload_world()
             else:
-                vehicles_list.append(response.actor_id)
+                world = client.get_world()
+            blueprints = world.get_blueprint_library().filter(args.filterv)
+            blueprintsWalkers = world.get_blueprint_library().filter(args.filterw)
 
-        # -------------
-        # Spawn Walkers
-        # -------------
-        # 1. take all the random locations to spawn
-        spawn_points = []
-        for i in range(args.number_of_walkers):
-            spawn_point = carla.Transform()
-            loc = world.get_random_location_from_navigation()
-            if (loc != None):
-                spawn_point.location = loc
-                spawn_points.append(spawn_point)
-        # 2. we spawn the walker object
-        batch = []
-        for spawn_point in spawn_points:
-            walker_bp = random.choice(blueprintsWalkers)
-            # set as not invencible
-            if walker_bp.has_attribute('is_invincible'):
-                walker_bp.set_attribute('is_invincible', 'false')
-            batch.append(SpawnActor(walker_bp, spawn_point))
-        results = client.apply_batch_sync(batch, True)
-        for i in range(len(results)):
-            if results[i].error:
-                logging.error(results[i].error)
-            else:
-                walkers_list.append({"id": results[i].actor_id})
-        # 3. we spawn the walker controller
-        batch = []
-        walker_controller_bp = world.get_blueprint_library().find('controller.ai.walker')
-        for i in range(len(walkers_list)):
-            batch.append(SpawnActor(walker_controller_bp, carla.Transform(), walkers_list[i]["id"]))
-        results = client.apply_batch_sync(batch, True)
-        for i in range(len(results)):
-            if results[i].error:
-                logging.error(results[i].error)
-            else:
-                walkers_list[i]["con"] = results[i].actor_id
-        # 4. we put altogether the walkers and controllers id to get the objects from their id
-        for i in range(len(walkers_list)):
-            all_id.append(walkers_list[i]["con"])
-            all_id.append(walkers_list[i]["id"])
-        all_actors = world.get_actors(all_id)
+            if args.safe:
+                blueprints = [x for x in blueprints if int(x.get_attribute('number_of_wheels')) == 4]
+                blueprints = [x for x in blueprints if not x.id.endswith('isetta')]
+                blueprints = [x for x in blueprints if not x.id.endswith('carlacola')]
 
-        # wait for a tick to ensure client receives the last transform of the walkers we have just created
-        world.wait_for_tick()
+            spawn_points = world.get_map().get_spawn_points()
+            number_of_spawn_points = len(spawn_points)
 
-        # 5. initialize each controller and set target to walk to (list is [controler, actor, controller, actor ...])
-        for i in range(0, len(all_id), 2):
-            # start walker
-            all_actors[i].start()
-            # set walk to random point
-            all_actors[i].go_to_location(world.get_random_location_from_navigation())
-            # random max speed
-            all_actors[i].set_max_speed(1 + random.random()/2)    # max speed between 1 and 1.5 (default is 1.4 m/s)
+            if args.number_of_vehicles < number_of_spawn_points:
+                random.shuffle(spawn_points)
+            elif args.number_of_vehicles > number_of_spawn_points:
+                msg = 'requested %d vehicles, but could only find %d spawn points'
+                logging.warning(msg, args.number_of_vehicles, number_of_spawn_points)
+                args.number_of_vehicles = number_of_spawn_points
 
-        print('spawned %d vehicles and %d walkers, press Ctrl+C to exit.' % (len(vehicles_list), len(walkers_list)))
+            # @todo cannot import these directly.
+            SpawnActor = carla.command.SpawnActor
+            SetAutopilot = carla.command.SetAutopilot
+            FutureActor = carla.command.FutureActor
 
-        while True:
+            # --------------
+            # Spawn vehicles
+            # --------------
+            batch = []
+            for n, transform in enumerate(spawn_points):
+                if n >= args.number_of_vehicles:
+                    break
+                blueprint = random.choice(blueprints)
+                if blueprint.has_attribute('color'):
+                    color = random.choice(blueprint.get_attribute('color').recommended_values)
+                    blueprint.set_attribute('color', color)
+                if blueprint.has_attribute('driver_id'):
+                    driver_id = random.choice(blueprint.get_attribute('driver_id').recommended_values)
+                    blueprint.set_attribute('driver_id', driver_id)
+                blueprint.set_attribute('role_name', 'autopilot')
+                batch.append(SpawnActor(blueprint, transform).then(SetAutopilot(FutureActor, True)))
+
+            for response in client.apply_batch_sync(batch):
+                if response.error:
+                    logging.error(response.error)
+                else:
+                    vehicles_list.append(response.actor_id)
+
+            # -------------
+            # Spawn Walkers
+            # -------------
+            # 1. take all the random locations to spawn
+            spawn_points = []
+            for i in range(args.number_of_walkers):
+                spawn_point = carla.Transform()
+                loc = world.get_random_location_from_navigation()
+                if (loc != None):
+                    spawn_point.location = loc
+                    spawn_points.append(spawn_point)
+            # 2. we spawn the walker object
+            batch = []
+            for spawn_point in spawn_points:
+                walker_bp = random.choice(blueprintsWalkers)
+                # set as not invencible
+                if walker_bp.has_attribute('is_invincible'):
+                    walker_bp.set_attribute('is_invincible', 'false')
+                batch.append(SpawnActor(walker_bp, spawn_point))
+            results = client.apply_batch_sync(batch, True)
+            for i in range(len(results)):
+                if results[i].error:
+                    logging.error(results[i].error)
+                else:
+                    walkers_list.append({"id": results[i].actor_id})
+            # 3. we spawn the walker controller
+            batch = []
+            walker_controller_bp = world.get_blueprint_library().find('controller.ai.walker')
+            for i in range(len(walkers_list)):
+                batch.append(SpawnActor(walker_controller_bp, carla.Transform(), walkers_list[i]["id"]))
+            results = client.apply_batch_sync(batch, True)
+            for i in range(len(results)):
+                if results[i].error:
+                    logging.error(results[i].error)
+                else:
+                    walkers_list[i]["con"] = results[i].actor_id
+            # 4. we put altogether the walkers and controllers id to get the objects from their id
+            for i in range(len(walkers_list)):
+                all_id.append(walkers_list[i]["con"])
+                all_id.append(walkers_list[i]["id"])
+            all_actors = world.get_actors(all_id)
+
+            # wait for a tick to ensure client receives the last transform of the walkers we have just created
             world.wait_for_tick()
 
-    finally:
+            # 5. initialize each controller and set target to walk to (list is [controler, actor, controller, actor ...])
+            for i in range(0, len(all_id), 2):
+                # start walker
+                all_actors[i].start()
+                # set walk to random point
+                all_actors[i].go_to_location(world.get_random_location_from_navigation())
+                # random max speed
+                all_actors[i].set_max_speed(1 + random.random()/2)    # max speed between 1 and 1.5 (default is 1.4 m/s)
 
-        print('\ndestroying %d vehicles' % len(vehicles_list))
-        client.apply_batch([carla.command.DestroyActor(x) for x in vehicles_list])
+            print('spawned %d vehicles and %d walkers, press Ctrl+C to exit.' % (len(vehicles_list), len(walkers_list)))
 
-        # stop walker controllers (list is [controler, actor, controller, actor ...])
-        for i in range(0, len(all_id), 2):
-            all_actors[i].stop()
+            while True:
+                world.wait_for_tick()
 
-        print('\ndestroying %d walkers' % len(walkers_list))
-        client.apply_batch([carla.command.DestroyActor(x) for x in all_id])
+        finally:
+
+            print('\ndestroying %d vehicles' % len(vehicles_list))
+            client.apply_batch([carla.command.DestroyActor(x) for x in vehicles_list])
+
+            # stop walker controllers (list is [controler, actor, controller, actor ...])
+            for i in range(0, len(all_id), 2):
+                all_actors[i].stop()
+
+            print('\ndestroying %d walkers' % len(walkers_list))
+            client.apply_batch([carla.command.DestroyActor(x) for x in all_id])
 
 
 if __name__ == '__main__':
