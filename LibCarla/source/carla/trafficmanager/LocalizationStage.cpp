10362b0174799fc9dc97a887d4f95df234f5b76e
Jacopo Bartiromo <jackbart94@gmail.com>
2019-12-11 08:31:18 +0100

    Increased min lane change distance Rename tm_demo to _tm_spawn_npc Add walker spawn Remove tm_set_autopilot_test

--- LibCarla/source/carla/trafficmanager/LocalizationStage.cpp
+++ LibCarla/source/carla/trafficmanager/LocalizationStage.cpp
@@ -9,7 +9,7 @@ namespace LocalizationConstants {
   static const float TARGET_WAYPOINT_HORIZON_LENGTH = 4.0f;
   static const float MINIMUM_JUNCTION_LOOK_AHEAD = 3.0f;
   static const float HIGHWAY_SPEED = 50 / 3.6f;
-  static const float MINIMUM_LANE_CHANGE_DISTANCE = 10.0f;
+  static const float MINIMUM_LANE_CHANGE_DISTANCE = 20.0f;
   static const float MAXIMUM_LANE_OBSTACLE_CURVATURE = 0.93969f;
 }
   using namespace LocalizationConstants;
