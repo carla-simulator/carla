e89170af13364e6cb66dab1e58fc8d1bbac611ff
Guillermo <glopez@cvc.uab.es>
2020-01-09 11:28:11 +0100

    WIP

--- LibCarla/source/carla/trafficmanager/CollisionStage.cpp
+++ LibCarla/source/carla/trafficmanager/CollisionStage.cpp
@@ -372,6 +372,52 @@ namespace CollisionStageConstants {
 
     return bbox_boundary;
   }
+  
+  bool CollisionStage::IsLocationAfterJunctionSafe(const Actor &ego_actor, const Actor &actor){
+    
+    bool safe_junction = true;
+
+    // GetBoundary() simplified for the new "ghost" location after the junction
+    const SimpleWaypointPtr safe_point_junction = localization_frame->at(vehicle_id_to_index.at(ego_actor->GetId())).safe_point_after_junction;
+
+    if (safe_point_junction != nullptr){
+
+      cg::Location location = safe_point_junction->GetLocation();
+      cg::Vector3D heading_vector = safe_point_junction->GetForwardVector();
+      heading_vector.z = 0.0f;
+      heading_vector = heading_vector.MakeUnitVector();
+
+      cg::BoundingBox bbox;
+      const auto vehicle = boost::static_pointer_cast<cc::Vehicle>(ego_actor);
+      bbox = vehicle->GetBoundingBox();
+      const cg::Vector3D extent = bbox.extent;
+
+      const cg::Vector3D perpendicular_vector = cg::Vector3D(-heading_vector.y, heading_vector.x, 0.0f);
+
+      const cg::Vector3D x_boundary_vector = heading_vector * extent.x;
+      const cg::Vector3D y_boundary_vector = perpendicular_vector * extent.y;
+
+      LocationList ego_actor_boundary = {
+        location + cg::Location(x_boundary_vector - y_boundary_vector),
+        location + cg::Location(-1.0f * x_boundary_vector - y_boundary_vector),
+        location + cg::Location(-1.0f * x_boundary_vector + y_boundary_vector),
+        location + cg::Location(x_boundary_vector + y_boundary_vector),
+      };
+
+      //DrawBoundary(ego_actor_boundary);
+
+      const Polygon reference_polygon = GetPolygon(ego_actor_boundary);
+
+      const Polygon other_polygon = GetPolygon(GetBoundary(actor));
+
+      const auto inter_bbox_distance = bg::distance(reference_polygon, other_polygon);
+      if (inter_bbox_distance < 0.5f){
+        safe_junction = false;
+      }
+    }
+
+    return safe_junction;
+  }
 
   void CollisionStage::DrawBoundary(const LocationList &boundary) {
     for (uint64_t i = 0u; i < boundary.size(); ++i) {
