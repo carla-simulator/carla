83836ffd3bf368cceaff4e6ed38333dba790fa01
Marc Garcia Puig <marcgpuig@gmail.com>
2020-01-13 19:10:20 +0100

    Added more weather attributes

--- LibCarla/source/carla/rpc/WeatherParameters.h
+++ LibCarla/source/carla/rpc/WeatherParameters.h
@@ -42,44 +42,59 @@ namespace rpc {
     WeatherParameters() = default;
 
     WeatherParameters(
-        float in_cloudyness,
+        float in_cloudiness,
         float in_precipitation,
         float in_precipitation_deposits,
         float in_wind_intensity,
         float in_sun_azimuth_angle,
-        float in_sun_altitude_angle)
-      : cloudyness(in_cloudyness),
+        float in_sun_altitude_angle,
+        float in_fog_density,
+        float in_fog_distance,
+        float in_wetness)
+      : cloudiness(in_cloudiness),
         precipitation(in_precipitation),
         precipitation_deposits(in_precipitation_deposits),
         wind_intensity(in_wind_intensity),
         sun_azimuth_angle(in_sun_azimuth_angle),
-        sun_altitude_angle(in_sun_altitude_angle) {}
+        sun_altitude_angle(in_sun_altitude_angle),
+        fog_density(in_fog_density),
+        fog_distance(in_fog_distance),
+        wetness(in_wetness) {}
 
-    float cloudyness = 0.0f;
+    float cloudiness = 0.0f;
     float precipitation = 0.0f;
     float precipitation_deposits = 0.0f;
     float wind_intensity = 0.0f;
     float sun_azimuth_angle = 0.0f;
     float sun_altitude_angle = 0.0f;
+    float fog_density = 0.0f;
+    float fog_distance = 0.0f;
+    float wetness = 0.0f;
 
 #ifdef LIBCARLA_INCLUDED_FROM_UE4
 
     WeatherParameters(const FWeatherParameters &Weather)
-      : cloudyness(Weather.Cloudyness),
+      : cloudiness(Weather.Cloudiness),
         precipitation(Weather.Precipitation),
         precipitation_deposits(Weather.PrecipitationDeposits),
         wind_intensity(Weather.WindIntensity),
         sun_azimuth_angle(Weather.SunAzimuthAngle),
-        sun_altitude_angle(Weather.SunAltitudeAngle) {}
+        sun_altitude_angle(Weather.SunAltitudeAngle),
+        fog_density(Weather.FogDensity),
+        fog_distance(Weather.FogDistance),
+        wetness(Weather.Wetness) {}
 
     operator FWeatherParameters() const {
       FWeatherParameters Weather;
-      Weather.Cloudyness = cloudyness;
+      Weather.Cloudiness = cloudiness;
       Weather.Precipitation = precipitation;
       Weather.PrecipitationDeposits = precipitation_deposits;
       Weather.WindIntensity = wind_intensity;
       Weather.SunAzimuthAngle = sun_azimuth_angle;
       Weather.SunAltitudeAngle = sun_altitude_angle;
+      Weather.FogDensity = fog_density;
+      Weather.FogDistance = fog_distance;
+      Weather.Wetness = wetness;
       return Weather;
     }
 
@@ -87,12 +102,15 @@ namespace rpc {
 
     bool operator!=(const WeatherParameters &rhs) const {
       return
-          cloudyness != rhs.cloudyness ||
+          cloudiness != rhs.cloudiness ||
           precipitation != rhs.precipitation ||
           precipitation_deposits != rhs.precipitation_deposits ||
           wind_intensity != rhs.wind_intensity ||
           sun_azimuth_angle != rhs.sun_azimuth_angle ||
-          sun_altitude_angle != rhs.sun_altitude_angle;
+          sun_altitude_angle != rhs.sun_altitude_angle ||
+          fog_density != rhs.fog_density ||
+          fog_distance != rhs.fog_distance ||
+          wetness != rhs.wetness;
     }
 
     bool operator==(const WeatherParameters &rhs) const {
@@ -100,12 +118,15 @@ namespace rpc {
     }
 
     MSGPACK_DEFINE_ARRAY(
-        cloudyness,
+        cloudiness,
         precipitation,
         precipitation_deposits,
         wind_intensity,
         sun_azimuth_angle,
-        sun_altitude_angle);
+        sun_altitude_angle,
+        fog_density,
+        fog_distance,
+        wetness);
   };
 
 } // namespace rpc
