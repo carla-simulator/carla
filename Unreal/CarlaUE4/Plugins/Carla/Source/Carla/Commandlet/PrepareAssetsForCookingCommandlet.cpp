371cd733c8d7398b588a3bb8bfcf8c54339e6327
Michele Bianchi <jazzinghen@gmail.com>
2020-01-16 18:06:51 +0900

    Fixed strange deletion and saving order

--- Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Commandlet/PrepareAssetsForCookingCommandlet.cpp
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Commandlet/PrepareAssetsForCookingCommandlet.cpp
@@ -222,10 +222,6 @@ bool UPrepareAssetsForCookingCommandlet::SaveWorld(
     SavePackage(PackagePath, Package);
   }
 
-  #if WITH_EDITOR
-  UEditorLoadingAndSavingUtils::SaveMap(World, PackagePath);
-  #endif
-
   return bPackageSaved;
 }
 
@@ -320,11 +316,6 @@ bool UPrepareAssetsForCookingCommandlet::SavePackage(const FString &PackagePath,
   FString PackageFileName = FPackageName::LongPackageNameToFilename(PackagePath,
       FPackageName::GetMapPackageExtension());
 
-  if (FPaths::FileExists(*PackageFileName))
-  {
-    // Will not save package if it already exists
-    return false;
-  }
   return UPackage::SavePackage(Package, World, EObjectFlags::RF_Public | EObjectFlags::RF_Standalone,
       *PackageFileName, GError, nullptr, true, true, SAVE_NoError);
 }
