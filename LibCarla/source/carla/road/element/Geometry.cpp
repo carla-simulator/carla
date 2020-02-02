fbed82acaf757b9f2230625201e060ed50ceebfd
Axel1092 <axellopez92@outlook.com>
2020-02-02 20:13:09 +0100

    Added geometry paramPoly3.

--- LibCarla/source/carla/road/element/Geometry.cpp
+++ LibCarla/source/carla/road/element/Geometry.cpp
@@ -162,7 +162,7 @@ namespace element {
 
   DirectedPoint GeometryParamPoly3::PosFromDist(double dist) const
   {
-    double p = dist;
+    /*double p = dist;
     if (!_arcLength) {
       p = dist / _length;
     }
@@ -181,13 +181,68 @@ namespace element {
 
     DirectedPoint point(_start_position, _heading + theta);
     point.location.x += pos.x;
-    point.location.y += pos.y;
-    return point;
+    point.location.y += pos.y;*/
+    if (!_arcLength) {
+      dist = dist / _length;
+    }
+    auto result = _rtree.GetNearestNeighbours(Rtree::BPoint(static_cast<float>(dist), 0.0f, 0.0f)).front();
+
+    auto &val1 = result.second.first;
+    auto &val2 = result.second.second;
+
+    double rate = (val2.s - dist) / (val2.s - val1.s);
+    double u = rate * val1.u + (1.0 - rate) * val2.u;
+    double v = rate * val1.v + (1.0 - rate) * val2.v;
+    double t_u = (rate * val1.t_u + (1.0 - rate) * val2.t_u);
+    double t_v = (rate * val1.t_v + (1.0 - rate) * val2.t_v);
+    double tangent = atan2(t_v, t_u); // ?
+
+    geom::Vector2D pos = RotatebyAngle(_heading, u, v);
+    DirectedPoint p(_start_position, _heading + tangent);
+    p.location.x += pos.x;
+    p.location.y += pos.y;
+    return p;
   }
   std::pair<float, float> GeometryParamPoly3::DistanceTo(const geom::Location &) const {
     // No analytical expression (Newton-Raphson?/point search)
     // throw_exception(std::runtime_error("not implemented"));
     return {_start_position.x, _start_position.y};
+  }
+
+  void GeometryParamPoly3::PreComputeSpline() {
+      size_t number_intervals = 1000;
+      double delta_p = 1.0 / number_intervals;
+      if (_arcLength) {
+          delta_p *= _length;
+      }
+      double param_p = 0;
+      double current_s = 0;
+      double last_u = 0;
+      double last_v = 0;
+      double last_s = 0;
+      RtreeValue last_val;
+      for(size_t i = 0; i < number_intervals; ++i) {
+          double current_u = _polyU.Evaluate(param_p);
+          double current_v = _polyV.Evaluate(param_p);
+          double du = current_u - last_u;
+          double dv = current_v - last_v;
+          double ds = sqrt(du * du + dv * dv);
+          current_s += ds;
+          double current_t_u = _polyU.Tangent(param_p);
+          double current_t_v = _polyV.Tangent(param_p);
+          RtreeValue current_val{ current_u, current_v, current_s, current_t_u, current_t_v };
+
+          Rtree::BPoint p1(static_cast<float>(last_s), 0.0f, 0.0f);
+          Rtree::BPoint p2(static_cast<float>(current_s), 0.0f, 0.0f);
+          _rtree.InsertElement(Rtree::BSegment(p1, p2), last_val, current_val);
+
+          last_u = current_u;
+          last_v = current_v;
+          last_s = current_s;
+          last_val = current_val;
+
+          param_p += delta_p;
+      }
   }
 } // namespace element
 } // namespace road
