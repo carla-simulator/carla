554f52e2a2a70149be80695e250e80e450a4c911
Marc Garcia Puig <marcgpuig@gmail.com>
2020-02-05 18:46:50 +0100

    Fixed linux FBX2OBJ installat

--- Util/BuildTools/Linux.mk
+++ Util/BuildTools/Linux.mk
@@ -9,7 +9,7 @@ launch: LibCarla.server.release
 launch-only:
 	@${CARLA_BUILD_TOOLS_FOLDER}/BuildCarlaUE4.sh --launch $(ARGS)
 
-import: CarlaUE4Editor
+import: CarlaUE4Editor PythonAPI build.utils
 	@${CARLA_BUILD_TOOLS_FOLDER}/Import.py $(ARGS)
 
 package: CarlaUE4Editor PythonAPI
