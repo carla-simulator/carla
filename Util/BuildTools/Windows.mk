4205562dbb78274a95859683eb12c42997ddc924
bernatx <bernatx@gmail.com>
2020-02-12 14:37:01 +0100

    Add 'make CarlaUE4Editor' to windows

--- Util/BuildTools/Windows.mk
+++ Util/BuildTools/Windows.mk
@@ -16,6 +16,9 @@ help:
 import: server
 	@"${CARLA_BUILD_TOOLS_FOLDER}/Import.py"
 
+CarlaUE4Editor: LibCarla
+	@"${CARLA_BUILD_TOOLS_FOLDER}/BuildCarlaUE4.bat"
+
 launch: LibCarla
 	@"${CARLA_BUILD_TOOLS_FOLDER}/BuildCarlaUE4.bat" --launch
 
