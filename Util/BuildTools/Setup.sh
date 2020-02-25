c3342d445c5b72aa89424f3d2640f81f143cfe91
Axel <axellopez92@outlook.com>
2020-02-25 09:51:54 +0100

    Updated boost patch to fix exception error.

--- Util/BuildTools/Setup.sh
+++ Util/BuildTools/Setup.sh
@@ -157,12 +157,12 @@ else
   rm -Rf ${BOOST_BASENAME}-source
   rm ${BOOST_PACKAGE_BASENAME}.tar.gz
 
-fi
-
 # Boost patch for exception handling
 cp "${CARLA_BUILD_FOLDER}/../Util/BoostFiles/rational.hpp" "${BOOST_BASENAME}-install/include/boost/rational.hpp"
 cp "${CARLA_BUILD_FOLDER}/../Util/BoostFiles/read.hpp" "${BOOST_BASENAME}-install/include/boost/geometry/io/wkt/read.hpp"
 # ---
+
+fi
 
 unset BOOST_BASENAME
 
