87cc5c7e47abe57d01498f731c590dcfec198f4e
bernat <bernatx@gmail.com>
2020-03-19 10:01:29 +0100

    More fixes to Jenkinsfile (Doxygen, Windows...) Adding more scripts to Jenkinsfile
    Adding autoscaling scripts

--- Util/BuildTools/Windows.mk
+++ Util/BuildTools/Windows.mk
@@ -26,7 +26,7 @@ launch-only:
 	@"${CARLA_BUILD_TOOLS_FOLDER}/BuildCarlaUE4.bat" --launch
 
 package: PythonAPI
-	@"${CARLA_BUILD_TOOLS_FOLDER}/Package.bat" --ue-version 4.22
+	@"${CARLA_BUILD_TOOLS_FOLDER}/Package.bat" --ue-version 4.22 $(ARGS)
 
 docs:
 	@doxygen
