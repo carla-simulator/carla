0fd3544fa4e1335b4636527e76b24398f8877e2f
Marc Garcia Puig <marcgpuig@gmail.com>
2020-03-20 17:05:13 +0100

    Fixed mesh indexes

--- LibCarla/source/carla/road/Map.cpp
+++ LibCarla/source/carla/road/Map.cpp
@@ -1021,20 +1021,22 @@ namespace road {
           } else {
             out_mesh.AddMaterial("road");
           }
-          // Add 2 first vertices only
-          std::pair<geom::Vector3D, geom::Vector3D> edges =
-              GetWaypointCornerPositions(*this, current_wp, lane);
-          out_mesh.AddVertex(edges.first);
-          out_mesh.AddVertex(edges.second);
 
+          std::pair<geom::Vector3D, geom::Vector3D> edges;
           if (!IsLaneStraight(lane)) {
             do {
               // Get the location of the edges of the current lane at the current waypoint
               edges = GetWaypointCornerPositions(*this, current_wp, lane);
-              // Extrude adding vertices and joining the using indices
-              const size_t last_index = out_mesh.GetLastVertexIndex();
-              ExtrudeMeshEdge(
-                  out_mesh, edges.first, edges.second, last_index - 1, last_index);
+              if (out_mesh.GetVerticesNum() < 2) {
+                // Add 2 first vertices only
+                out_mesh.AddVertex(edges.first);
+                out_mesh.AddVertex(edges.second);
+              } else {
+                // Extrude adding vertices and joining the using indices
+                const size_t last_index = out_mesh.GetLastVertexIndex();
+                ExtrudeMeshEdge(
+                    out_mesh, edges.first, edges.second, last_index - 1, last_index);
+              }
 
               // Update the current waypoint's "s"
               current_wp.s += distance;
@@ -1063,7 +1065,7 @@ namespace road {
 
     // const std::vector<geom::Location> crosswalk_vertex = GetAllCrosswalkZones();
 
-    //
+    // todo: delete this
     std::vector<geom::Location> crosswalk_vertex = GetAllCrosswalkZones();
     std::transform(
         crosswalk_vertex.begin(), crosswalk_vertex.end(), crosswalk_vertex.begin(),
@@ -1078,42 +1080,44 @@ namespace road {
     out_mesh.AddMaterial("sidewalk");
     size_t start_vertex_index = 0;
     size_t fan_index = 1;
+
+    size_t initial_v = 0;
     out_mesh.AddVertex(crosswalk_vertex[0]);
+
     std::cout << "v " << 0 << std::endl;
 
     for (size_t i = 1; i < crosswalk_vertex.size(); ++i) {
       if (crosswalk_vertex[start_vertex_index] == crosswalk_vertex[i]) {
-        // Close triangle fan
-        out_mesh.AddIndex(start_vertex_index);
-        out_mesh.AddIndex(i - 2);
-        out_mesh.AddIndex(i - 1);
-        std::cout << "* " << start_vertex_index << " " << i - 2 << " " << i - 1 << std::endl;
         // If is the last vertex of the list, is done
+        std::cout << "fan done" << std::endl;
         if (i >= crosswalk_vertex.size() - 1 ) {
+          std::cout << "all fan done" << std::endl;
           break;
         }
         start_vertex_index = ++i;
         fan_index = 0;
+        initial_v = out_mesh.GetVerticesNum();
       }
 
       out_mesh.AddVertex(crosswalk_vertex[i]);
-      std::cout << "v " << fan_index << std::endl;
+
+      std::cout << "current vertex: " << out_mesh.GetVerticesNum() - 1 << std::endl;
+
       if (fan_index >= 2) {
         // Vertex order is counter clockwise
 
         // @todo Fix wrong index! It is using the raw index and it should
         //  depend on start_vertex_index, something like:
-
-        // out_mesh.AddIndex(start_vertex_index);
-        // out_mesh.AddIndex(start_vertex_index + fan_index);
-        // out_mesh.AddIndex(start_vertex_index + fan_index + 1);
+        out_mesh.AddIndex(initial_v);
+        out_mesh.AddIndex(out_mesh.GetVerticesNum() - 2);
+        out_mesh.AddIndex(out_mesh.GetVerticesNum() - 1);
 
         // instead of
-        out_mesh.AddIndex(start_vertex_index);
-        out_mesh.AddIndex(i - 1);
-        out_mesh.AddIndex(i);
+        // out_mesh.AddIndex(start_vertex_index);
+        // out_mesh.AddIndex(i - 1);
+        // out_mesh.AddIndex(i);
 
-        std::cout << "  " << start_vertex_index << " " << i - 1 << " " << i << std::endl;
+        std::cout << "  " << initial_v << " " << out_mesh.GetVerticesNum() - 2 << " " << out_mesh.GetVerticesNum() - 1 << std::endl;
       }
       ++fan_index;
     }
