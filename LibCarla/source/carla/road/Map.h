ffd7f592189767054f0895255dc2e3c4cc18d4d8
Axel1092 <axellopez92@outlook.com>
2020-03-23 15:01:24 +0100

    Added computation of conflicting road in junctions.

--- LibCarla/source/carla/road/Map.h
+++ LibCarla/source/carla/road/Map.h
@@ -139,6 +139,9 @@ namespace road {
 
     const Junction* GetJunction(JuncId id) const;
 
+    std::unordered_map<road::RoadId, std::unordered_set<road::RoadId>>
+        ComputeJunctionConflicts(JuncId id) const;
+
     /// Buids a mesh based on the OpenDRIVE
     geom::Mesh GenerateMesh(double distance) const;
 
