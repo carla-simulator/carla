67b17ee42a4a588e85bcb3a49133c437790c4259
Bernd Gassmann <bernd.gassmann@intel.com>
2020-03-25 18:10:25 +0100

    Code format according to google style
    and tried to spot the remaining CamelStyle variable names and changed to
    snake_style
    
    Added code formatting script and clang-format slightly changed from
    google format:
    AccessModifierOffset: -1 -> -2
    AllowShortFunctionsOnASingleLine: All -> Empty
    ConstructorInitializerIndentWidth: 4 -> 2

--- LibCarla/source/carla/rss/RssSensor.cpp
+++ LibCarla/source/carla/rss/RssSensor.cpp
@@ -13,22 +13,20 @@
 #include <fstream>
 
 #include "carla/Logging.h"
-#include "carla/client/Sensor.h"
 #include "carla/client/Map.h"
+#include "carla/client/Sensor.h"
 #include "carla/client/detail/Simulator.h"
 #include "carla/rss/RssCheck.h"
 #include "carla/sensor/data/RssResponse.h"
-
 
 namespace carla {
 namespace client {
 
 RssSensor::RssSensor(ActorInitializer init)
-  : Sensor(std::move(init))
-  , _on_tick_register_id(0u)
-  , _rss_check(std::make_shared<::carla::rss::RssCheck>())
-  , _drop_route(false) {
-}
+  : Sensor(std::move(init)),
+    _on_tick_register_id(0u),
+    _rss_check(std::make_shared<::carla::rss::RssCheck>()),
+    _drop_route(false) {}
 
 RssSensor::~RssSensor() = default;
 
@@ -39,7 +37,8 @@ void RssSensor::Listen(CallbackFunctionType callback) {
   }
 
   if (GetParent() == nullptr) {
-    throw_exception(std::runtime_error(GetDisplayId() + ": not attached to vehicle"));
+    throw_exception(
+        std::runtime_error(GetDisplayId() + ": not attached to vehicle"));
     return;
   }
 
@@ -49,23 +48,24 @@ void RssSensor::Listen(CallbackFunctionType callback) {
 
   ::ad::map::access::cleanup();
   ::ad::map::access::initFromOpenDriveContent(
-    open_drive_content, 0.2,
-    ::ad::map::intersection::IntersectionType::TrafficLight,
-    ::ad::map::landmark::TrafficLightType::LEFT_STRAIGHT_RED_YELLOW_GREEN);
+      open_drive_content, 0.2,
+      ::ad::map::intersection::IntersectionType::TrafficLight,
+      ::ad::map::landmark::TrafficLightType::LEFT_STRAIGHT_RED_YELLOW_GREEN);
 
   auto self = boost::static_pointer_cast<RssSensor>(shared_from_this());
 
   log_debug(GetDisplayId(), ": subscribing to tick event");
-  _on_tick_register_id = GetEpisode().Lock()->RegisterOnTickEvent(
-      [cb = std::move(callback), weak_self = WeakPtr<RssSensor>(self)](const auto &snapshot) {
-        auto self = weak_self.lock();
-        if (self != nullptr) {
-          auto data = self->TickRssSensor(snapshot.GetTimestamp());
-          if (data != nullptr) {
-            cb(std::move(data));
-          }
-        }
-      });
+  _on_tick_register_id = GetEpisode().Lock()->RegisterOnTickEvent([
+    cb = std::move(callback), weak_self = WeakPtr<RssSensor>(self)
+  ](const auto &snapshot) {
+    auto self = weak_self.lock();
+    if (self != nullptr) {
+      auto data = self->TickRssSensor(snapshot.GetTimestamp());
+      if (data != nullptr) {
+        cb(std::move(data));
+      }
+    }
+  });
 }
 
 void RssSensor::Stop() {
@@ -84,35 +84,42 @@ void RssSensor::Stop() {
   ::ad::map::access::cleanup();
 }
 
-const ::ad::rss::world::RssDynamics& RssSensor::GetEgoVehicleDynamics() const {
+const ::ad::rss::world::RssDynamics &RssSensor::GetEgoVehicleDynamics() const {
   return _rss_check->GetEgoVehicleDynamics();
 }
 
-void RssSensor::SetEgoVehicleDynamics(const ::ad::rss::world::RssDynamics &ego_dynamics) {
+void RssSensor::SetEgoVehicleDynamics(
+    const ::ad::rss::world::RssDynamics &ego_dynamics) {
   _rss_check->SetEgoVehicleDynamics(ego_dynamics);
 }
 
-const ::ad::rss::world::RssDynamics& RssSensor::GetOtherVehicleDynamics() const {
+const ::ad::rss::world::RssDynamics &RssSensor::GetOtherVehicleDynamics()
+    const {
   return _rss_check->GetOtherVehicleDynamics();
 }
 
-void RssSensor::SetOtherVehicleDynamics(const ::ad::rss::world::RssDynamics &other_vehicle_dynamics) {
+void RssSensor::SetOtherVehicleDynamics(
+    const ::ad::rss::world::RssDynamics &other_vehicle_dynamics) {
   _rss_check->SetOtherVehicleDynamics(other_vehicle_dynamics);
 }
 
-const ::carla::rss::RoadBoundariesMode& RssSensor::GetRoadBoundariesMode() const {
+const ::carla::rss::RoadBoundariesMode &RssSensor::GetRoadBoundariesMode()
+    const {
   return _rss_check->GetRoadBoundariesMode();
 }
 
-void RssSensor::SetRoadBoundariesMode(const ::carla::rss::RoadBoundariesMode &road_boundaries_mode) {
+void RssSensor::SetRoadBoundariesMode(
+    const ::carla::rss::RoadBoundariesMode &road_boundaries_mode) {
   _rss_check->SetRoadBoundariesMode(road_boundaries_mode);
 }
 
-void RssSensor::AppendRoutingTarget(const ::carla::geom::Transform &routing_target) {
+void RssSensor::AppendRoutingTarget(
+    const ::carla::geom::Transform &routing_target) {
   _rss_check->AppendRoutingTarget(routing_target);
 }
 
-const std::vector<::carla::geom::Transform> RssSensor::GetRoutingTargets() const {
+const std::vector<::carla::geom::Transform> RssSensor::GetRoutingTargets()
+    const {
   return _rss_check->GetRoutingTargets();
 }
 
@@ -120,21 +127,23 @@ void RssSensor::ResetRoutingTargets() {
   _rss_check->ResetRoutingTargets();
 }
 
-void RssSensor::SetVisualizationMode(const ::carla::rss::VisualizationMode &visualization_mode) {
+void RssSensor::SetVisualizationMode(
+    const ::carla::rss::VisualizationMode &visualization_mode) {
   _rss_check->SetVisualizationMode(visualization_mode);
 }
 
-const ::carla::rss::VisualizationMode& RssSensor::GetVisualizationMode() const {
+const ::carla::rss::VisualizationMode &RssSensor::GetVisualizationMode() const {
   return _rss_check->GetVisualizationMode();
 }
 
 void RssSensor::DropRoute() {
   // don't execute this immediately as it might break calculations completely
   // postpone to next sensor tick
-  _drop_route=true;
+  _drop_route = true;
 }
 
-SharedPtr<sensor::SensorData> RssSensor::TickRssSensor(const Timestamp &timestamp) {
+SharedPtr<sensor::SensorData> RssSensor::TickRssSensor(
+    const Timestamp &timestamp) {
   try {
     bool result = false;
     ::ad::rss::state::ProperResponse response;
@@ -146,24 +155,29 @@ SharedPtr<sensor::SensorData> RssSensor::TickRssSensor(const Timestamp &timestam
       carla::client::World world = GetWorld();
       SharedPtr<carla::client::ActorList> actors = world.GetActors();
 
-      if ( _drop_route )
-      {
-        _drop_route=false;
+      if (_drop_route) {
+        _drop_route = false;
         _rss_check->DropRoute();
       }
 
       // check all object<->ego pairs with RSS and calculate proper response
-      result = _rss_check->CheckObjects(timestamp, actors, GetParent(), response, acceleration_restriction, rss_state_snapshot,
-                                        ego_dynamics_on_route);
+      result = _rss_check->CheckObjects(
+          timestamp, actors, GetParent(), response, acceleration_restriction,
+          rss_state_snapshot, ego_dynamics_on_route);
       _processing_lock.unlock();
 
       _rss_check->VisualizeResults(world, GetParent());
 
-      spdlog::debug("RssSensor response: T={} S:{}->E:{} DeltaT:{}", timestamp.frame, ego_dynamics_on_route.time_since_epoch_check_start_ms,
+      spdlog::debug("RssSensor response: T={} S:{}->E:{} DeltaT:{}",
+                    timestamp.frame,
+                    ego_dynamics_on_route.time_since_epoch_check_start_ms,
                     ego_dynamics_on_route.time_since_epoch_check_end_ms,
-                    ego_dynamics_on_route.time_since_epoch_check_end_ms-ego_dynamics_on_route.time_since_epoch_check_start_ms);
-      return MakeShared<sensor::data::RssResponse>(timestamp.frame, timestamp.elapsed_seconds, GetTransform(),
-                                                   result, response, acceleration_restriction, rss_state_snapshot, ego_dynamics_on_route);
+                    ego_dynamics_on_route.time_since_epoch_check_end_ms -
+                        ego_dynamics_on_route.time_since_epoch_check_start_ms);
+      return MakeShared<sensor::data::RssResponse>(
+          timestamp.frame, timestamp.elapsed_seconds, GetTransform(), result,
+          response, acceleration_restriction, rss_state_snapshot,
+          ego_dynamics_on_route);
     } else {
       spdlog::debug("RssSensor tick dropped: T={}", timestamp.frame);
       return nullptr;
@@ -172,11 +186,11 @@ SharedPtr<sensor::SensorData> RssSensor::TickRssSensor(const Timestamp &timestam
     /// @todo do we need to unsubscribe the sensor here?
     std::cout << e.what() << std::endl;
     _processing_lock.unlock();
-      spdlog::error("RssSensor tick exception");
+    spdlog::error("RssSensor tick exception");
     return nullptr;
   } catch (...) {
     _processing_lock.unlock();
-      spdlog::error("RssSensor tick exception");
+    spdlog::error("RssSensor tick exception");
     return nullptr;
   }
 }
