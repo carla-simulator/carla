6bcb390d1f0d45c99b81c2ae4b355bf859bacd65
Axel1092 <axellopez92@outlook.com>
2020-04-13 15:43:41 +0200

    Fixed crash when generating signals with an s position equal to the road length.

--- LibCarla/source/carla/road/Map.cpp
+++ LibCarla/source/carla/road/Map.cpp
@@ -240,8 +240,7 @@ namespace road {
     const Road &road = _data.GetRoad(waypoint.road_id);
 
     // check the 's' distance
-    if (s < 0.0f || s >= road.GetLength())
-    {
+    if (s < 0.0f || s >= road.GetLength()) {
       return boost::optional<Waypoint>{};
     }
 
