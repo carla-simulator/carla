07ff7f3ad1b330bec714a3280544dd2f492f4a89
bernat <bernatx@gmail.com>
2020-04-21 14:39:39 +0200

    Better build script

--- Util/DockerUtils/dist/build.sh
+++ Util/DockerUtils/dist/build.sh
@@ -1,21 +1,18 @@
 #! /bin/bash
 
-# check if the FBX file exist
-if [ ! -f "$1.fbx" ]; then
-    echo Nothing to do: there is no FBX file
-    exit
+if [ -f "crosswalks.obj" ]; then
+    rm crosswalks.obj
 fi
 
-# prepare to load .SO from current folder
-export LD_LIBRARY_PATH="./"
-# convert FBX to OBJ
-chmod +x FBX2OBJ
-./FBX2OBJ "$1.fbx" "$1.obj"
-
-# check if the OBJ file exist
-if [ ! -f "$1.obj" ]; then
-    echo Nothing to do: conversion from FBX to OBJ failed
-    exit
+# check if the FBX file exist
+if [ -f "FBX2OBJ" ]; then
+    if [ -f "$1.fbx" ]; then
+        # prepare to load .SO from current folder
+        export LD_LIBRARY_PATH="./"
+        # convert FBX to OBJ
+        chmod +x FBX2OBJ
+        ./FBX2OBJ "$1.fbx" "$1.obj"
+    fi
 fi
 
 # check if the XODR file exist
@@ -30,11 +27,12 @@ fi
 if [ -f "crosswalks.obj" ]; then
     # join both OBJ
     python addOBJ.py "$1.obj" crosswalks.obj
-    rm crosswalks.obj
 fi
 
-# calculate the BIN file (result is same name .BIN)
-chmod +x RecastBuilder
-./RecastBuilder "$1.obj"
-
-echo Success!
+if [ -f "$1.obj" ]; then
+    if [ -f "RecastBuilder" ]; then
+        # calculate the BIN file (result is same name .BIN)
+        chmod +x RecastBuilder
+        ./RecastBuilder "$1.obj"
+    fi
+fi
