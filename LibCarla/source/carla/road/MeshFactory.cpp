1f88df9df91cde033052e4a7bc7df10aa2362229
Marc Garcia Puig <marcgpuig@gmail.com>
2020-05-28 15:43:41 +0200

    Fixed MeshFactory circular dependency

--- LibCarla/source/carla/road/MeshFactory.cpp
+++ LibCarla/source/carla/road/MeshFactory.cpp
@@ -4,7 +4,7 @@
 // This work is licensed under the terms of the MIT license.
 // For a copy, see <https://opensource.org/licenses/MIT>.
 
-#include <carla/geom/MeshFactory.h>
+#include <carla/road/MeshFactory.h>
 
 #include <vector>
 
