399c245a4ba50ecef20d81ef4c78a85d992f1a3f
Daniel Santos-Olivan <dsantosolivan@gmail.com>
2020-07-15 17:24:43 +0200

    Added data/serializer structures for RayLidar

--- LibCarla/source/carla/sensor/SensorRegistry.h
+++ LibCarla/source/carla/sensor/SensorRegistry.h
@@ -20,6 +20,7 @@
 #include "carla/sensor/s11n/GnssSerializer.h"
 #include "carla/sensor/s11n/ImageSerializer.h"
 #include "carla/sensor/s11n/IMUSerializer.h"
+#include "carla/sensor/s11n/LidarRawSerializer.h"
 #include "carla/sensor/s11n/LidarSerializer.h"
 #include "carla/sensor/s11n/NoopSerializer.h"
 #include "carla/sensor/s11n/ObstacleDetectionEventSerializer.h"
@@ -60,7 +61,7 @@ namespace sensor {
     std::pair<ALaneInvasionSensor *, s11n::NoopSerializer>,
     std::pair<AObstacleDetectionSensor *, s11n::ObstacleDetectionEventSerializer>,
     std::pair<ARadar *, s11n::RadarSerializer>,
-    std::pair<ARayCastRawLidar *, s11n::LidarSerializer>,
+    std::pair<ARayCastRawLidar *, s11n::LidarRawSerializer>,
     std::pair<ARayCastLidar *, s11n::LidarSerializer>,
     std::pair<ARssSensor *, s11n::NoopSerializer>,
     std::pair<ASceneCaptureCamera *, s11n::ImageSerializer>,
