df84c283d4f2f2b21bcc69e94c7dc2ca8900c47b
Axel1092 <axellopez92@outlook.com>
2020-07-23 15:08:50 +0200

    Added settings class for osm to odr conversion and documentation.

--- PythonAPI/carla/source/libcarla/Converter.cpp
+++ PythonAPI/carla/source/libcarla/Converter.cpp
@@ -8,19 +8,25 @@
 
 class OSM2ODR {
 public:
-  static std::string ConvertOSMToOpenDRIVE(std::string osm_file) {
-    return osm2odr::ConvertOSMToOpenDRIVE(osm_file);
-  }
+  // static std::string ConvertOSMToOpenDRIVE(std::string osm_file, osm2odr::) {
+  //   return osm2odr::ConvertOSMToOpenDRIVE(osm_file);
+  // }
 };
 
 void export_converter() {
   using namespace osm2odr;
   using namespace boost::python;
 
-  // def("convert_to_odr", &ConvertOSMToOpenDRIVE, (arg("osm_file"), arg("offsetX") = 0, arg("offsetY") = 0));
+  class_<OSM2ODRSettings>("OSM2ODRSettings", init<>())
+    .add_property("use_offsets", &OSM2ODRSettings::use_offsets, &OSM2ODRSettings::use_offsets)
+    .add_property("offset_x", &OSM2ODRSettings::offset_x, &OSM2ODRSettings::offset_x)
+    .add_property("offset_y", &OSM2ODRSettings::offset_y, &OSM2ODRSettings::offset_y)
+    .add_property("default_lane_width", &OSM2ODRSettings::default_lane_width, &OSM2ODRSettings::default_lane_width)
+    .add_property("elevation_layer_height", &OSM2ODRSettings::elevation_layer_height, &OSM2ODRSettings::elevation_layer_height)
+  ;
 
-  class_<OSM2ODR>("OSM2ODR", init<>())
-    .def("convert_to_odr", &OSM2ODR::ConvertOSMToOpenDRIVE, (arg("osm_file")))
+  class_<OSM2ODR>("OSM2ODR", no_init)
+    .def("convert_to_odr", &ConvertOSMToOpenDRIVE, (arg("osm_file"), arg("OSM2ODRSettings") = OSM2ODRSettings()))
       .staticmethod("convert_to_odr")
   ;
 }
