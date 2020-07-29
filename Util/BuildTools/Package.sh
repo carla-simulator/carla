2c38e27786d5a9a0ac8d3cd550dcc45c77e730ab
Axel <axellopez92@outlook.com>
2020-07-29 11:32:53 +0200

    Updated package.

--- Util/BuildTools/Package.sh
+++ Util/BuildTools/Package.sh
@@ -145,6 +145,7 @@ if ${DO_CARLA_RELEASE} ; then
   copy_if_changed "./PythonAPI/util/*.py" "${DESTINATION}/PythonAPI/util/"
   copy_if_changed "./PythonAPI/util/opendrive/" "${DESTINATION}/PythonAPI/util/opendrive/"
   copy_if_changed "./PythonAPI/util/requirements.txt" "${DESTINATION}/PythonAPI/util/"
+  copy_if_changed "./PythonAPI/carla/data/*" "${DESTINATION}/PythonAPI/carla/data"
 
   copy_if_changed "./Co-Simulation/" "${DESTINATION}/Co-Simulation/"
 
