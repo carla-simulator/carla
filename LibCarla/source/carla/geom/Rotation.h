c8f5af780dc5d6ec2b4062bbac83ac1d14f7c3b3
doterop <daniel.oterop@gmail.com>
2020-09-14 11:41:09 +0200

    Added rotation to the BBs

--- LibCarla/source/carla/geom/Rotation.h
+++ LibCarla/source/carla/geom/Rotation.h
@@ -87,7 +87,7 @@ namespace geom {
       in_point = out_point;
     }
 
-    Vector3D RotateVector(const Vector3D in_point) const {
+    Vector3D RotateVector(const Vector3D& in_point) const {
       // Rotates Rz(yaw) * Ry(pitch) * Rx(roll) = first x, then y, then z.
       const float cy = std::cos(Math::ToRadians(yaw));
       const float sy = std::sin(Math::ToRadians(yaw));
