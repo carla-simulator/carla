3f886eda8dd8c4c7cd1c9572ccd1058bc9562e13
Praveen Kumar <pravinblaze@hotmail.com>
2020-09-17 19:47:05 +0530

    Implemented parameter for OSM mode.

--- LibCarla/source/carla/trafficmanager/TrafficManagerBase.h
+++ LibCarla/source/carla/trafficmanager/TrafficManagerBase.h
@@ -104,6 +104,9 @@ public:
   /// Method to set hybrid physics radius.
   virtual void SetHybridPhysicsRadius(const float radius) = 0;
 
+  /// Method to set Open Street Map mode.
+  virtual void SetOSMMode(const bool mode_switch) = 0;
+
 protected:
 
 };
