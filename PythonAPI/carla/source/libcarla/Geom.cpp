b2af5db2819e28cae3f225117b84271728d31879
doterop <daniel.oterop@gmail.com>
2020-09-24 23:33:11 +0200

    Retrieve BBs of level (#3360)
    * Added new function to calculate BBs on SKM and SM
    
    * Added BB calculation for an array of actors
    
    * Added BB folliage calculation
    
    * Added a call on the GM to get all the BBs of the level
    
    * Fixed Folliage BB calculation.
    
    * GM::GetAllBBsOfLevel now returns the array of BBs
    
    * Added call on the server side to retrieve the BBs
    
    * Removed debug draw calls
    
    * Added call to the PythonAPI
    
    * Added check for empty folliage actors
    
    * Added missing EOF
    
    * Added new RotateVector function
    
    * Added rotation to the BBs
    
    * Update changelog
    
    * Split BB calculation on more functions
    
    * Add tag query and simplified Folliage actor BB calculation
    
    * Look for ISM instead of HISM
    
    * Cleaning comments
    
    * Fixed BB duplication on BP_Procedural_Buildings
    
    * Fixed BB duplication on vehicles BPs
    
    * Added pedestrians case
    
    * Added CityObjectLabel enum on PythonAPI
    
    * Splitted logic in small functions
    
    * Fixed uint8_t
    
    * Merged BB of the lights in a TL
    
    * Splitted work into more functions
    
    * Added missing BB and rotation to TL
    
    * Cleaned comments
    
    * Added missing include
    
    * Changed None to Any in PythonAPI
    
    * Fixed Any enum
    
    * Added check for empty folliage actors
    
    * Added missing EOF
    
    * Added new RotateVector function
    
    * Added rotation to the BBs
    
    * Update changelog
    
    * Split BB calculation on more functions
    
    * Add tag query and simplified Folliage actor BB calculation
    
    * Look for ISM instead of HISM
    
    * Cleaning comments
    
    * Fixed BB duplication on BP_Procedural_Buildings
    
    * Fixed BB duplication on vehicles BPs
    
    * Added pedestrians case
    
    * Added CityObjectLabel enum on PythonAPI
    
    * Splitted logic in small functions
    
    * Fixed uint8_t
    
    * Merged BB of the lights in a TL
    
    * Splitted work into more functions
    
    * Added missing BB and rotation to TL
    
    * Cleaned comments
    
    * Changed None to Any in PythonAPI
    
    * Fixed Any enum
    
    * Fixed rebase
    
    * Fixing wrong BB on splines
    
    * Fixed ISM transform to World
    
    * Fixed duplication of RotateVector
    
    * Added Rotation to BB output string
    
    * Removed hidden components and procedural building
    
    * Fixed string BB output
    
    * Removed logs
    
    * Fixed filter on pedestrians
    
    * Fixed black debug lines in package
    
    Co-authored-by: Marc Garcia Puig <marcgpuig@gmail.com>

--- PythonAPI/carla/source/libcarla/Geom.cpp
+++ PythonAPI/carla/source/libcarla/Geom.cpp
@@ -65,6 +65,7 @@ namespace geom {
   std::ostream &operator<<(std::ostream &out, const BoundingBox &box) {
     out << "BoundingBox(" << box.location << ", ";
     WriteVector3D(out, "Extent", box.extent);
+    out << ", " << box.rotation;
     out << ')';
     return out;
   }
