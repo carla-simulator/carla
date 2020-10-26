9f280caa4b618896cf8859bb1dfacecdc8afeef6
doterop <daniel.oterop@gmail.com>
2020-10-26 11:03:53 +0100

    Added missing file

--- LibCarla/source/carla/rpc/MapLayer.h
+++ LibCarla/source/carla/rpc/MapLayer.h
@@ -15,16 +15,16 @@ namespace rpc {
 
 enum class MapLayer : uint8_t
 {
-  None          =  0,
-  Buildings     =  0x1,
-  Decals        =  0x1 << 1,
-  Foliage       =  0x1 << 2,
-  Ground        =  0x1 << 3,
-  ParkedCars    =  0x1 << 4,
-  Props         =  0x1 << 5,
-  StreetLights  =  0x1 << 6,
-  Walls         =  0x1 << 7,
-  All           =  0xFF
+  None            =  0,
+  Buildings       =  0x1,
+  Decals          =  0x1 << 1,
+  Foliage         =  0x1 << 2,
+  Ground          =  0x1 << 3,
+  ParkedVehicles  =  0x1 << 4,
+  Props           =  0x1 << 5,
+  StreetLights    =  0x1 << 6,
+  Walls           =  0x1 << 7,
+  All             =  0xFF
 };
 
 
