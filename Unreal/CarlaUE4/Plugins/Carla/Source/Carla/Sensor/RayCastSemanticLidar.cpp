51490611d9f92a4a9dab35c8b8fb99c4dc6f1b7d
Tobias Völkel <tobias_bruno_oskar.voelkel@porsche.de>
2020-10-29 13:31:02 +0100

    Add horizontal fov property to lidar description

--- Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Sensor/RayCastSemanticLidar.cpp
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Sensor/RayCastSemanticLidar.cpp
@@ -95,7 +95,8 @@ void ARayCastSemanticLidar::SimulateLidar(const float DeltaTime)
 
   const float CurrentHorizontalAngle = carla::geom::Math::ToDegrees(
       SemanticLidarData.GetHorizontalAngle());
-  const float AngleDistanceOfTick = Description.RotationFrequency * 360.0f * DeltaTime;
+  const float AngleDistanceOfTick = Description.RotationFrequency * Description.HorizontalFov 
+      * DeltaTime;
   const float AngleDistanceOfLaserMeasure = AngleDistanceOfTick / PointsToScanWithOneLaser;
 
   ResetRecordedHits(ChannelCount, PointsToScanWithOneLaser);
@@ -106,7 +107,9 @@ void ARayCastSemanticLidar::SimulateLidar(const float DeltaTime)
     for (auto idxPtsOneLaser = 0u; idxPtsOneLaser < PointsToScanWithOneLaser; idxPtsOneLaser++) {
       FHitResult HitResult;
       const float VertAngle = LaserAngles[idxChannel];
-      const float HorizAngle = CurrentHorizontalAngle + AngleDistanceOfLaserMeasure * idxPtsOneLaser;
+      const float HorizAngle = std::fmod(std::fmod(CurrentHorizontalAngle 
+          + AngleDistanceOfLaserMeasure * idxPtsOneLaser, Description.HorizontalFov) 
+          - Description.HorizontalFov / 2, 360.0f);
       const bool PreprocessResult = RayPreprocessCondition[idxChannel][idxPtsOneLaser];
 
       if (PreprocessResult && ShootLaser(VertAngle, HorizAngle, HitResult)) {
@@ -120,7 +123,7 @@ void ARayCastSemanticLidar::SimulateLidar(const float DeltaTime)
   ComputeAndSaveDetections(ActorTransf);
 
   const float HorizontalAngle = carla::geom::Math::ToRadians(
-      std::fmod(CurrentHorizontalAngle + AngleDistanceOfTick, 360.0f));
+      std::fmod(CurrentHorizontalAngle + AngleDistanceOfTick, Description.HorizontalFov));
   SemanticLidarData.SetHorizontalAngle(HorizontalAngle);
 }
 
