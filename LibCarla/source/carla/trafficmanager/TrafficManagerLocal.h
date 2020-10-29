5d27391f462533c39fdcf22b9b63be94dd925971
Praveen Kumar <pravinblaze@hotmail.com>
2020-10-29 15:16:28 +0530

    Registration lock while frame re-allocation

--- LibCarla/source/carla/trafficmanager/TrafficManagerLocal.h
+++ LibCarla/source/carla/trafficmanager/TrafficManagerLocal.h
@@ -8,6 +8,7 @@
 
 #include <atomic>
 #include <chrono>
+#include <mutex>
 #include <thread>
 #include <vector>
 
@@ -115,6 +116,8 @@ private:
   /// Randomization seed.
   uint64_t seed {static_cast<uint64_t>(time(NULL))};
   std::vector<ActorId> marked_for_removal;
+  /// Mutex to prevent vehicle registration during frame array re-allocation.
+  std::mutex registration_mutex;
 
   /// Method to check if all traffic lights are frozen in a group.
   bool CheckAllFrozen(TLGroup tl_to_freeze);
