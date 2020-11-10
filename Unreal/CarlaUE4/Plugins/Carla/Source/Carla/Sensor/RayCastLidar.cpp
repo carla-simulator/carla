a73f5f3289f30e3e68de98f120a67251c3a1c37b
Daniel Santos-Oliván <dsantosolivan@gmail.com>
2020-11-10 17:37:24 +0100

    Fix build in windows

--- Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Sensor/RayCastLidar.cpp
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Sensor/RayCastLidar.cpp
@@ -99,8 +99,8 @@ ARayCastLidar::FDetection ARayCastLidar::ComputeDetection(const FHitResult& HitI
   void ARayCastLidar::PreprocessRays(uint32_t Channels, uint32_t MaxPointsPerChannel) {
     Super::PreprocessRays(Channels, MaxPointsPerChannel);
 
-    for (auto ch = 0; ch < Channels; ch++) {
-      for (auto p = 0; p < MaxPointsPerChannel; p++) {
+    for (auto ch = 0u; ch < Channels; ch++) {
+      for (auto p = 0u; p < MaxPointsPerChannel; p++) {
         RayPreprocessCondition[ch][p] = !(DropOffGenActive && RandomEngine->GetUniformFloat() < Description.DropOffGenRate);
       }
     }
