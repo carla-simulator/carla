f5ec8d8d70a848a304787289f4db651ac14e4344
doterop <daniel.oterop@gmail.com>
2020-12-09 15:22:55 +0100

    Corrected header

--- Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Sensor/BenchmarkSensor.cpp
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Sensor/BenchmarkSensor.cpp
@@ -1,5 +1,6 @@
-// Copyright (c) 2017 Computer Vision Center (CVC) at the Universitat Autonoma de Barcelona (UAB). This work is licensed under the terms of the MIT license. For a copy, see <https://opensource.org/licenses/MIT>.
-
+// Copyright (c) 2017 Computer Vision Center (CVC) at the Universitat Autonoma de Barcelona (UAB).
+// This work is licensed under the terms of the MIT license.
+// For a copy, see <https://opensource.org/licenses/MIT>.
 
 #include "BenchmarkSensor.h"
 
