2cae4e192c1d7e81ebd64ee0d97757b3420e779d
doterop <daniel.oterop@gmail.com>
2021-02-03 15:26:24 +0100

    Updates on FDVector

--- Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Math/DVector.h
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Math/DVector.h
@@ -1,4 +1,8 @@
+// Copyright (c) 2021 Computer Vision Center (CVC) at the Universitat Autonoma de Barcelona (UAB).
+// This work is licensed under the terms of the MIT license.
+// For a copy, see <https://opensource.org/licenses/MIT>.
 
+#pragma once
 
 struct FDVector
 {
@@ -8,6 +12,10 @@ struct FDVector
   double Z = 0.0;
 
   FDVector() : X(0.0), Y(0.0), Z(0.0) {}
+
+  FDVector(float InX, float InY, float InZ) : X(InX), Y(InY), Z(InZ) {}
+
+  FDVector(double InX, double InY, double InZ) : X(InX), Y(InY), Z(InZ) {}
 
   FDVector(const FVector& V) : X(V.X), Y(V.Y), Z(V.Z) {}
 
@@ -25,5 +33,11 @@ struct FDVector
     this->Z = Other.Z;
     return *this;
   }
+
+  FDVector operator+(const FDVector& V) const
+  {
+    return FDVector(X + V.X, Y + V.Y, Z + V.Z);
+  }
+
 
 };
