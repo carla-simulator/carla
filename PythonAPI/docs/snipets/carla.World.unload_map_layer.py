e18054597ae6b15d357294923460bd9de131dc55
corkyw10 <corinnewhite@live.co.uk>
2021-02-12 15:21:31 +0100

    Added extra line to bottom of snippets, updated unload and load map layers snippets to show maps being loaded with differents layers staright away, added info about minimum layout to core maps and Python API warning for load and unload maps.

--- PythonAPI/docs/snipets/carla.World.unload_map_layer.py
+++ PythonAPI/docs/snipets/carla.World.unload_map_layer.py
@@ -1,22 +1,11 @@
-# This recipe toggles off and on several layers in our "_Opt" maps
+# This recipe toggles off several layers in our "_Opt" maps
 
-# Load layered map for Town 01
-world = client.load_world('Town01_Opt')
+# Load town one with minimum layout (roads, sidewalks, traffic lights and traffic signs)
+# as well as buildings and parked vehicles
+world = client.load_world('Town01_Opt', carla.MapLayer.Buildings | carla.MapLayer.ParkedVehicles) 
 
 # Toggle all buildings off
 world.unload_map_layer(carla.MapLayer.Buildings)
 
-# Toggle all foliage off
-world.unload_map_layer(carla.MapLayer.Foliage)
-
 # Toggle all parked vehicles off
 world.unload_map_layer(carla.MapLayer.ParkedVehicles)
-
-# Toggle all buildings on
-world.load_map_layer(carla.MapLayer.Buildings)
-
-# Toggle all foliage on
-world.load_map_layer(carla.MapLayer.Foliage)
-
-# Toggle all parked vehicles on
-world.load_map_layer(carla.MapLayer.ParkedVehicles)
