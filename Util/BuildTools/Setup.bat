30f88cfb735689ac427ce9bdf8cd913befbd223f
Axel1092 <axellopez92@outlook.com>
2021-02-22 09:39:54 +0100

    Removed boost RTTI check.

--- Util/BuildTools/Setup.bat
+++ Util/BuildTools/Setup.bat
@@ -268,9 +268,7 @@ set CMAKE_CONFIG_FILE=%INSTALLATION_DIR%CMakeLists.txt.in
 >>"%CMAKE_CONFIG_FILE%" echo.
 >>"%CMAKE_CONFIG_FILE%" echo if (CMAKE_BUILD_TYPE STREQUAL "Server")
 >>"%CMAKE_CONFIG_FILE%" echo   # Prevent exceptions
-if not %USE_CHRONO% == true (
->>"%CMAKE_CONFIG_FILE%" echo   add_compile_options(/GR-^)
-)
+>>"%CMAKE_CONFIG_FILE%" echo   add_definitions(-DBOOST_TYPE_INDEX_FORCE_NO_RTTI_COMPATIBILITY) 
 >>"%CMAKE_CONFIG_FILE%" echo   add_compile_options(/EHsc)
 >>"%CMAKE_CONFIG_FILE%" echo   add_definitions(-DASIO_NO_EXCEPTIONS)
 >>"%CMAKE_CONFIG_FILE%" echo   add_definitions(-DBOOST_NO_EXCEPTIONS)
