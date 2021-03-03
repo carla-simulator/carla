94648d95c3fa5ba31376d223352e5723ec630fe4
Axel <axellopez92@outlook.com>
2021-03-03 13:15:03 +0100

    TM will stay in deterministic mode after the deterministic test.

--- PythonAPI/test/smoke/test_determinism.py
+++ PythonAPI/test/smoke/test_determinism.py
@@ -130,7 +130,6 @@ class TestDeterminism(SmokeTest):
         record_run2 = self.run_simulation(world, vehicle_actor_list)
 
         self.client.reload_world()
-        traffic_manager.set_synchronous_mode(False)
         world.apply_settings(old_settings)
 
         self.compare_records(record_run1, record_run2)
