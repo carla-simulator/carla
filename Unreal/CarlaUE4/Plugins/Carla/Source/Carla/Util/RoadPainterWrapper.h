954ed13c4894620b4148d43cf43f66105abe4de6
Roel Algaba Brizuela <algababr@esat-alumni.com>
2021-03-29 20:28:38 +0200

    Texture creation and apply material in runtime
    Extended road painter wrapper class in order to create textures statically in the content folder so the road painter blueprint can paint the road map into these new textures and save them, instead of having to share the same texture over several maps. The material is now applied on runtime (as soon as the map is launched, either in package mode or play mode in the editor)

--- Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Util/RoadPainterWrapper.h
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Util/RoadPainterWrapper.h
@@ -7,6 +7,7 @@
 #pragma once
 
 #include "GameFramework/Actor.h"
+#include "Materials/MaterialInstanceConstant.h"
 #include "RoadPainterWrapper.generated.h"
 
 UCLASS()
@@ -18,42 +19,73 @@ public:
 
   ARoadPainterWrapper();
 
-  UFUNCTION(BlueprintImplementableEvent, Category = "ARoadPainterWrapper")
-	  void ZSizeEvent();
+  void BeginPlay() override;
+
+  UFUNCTION(BlueprintImplementableEvent, Category = "RoadPainter Wrapper")
+	void ZSizeEvent();
 
   //Paint events
-  UFUNCTION(BlueprintImplementableEvent, Category = "ARoadPainterWrapper")
-	  void PaintByActorEvent();
+  UFUNCTION(BlueprintImplementableEvent, Category = "RoadPainter Wrapper")
+	void PaintByActorEvent();
 
-  UFUNCTION(BlueprintImplementableEvent, Category = "ARoadPainterWrapper")
-	  void PaintOverSquareEvent();
+  UFUNCTION(BlueprintImplementableEvent, Category = "RoadPainter Wrapper")
+	void PaintOverSquareEvent();
 
-  UFUNCTION(BlueprintImplementableEvent, Category = "ARoadPainterWrapper")
-	  void PaintOverCircleEvent();
+  UFUNCTION(BlueprintImplementableEvent, Category = "RoadPainter Wrapper")
+	void PaintOverCircleEvent();
 
-  UFUNCTION(BlueprintImplementableEvent, Category = "ARoadPainterWrapper")
-	  void PaintAllRoadsEvent();
+  UFUNCTION(BlueprintImplementableEvent, Category = "RoadPainter Wrapper")
+	void PaintAllRoadsEvent();
 
   //Spawn events
-  UFUNCTION(BlueprintImplementableEvent, Category = "ARoadPainterWrapper")
-	  void SpawnMeshesByActorEvent();
+  UFUNCTION(BlueprintImplementableEvent, Category = "RoadPainter Wrapper")
+	void SpawnMeshesByActorEvent();
 
-  UFUNCTION(BlueprintImplementableEvent, Category = "ARoadPainterWrapper")
-	  void SpawnMeshesEvent();
+  UFUNCTION(BlueprintImplementableEvent, Category = "RoadPainter Wrapper")
+	void SpawnMeshesEvent();
 
-  UFUNCTION(BlueprintImplementableEvent, Category = "ARoadPainterWrapper")
-	  void SpawnDecalsEvent();
+  UFUNCTION(BlueprintImplementableEvent, Category = "RoadPainter Wrapper")
+	void SpawnDecalsEvent();
 
   //Clear events
-  UFUNCTION(BlueprintImplementableEvent, Category = "ARoadPainterWrapper")
-	  void ClearMaterialEvent();
+  UFUNCTION(BlueprintImplementableEvent, Category = "RoadPainter Wrapper")
+	void ClearMaterialEvent();
 
-  UFUNCTION(BlueprintImplementableEvent, Category = "ARoadPainterWrapper")
-	  void ClearMaterialByActorEvent();
+  UFUNCTION(BlueprintImplementableEvent, Category = "RoadPainter Wrapper")
+	void ClearMaterialByActorEvent();
 
-  UFUNCTION(BlueprintImplementableEvent, Category = "ARoadPainterWrapper")
-	  void ClearAllEvent();
-	
-  UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "ARoadPainterWrapper")
-	  float MapSize;
+  UFUNCTION(BlueprintImplementableEvent, Category = "RoadPainter Wrapper")
+	void ClearAllEvent();
+
+  UFUNCTION(BlueprintImplementableEvent, Category = "RoadPainter Wrapper")
+  void SetBlueprintVariables();
+
+  UFUNCTION(BlueprintCallable, Category = "RoadPainter Wrapper")
+  void GenerateDynamicAssets();
+
+  UFUNCTION(Category = "RoadPainter Wrapper")
+  void ModifyRoadMaterialParameters();
+
+  UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "RoadPainter Wrapper")
+  UMaterialInstanceDynamic *DynamicRoadMaterial;
+
+  UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "RoadPainter Wrapper")
+  UTextureRenderTarget2D *RoadTexture;
+
+  UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "RoadPainter Wrapper")
+  float MapSize;
+
+  UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "RoadPainter Wrapper")
+  bool bIsRenderedToTexture;
+
+private:
+
+  UFUNCTION(Category = "RoadPainter Wrapper")
+  void GenerateTexture();
+
+  UPROPERTY()
+  UMaterial *RoadNodeMasterMaterial;
+
+  UPROPERTY()
+  UMaterialInstanceConstant *RoadNodePresetMaterial;
 };
