2e588da47f21fe02119f932b8aa12fd6ec4c037a
Roel Algaba Brizuela <algababr@esat-alumni.com>
2021-04-20 17:19:52 +0200

    Correct spaces and tabulations

--- Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Game/CarlaGameModeBase.h
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Game/CarlaGameModeBase.h
@@ -75,7 +75,7 @@ public:
 
   void EnableEnvironmentObjects(const TSet<uint64>& EnvObjectIds, bool Enable);
 
-  void EnableCarSimChronoOverlapEvents();
+  void EnableOverlapEvents();
 
   void CheckForEmptyMeshes();
 
