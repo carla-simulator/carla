0941abc43d9f0811793cd9e57d5a3d806ebbefec
Roel Algaba Brizuela <algababr@esat-alumni.com>
2021-04-20 17:34:07 +0200

    Fixed spaces between lines

--- Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Game/CarlaGameModeBase.cpp
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Game/CarlaGameModeBase.cpp
@@ -343,7 +343,7 @@ void ACarlaGameModeBase::CheckForEmptyMeshes()
 {
   TArray<AActor*> WorldActors;
   UGameplayStatics::GetAllActorsOfClass(GetWorld(), AStaticMeshActor::StaticClass(), WorldActors);
-  
+
   for(AActor *Actor : WorldActors)
   {
     AStaticMeshActor *MeshActor = CastChecked<AStaticMeshActor>(Actor);
@@ -358,7 +358,7 @@ void ACarlaGameModeBase::EnableOverlapEvents()
 {
   TArray<AActor*> WorldActors;
   UGameplayStatics::GetAllActorsOfClass(GetWorld(), AStaticMeshActor::StaticClass(), WorldActors);
-  
+
   for(AActor *Actor : WorldActors)
   {
     AStaticMeshActor *MeshActor = CastChecked<AStaticMeshActor>(Actor);
