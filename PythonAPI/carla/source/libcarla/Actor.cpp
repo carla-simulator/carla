c62b4bb280933625db8305e654a33afbbad87982
Axel <axellopez92@outlook.com>
2021-05-04 09:48:20 +0200

    Added get_stop_waypoints() to traffic light. Renamed affected_lane_waypoints function. Added transform_vector function.

--- PythonAPI/carla/source/libcarla/Actor.cpp
+++ PythonAPI/carla/source/libcarla/Actor.cpp
@@ -222,9 +222,10 @@ void export_actor() {
       .def("get_pole_index", &cc::TrafficLight::GetPoleIndex)
       .def("get_group_traffic_lights", &GetGroupTrafficLights)
       .def("reset_group", &cc::TrafficLight::ResetGroup)
-      .def("get_effect_waypoints", CALL_RETURNING_LIST(cc::TrafficLight, GetEffectPositions))
+      .def("get_affected_lane_waypoints", CALL_RETURNING_LIST(cc::TrafficLight, GetAffectedLaneWaypoints))
       .def("get_light_boxes", &GetLightBoxes)
       .def("get_opendrive_id", &cc::TrafficLight::GetOpenDRIVEID)
+      .def("get_stop_waypoints", CALL_RETURNING_LIST(cc::TrafficLight, GetStopWaypoints))
       .def(self_ns::str(self_ns::self))
   ;
 }
