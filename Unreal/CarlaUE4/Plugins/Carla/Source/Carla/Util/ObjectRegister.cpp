5e002c08e22c44da373f36572cb56e0700fcbe4c
Axel <axellopez92@outlook.com>
2021-06-11 09:36:59 +0200

    Fixed missing includes

--- Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Util/ObjectRegister.cpp
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Util/ObjectRegister.cpp
@@ -10,6 +10,8 @@
 #include "Carla/Game/Tagger.h"
 #include "Carla/Util/BoundingBoxCalculator.h"
 
+#include "GameFramework/Character.h"
+
 #if WITH_EDITOR
 #include "FileHelper.h"
 #include "Paths.h"
