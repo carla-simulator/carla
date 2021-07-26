38a73e68752aff746b9acc264510d687b4ee1ade
Roel Algaba Brizuela <algababr@esat-alumni.com>
2021-07-26 15:55:54 +0200

    Fixed import process fatal error

--- Unreal/CarlaUE4/Plugins/Carla/Source/Carla/OpenDrive/OpenDrive.cpp
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/OpenDrive/OpenDrive.cpp
@@ -10,6 +10,47 @@
 #include "Runtime/Core/Public/HAL/FileManagerGeneric.h"
 
 
+FString UOpenDrive::FindPathToXODRFile(const FString &InMapName){
+
+  FString MapName = InMapName;
+
+#if WITH_EDITOR
+    {
+      // When playing in editor the map name gets an extra prefix, here we
+      // remove it.
+      FString CorrectedMapName = MapName;
+      constexpr auto PIEPrefix = TEXT("UEDPIE_0_");
+      CorrectedMapName.RemoveFromStart(PIEPrefix);
+      MapName = CorrectedMapName;
+    }
+#endif // WITH_EDITOR
+
+  MapName += TEXT(".xodr");
+
+  const FString DefaultFilePath =
+      FPaths::ProjectContentDir() +
+      TEXT("Carla/Maps/OpenDrive/") +
+      MapName;
+
+  auto &FileManager = IFileManager::Get();
+
+  if (FileManager.FileExists(*DefaultFilePath))
+  {
+    return DefaultFilePath;
+  }
+
+  TArray<FString> FilesFound;
+  FileManager.FindFilesRecursive(
+      FilesFound,
+      *FPaths::ProjectContentDir(),
+      *MapName,
+      true,
+      false,
+      false);
+
+  return FilesFound.Num() > 0 ? FilesFound[0u] : FString{};
+}
+
 FString UOpenDrive::GetXODR(const UWorld *World)
 {
   auto MapName = World->GetMapName();
@@ -51,10 +92,67 @@ FString UOpenDrive::GetXODR(const UWorld *World)
   return Content;
 }
 
-UOpenDriveMap *UOpenDrive::LoadOpenDriveMap(const UWorld *World)
+FString UOpenDrive::LoadXODR(const FString &MapName)
+{
+  const auto FilePath = FindPathToXODRFile(MapName);
+
+  FString Content;
+
+  if (FilePath.IsEmpty())
+  {
+    UE_LOG(LogTemp, Error, TEXT("Failed to find OpenDrive file for map '%s'"), *MapName);
+  }
+  else if (FFileHelper::LoadFileToString(Content, *FilePath))
+  {
+    UE_LOG(LogTemp, Log, TEXT("Loaded OpenDrive file '%s'"), *FilePath);
+  }
+  else
+  {
+    UE_LOG(LogTemp, Error, TEXT("Failed to load OpenDrive file '%s'"), *FilePath);
+  }
+
+  return Content;
+}
+
+FString UOpenDrive::GetXODRByPath(FString XODRPath, FString MapName){
+
+  // When playing in editor the map name gets an extra prefix, here we
+  // remove it.
+  #if WITH_EDITOR
+  {
+    FString CorrectedMapName = MapName;
+    constexpr auto PIEPrefix = TEXT("UEDPIE_0_");
+    CorrectedMapName.RemoveFromStart(PIEPrefix);
+    MapName = CorrectedMapName;
+  }
+  #endif // WITH_EDITOR
+
+  FString FileName = XODRPath.EndsWith(MapName) ? "*" : MapName;
+  FString FolderDir = XODRPath;
+  FolderDir.RemoveFromEnd(MapName + ".xodr");
+
+  // Find all the xodr and bin files from the map
+  TArray<FString> Files;
+  IFileManager::Get().FindFilesRecursive(Files, *FolderDir, *FString(FileName + ".xodr"), true, false, false);
+
+  FString Content;
+
+  if (!Files.Num())
+  {
+    UE_LOG(LogTemp, Error, TEXT("Failed to find OpenDrive file for map '%s'"), *MapName);
+  }
+  else if (FFileHelper::LoadFileToString(Content, *Files[0]))
+  {
+    UE_LOG(LogTemp, Log, TEXT("Loaded OpenDrive file '%s'"), *Files[0]);
+  }
+
+  return Content;
+}
+
+UOpenDriveMap *UOpenDrive::LoadOpenDriveMap(const FString &MapName)
 {
   UOpenDriveMap *Map = nullptr;
-  auto XODRContent = GetXODR(World);
+  auto XODRContent = LoadXODR(MapName);
   if (!XODRContent.IsEmpty())
   {
     Map = NewObject<UOpenDriveMap>();
@@ -66,5 +164,7 @@ UOpenDriveMap *UOpenDrive::LoadOpenDriveMap(const UWorld *World)
 UOpenDriveMap *UOpenDrive::LoadCurrentOpenDriveMap(const UObject *WorldContextObject)
 {
   UWorld *World = GEngine->GetWorldFromContextObject(WorldContextObject, EGetWorldErrorMode::LogAndReturnNull);
-  return World != nullptr ? LoadOpenDriveMap(World) : nullptr;
+  return World != nullptr ?
+      LoadOpenDriveMap(World->GetMapName()) :
+      nullptr;
 }
