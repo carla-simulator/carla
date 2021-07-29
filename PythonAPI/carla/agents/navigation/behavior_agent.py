849f3b29f20685c9a9034daefe4de447fea8d39e
Guillermo <glopez@cvc.uab.es>
2021-07-29 11:40:25 +0200

    Added missing function

--- PythonAPI/carla/agents/navigation/behavior_agent.py
+++ PythonAPI/carla/agents/navigation/behavior_agent.py
@@ -406,3 +406,16 @@ class BehaviorAgent(BasicAgent):
         control = self._local_planner.run_step(debug=debug)
 
         return control
+
+    def emergency_stop(self):
+        """
+        Overwrites the throttle a brake values of a control to perform an emergency stop.
+        The steering is kept the same to avoid going out of the lane when stopping during turns
+
+            :param speed (carl.VehicleControl): control to be modified
+        """
+        control = carla.VehicleControl()
+        control.throttle = 0.0
+        control.brake = self._max_brake
+        control.hand_brake = False
+        return control
