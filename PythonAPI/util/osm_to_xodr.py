0896fed5c1dc3547e428d67f5864fd14224649aa
Hita Barasm Mamagani <barasm.hita@outlook.com>
2021-09-12 14:04:34 +0430

    Added sample OSM road and converter file

---
+++ PythonAPI/util/osm_to_xodr.py
@@ -0,0 +1,19 @@
+import io
+import carla
+
+# Read the .osm data
+f = io.open("../examples/map.osm", mode="r", encoding="utf-8")
+osm_data = f.read()
+f.close()
+
+# Define the desired settings. In this case, default values.
+settings = carla.Osm2OdrSettings()
+# Set OSM road types to export to OpenDRIVE
+settings.set_osm_way_types(["motorway", "motorway_link", "trunk", "trunk_link", "primary", "primary_link", "secondary", "secondary_link", "tertiary", "tertiary_link", "unclassified", "residential"])
+# Convert to .xodr
+xodr_data = carla.Osm2Odr.convert(osm_data, settings)
+
+# save opendrive file
+f = open("../examples/map.xodr", 'w', encoding="utf-8")
+f.write(xodr_data)
+f.close()
