3e8ca945a28379c58cd21f29242cd7c352b22faa
Hita Barasm Mamagani <barasm.hita@outlook.com>
2021-09-24 12:21:38 +0330

    Some code improvements for Pylint

--- PythonAPI/util/osm_to_xodr.py
+++ PythonAPI/util/osm_to_xodr.py
@@ -1,20 +1,29 @@
-import io
+""" Convert OpenStreetMap file to OpenDRIVE file. """
 import carla
 
 # Read the .osm data
-f = io.open("examples/map.osm", mode="r", encoding="utf-8")
-osm_data = f.read()
-f.close()
+with open("examples/map.osm", mode="r", encoding="utf-8") as osmFile:
+    osm_data = osmFile.read()
 
 # Define the desired settings. In this case, default values.
 settings = carla.Osm2OdrSettings()
 # Set OSM road types to export to OpenDRIVE
-settings.set_osm_way_types(["motorway", "motorway_link", "trunk", "trunk_link", "primary", "primary_link", "secondary", "secondary_link", "tertiary", "tertiary_link", "unclassified", "residential"])
+settings.set_osm_way_types(["motorway",
+                            "motorway_link",
+                            "trunk",
+                            "trunk_link",
+                            "primary",
+                            "primary_link",
+                            "secondary",
+                            "secondary_link",
+                            "tertiary",
+                            "tertiary_link",
+                            "unclassified",
+                            "residential"])
 settings.default_lane_width = 6.0
 # Convert to .xodr
 xodr_data = carla.Osm2Odr.convert(osm_data, settings)
 
 # save opendrive file
-f = open("examples/map.xodr", 'w', encoding="utf-8")
-f.write(xodr_data)
-f.close()
+with open("examples/map.xodr", "w", encoding="utf-8") as xodrFile:
+    xodrFile.write(xodr_data)
