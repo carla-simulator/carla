4b61e7c76d4f3d742dd9df6f59c5241a7dccd5f3
Hita Barasm Mamagani <barasm.hita@outlook.com>
2021-10-11 17:26:37 +0330

    New python examples for spawn point extraction and OSM2XODR conversion

---
+++ PythonAPI/util/spawn_points.py
@@ -0,0 +1,71 @@
+""" CARLA map spawn points extractor """
+
+from __future__ import print_function
+import sys
+import argparse
+import logging
+import carla
+
+
+def extract(args):
+    try:
+        client = carla.Client(args.host, args.port, worker_threads=1)
+        client.set_timeout(2.0)
+
+        world = client.get_world()
+        try:
+            _map = world.get_map()
+        except RuntimeError as error:
+            logging.info('RuntimeError: %s', error)
+            sys.exit(1)
+
+        if not _map.get_spawn_points():
+            logging.info('There are no spawn points available in your map/town.')
+            logging.info('Please add some Vehicle Spawn Point to your UE4 scene.')
+            sys.exit(1)
+        spawn_points = _map.get_spawn_points()
+        with open("spawn_points.csv", "w", encoding='utf8') as file:
+            index = 0
+            for index, spawn_point in enumerate(spawn_points):
+                file.write(f'{index},{spawn_point.location.x},{spawn_point.location.y}\n')
+
+    finally:
+        world = None
+
+# ==============================================================================
+# -- main() --------------------------------------------------------------------
+# ==============================================================================
+
+
+def main():
+    argparser = argparse.ArgumentParser(
+        description='CARLA map spawn points extractor')
+    argparser.add_argument(
+        '--host',
+        metavar='H',
+        default='127.0.0.1',
+        help='IP of the host server (default: 127.0.0.1)')
+    argparser.add_argument(
+        '-p', '--port',
+        metavar='P',
+        default=2000,
+        type=int,
+        help='TCP port to listen to (default: 2000)')
+    args = argparser.parse_args()
+
+    logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
+
+    logging.info('listening to server %s:%s', args.host, args.port)
+
+    print(__doc__)
+
+    try:
+        extract(args)
+
+    except KeyboardInterrupt:
+        print('\nCancelled by user. Bye!')
+
+
+if __name__ == '__main__':
+
+    main()
