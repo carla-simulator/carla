f378b9336c78a8b24e7a33641f4e4db36e0c234d
Gustavo Silvera <grs5382@gmail.com>
2022-02-26 18:35:20 -0500

    Build fix for incomplete type error

--- Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Traffic/YieldSignComponent.cpp
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Traffic/YieldSignComponent.cpp
@@ -6,10 +6,12 @@
 
 #include "YieldSignComponent.h"
 #include "TrafficLightState.h"
+#include "Vehicle/CarlaWheeledVehicle.h"
 #include <queue>
 
 #include <compiler/disable-ue4-macros.h>
 #include <carla/road/element/RoadInfoSpeed.h>
+#include <carla/road/element/RoadInfoSignal.h>
 #include <compiler/enable-ue4-macros.h>
 
 void UYieldSignComponent::InitializeSign(const carla::road::Map &Map)
