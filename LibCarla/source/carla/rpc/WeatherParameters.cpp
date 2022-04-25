77470405da5c1236c2bfb047ef1474e095b3b722
Marcel Pi <marcelpi97@gmail.com>
2022-04-25 12:18:37 +0200

    Fix minor DustStorm preset error.

--- LibCarla/source/carla/rpc/WeatherParameters.cpp
+++ LibCarla/source/carla/rpc/WeatherParameters.cpp
@@ -34,7 +34,7 @@ namespace rpc {
   WP WP::SoftRainNight   = {    60.0f,   30.0f,    50.0f,    30.0f,    -1.0f,    -90.0f,    60.0f,     0.75f,     0.1f,   60.0f,   1.0f,   0.03f,   0.0331f,            0.0f };
   WP WP::MidRainyNight   = {    80.0f,   60.0f,    60.0f,    60.0f,    -1.0f,    -90.0f,    60.0f,     0.75f,     0.1f,   80.0f,   1.0f,   0.03f,   0.0331f,            0.0f };
   WP WP::HardRainNight   = {   100.0f,  100.0f,    90.0f,    100.0f,   -1.0f,    -90.0f,   100.0f,     0.75f,     0.1f,  100.0f,   1.0f,   0.03f,   0.0331f,            0.0f };
-  WP WP::DustStorm       = {   100.0f,     0.0f,    0.0f,    100.0f,   -1.0f,     45.0f,     2.0f,     0.75f,     0.1f,    0.0f,   1.0f,   0.03f,   0.0331f,            1.0f  };
+  WP WP::DustStorm       = {   100.0f,     0.0f,    0.0f,    100.0f,   -1.0f,     45.0f,     2.0f,     0.75f,     0.1f,    0.0f,   1.0f,   0.03f,   0.0331f,            100.0f  };
 
 } // namespace rpc
 } // namespace carla
