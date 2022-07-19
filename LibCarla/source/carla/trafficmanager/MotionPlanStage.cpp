480c50e851cb113f64267f0146a2400dbbb4240b
Joel Moriana <joel.moriana@gmail.com>
2022-07-19 11:33:25 +0200

    Added lane offset parameter

--- LibCarla/source/carla/trafficmanager/MotionPlanStage.cpp
+++ LibCarla/source/carla/trafficmanager/MotionPlanStage.cpp
@@ -159,7 +159,13 @@ void MotionPlanStage::Update(const unsigned long index) {
       const float target_point_distance = std::max(vehicle_speed * TARGET_WAYPOINT_TIME_HORIZON,
                                                   MIN_TARGET_WAYPOINT_DISTANCE);
       const SimpleWaypointPtr &target_waypoint = GetTargetWaypoint(waypoint_buffer, target_point_distance).first;
-      const cg::Location target_location = target_waypoint->GetLocation();
+      cg::Location target_location = target_waypoint->GetLocation();
+      
+      float offset = parameters.GetLaneOffset(actor_id);
+      auto right_vector = target_waypoint->GetTransform().GetRightVector();
+      auto offset_location = cg::Location(cg::Vector3D(offset*right_vector.x, offset*right_vector.y, 0.0f));
+      target_location = target_location + offset_location;
+      
       float dot_product = DeviationDotProduct(vehicle_location, vehicle_heading, target_location);
       float cross_product = DeviationCrossProduct(vehicle_location, vehicle_heading, target_location);
       dot_product = acos(dot_product) / PI;
