3ea02b06b5cd27b452c742ad07ccf9bd662dcd09
bernatx <bernatx@gmail.com>
2022-09-28 00:46:06 +0200

    Adding new attachment type 'SpringArmGhost'

--- Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Util/ActorAttacher.cpp
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Util/ActorAttacher.cpp
@@ -51,6 +51,48 @@ static void UActorAttacher_AttachActorsWithSpringArm(
   ChildComp->RegisterComponent();
 }
 
+static void UActorAttacher_AttachActorsWithSpringArmGhost(
+    AActor *Child,
+    AActor *Parent)
+{
+  auto SpringArm = NewObject<USpringArmComponent>(Parent);
+
+  // Child location negated to compensate for the spring arm rotation (rotated
+  // from the "other end" of the arm).
+  const auto ChildLocation = -Child->GetActorLocation();
+  Child->SetActorLocation(FVector::ZeroVector);
+
+  // Adding Z-offset to avoid colliding against the ground on bumpy terrain.
+  SpringArm->TargetOffset = FVector(0.0f, 0.0f, 0.0f);
+  SpringArm->bDoCollisionTest = false;
+
+  FRotator LookAt = FRotationMatrix::MakeFromX(ChildLocation).Rotator();
+  SpringArm->SetRelativeRotation(LookAt);
+  SpringArm->SetupAttachment(Parent->GetRootComponent());
+
+  SpringArm->TargetArmLength = ChildLocation.Size();
+  SpringArm->bEnableCameraRotationLag = true;
+  SpringArm->CameraRotationLagSpeed = 8.0f;
+
+  SpringArm->bInheritPitch = false;
+  SpringArm->bInheritRoll = false;
+  SpringArm->bInheritYaw = true;
+
+  SpringArm->AttachToComponent(
+      Parent->GetRootComponent(),
+      FAttachmentTransformRules::KeepRelativeTransform);
+  SpringArm->RegisterComponent();
+
+  auto ChildComp = NewObject<UChildActorComponent>(Parent);
+  ChildComp->SetupAttachment(
+      SpringArm,
+      USpringArmComponent::SocketName);
+  Child->AttachToComponent(
+      ChildComp,
+      FAttachmentTransformRules::KeepRelativeTransform);
+  ChildComp->RegisterComponent();
+}
+
 void UActorAttacher::AttachActors(
     AActor *Child,
     AActor *Parent,
@@ -66,6 +108,9 @@ void UActorAttacher::AttachActors(
       break;
     case EAttachmentType::SpringArm:
       UActorAttacher_AttachActorsWithSpringArm(Child, Parent);
+      break;
+    case EAttachmentType::SpringArmGhost:
+      UActorAttacher_AttachActorsWithSpringArmGhost(Child, Parent);
       break;
     default:
       UE_LOG(LogCarla, Fatal, TEXT("Invalid attachment type"));
