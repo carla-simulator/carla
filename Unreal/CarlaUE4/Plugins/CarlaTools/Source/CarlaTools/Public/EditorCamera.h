1b5760d02cc2a84b7a1180712da9c5d670971665
Marcel Pi <marcelpi97@gmail.com>
2023-05-12 12:01:54 +0200

    Add copyright and fix indentation.

--- Unreal/CarlaUE4/Plugins/CarlaTools/Source/CarlaTools/Public/EditorCamera.h
+++ Unreal/CarlaUE4/Plugins/CarlaTools/Source/CarlaTools/Public/EditorCamera.h
@@ -1,3 +1,9 @@
+// Copyright (c) 2023 Computer Vision Center (CVC) at the Universitat Autonoma
+// de Barcelona (UAB).
+//
+// This work is licensed under the terms of the MIT license.
+// For a copy, see <https://opensource.org/licenses/MIT>.
+
 #pragma once
 #include "CoreMinimal.h"
 #include "EditorCamera.generated.h"
@@ -7,20 +13,20 @@
 
 UCLASS(BlueprintType)
 class CARLATOOLS_API AEditorCameraUtils :
-	public AActor
+    public AActor
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
 public:
 
-	UFUNCTION(BlueprintCallable, CallInEditor)
-	void Get();
+    UFUNCTION(BlueprintCallable, CallInEditor)
+    void Get();
 
-	UFUNCTION(BlueprintCallable, CallInEditor)
-	void Set();
+    UFUNCTION(BlueprintCallable, CallInEditor)
+    void Set();
 
 
 
-	UPROPERTY(BlueprintReadWrite, EditAnywhere)
-	FTransform CameraTransform;
+    UPROPERTY(BlueprintReadWrite, EditAnywhere)
+    FTransform CameraTransform;
 
 };
