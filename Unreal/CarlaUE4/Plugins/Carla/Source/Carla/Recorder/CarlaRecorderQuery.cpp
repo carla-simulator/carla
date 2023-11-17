a227306ee9ced5158d44c26f12631c35f0186a6e
javiergrCS <jgr.cs202x@gmail.com>
2023-11-17 10:14:18 +0100

    Last correction

--- Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Recorder/CarlaRecorderQuery.cpp
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Recorder/CarlaRecorderQuery.cpp
@@ -19,6 +19,7 @@
 #include <carla/rpc/VehiclePhysicsControl.h>
 #include <compiler/enable-ue4-macros.h>
 
+
 #include <Carla/Vehicle/CarlaWheeledVehicle.h>
 
 inline bool CarlaRecorderQuery::ReadHeader(void)
