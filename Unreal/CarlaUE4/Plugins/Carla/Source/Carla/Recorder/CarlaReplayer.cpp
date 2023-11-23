c461f901cf5e76357b7ef1da0da0c20b5fb584d9
javiergrCS <139075626+javiergrCS@users.noreply.github.com>
2023-11-23 09:36:43 +0100

    Javiergr cs/recorder doors (#6922)
    * Fix a typo in setting the max brake
    
    From self._max_steer to self._max_brake
    
    * Fix example commands in Multi-GPU docs
    
    Only one dash before `-nullrhi` argument
    
    * Correct incoherent structuring of tutorial for adding sensor to CARLA.
    
    * Add contribution to CHANGELOG.md
    
    * Add notes into breakout boxes in sensor create tutorial.
    
    * Update build_linux.md
    
    Change pseudopath to linux format
    
    * Update build_linux.md
    
    The same change 1 line above
    
    * Fixing Recast pulling by branch instead of hash id
    
    * Added functionality to the recorder to add door state info
    
    * Final update
    
    * Correction CarlaRecorder.h
    
    * Include correction CarlaRecorderQuery.cpp
    
    * Last correction
    
    * Changelog update
    
    * Last corrections
    
    ---------
    
    Co-authored-by: kykim0 <kykim144@gmail.com>
    Co-authored-by: bernatx <bernatx@gmail.com>
    Co-authored-by: Paul Erik Frivold <paulerikf@gmail.com>
    Co-authored-by: Balázs Kis <balazs_kis35@yahoo.com>
    Co-authored-by: matejm42 <116560704+matejm42@users.noreply.github.com>
    Co-authored-by: Blyron <53337103+Blyron@users.noreply.github.com>

--- Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Recorder/CarlaReplayer.cpp
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Recorder/CarlaReplayer.cpp
@@ -378,6 +378,14 @@ void CarlaReplayer::ProcessToTime(double Time, bool IsFirstTime)
           SkipPacket();
         break;
 
+      // vehicle door animation
+      case static_cast<char>(CarlaRecorderPacketId::VehicleDoor):
+        if (bFrameFound)
+          ProcessDoorVehicle();
+        else
+          SkipPacket();
+        break;
+
       // scene lights animation
       case static_cast<char>(CarlaRecorderPacketId::SceneLight):
         if (bFrameFound)
@@ -645,6 +653,25 @@ void CarlaReplayer::ProcessLightVehicle(void)
     if (!(IgnoreHero && IsHeroMap[LightVehicle.DatabaseId]))
     {
       Helper.ProcessReplayerLightVehicle(LightVehicle);
+    }
+  }
+}
+
+void CarlaReplayer::ProcessDoorVehicle(void)
+{
+  uint16_t Total;
+  CarlaRecorderDoorVehicle DoorVehicle;
+
+  // read Total walkers
+  ReadValue<uint16_t>(File, Total);
+  for (uint16_t i = 0; i < Total; ++i)
+  {
+    DoorVehicle.Read(File);
+    DoorVehicle.DatabaseId = MappedId[DoorVehicle.DatabaseId];
+    // check if ignore this actor
+    if (!(IgnoreHero && IsHeroMap[DoorVehicle.DatabaseId]))
+    {
+      Helper.ProcessReplayerDoorVehicle(DoorVehicle);
     }
   }
 }
