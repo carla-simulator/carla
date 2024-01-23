ba1c22fc3df29daa12e34d65f63cbbe08edbc1b6
Marcel Pi <25649656+MarcelPiNacy@users.noreply.github.com>
2024-01-23 12:39:20 +0100

    Update copyright.

--- LibCarla/source/carla/client/Light.cpp
+++ LibCarla/source/carla/client/Light.cpp
@@ -1,5 +1,5 @@
 
-// Copyright (c) 2023 Computer Vision Center (CVC) at the Universitat Autonoma
+// Copyright (c) 2024 Computer Vision Center (CVC) at the Universitat Autonoma
 // de Barcelona (UAB).
 //
 // This work is licensed under the terms of the MIT license.
