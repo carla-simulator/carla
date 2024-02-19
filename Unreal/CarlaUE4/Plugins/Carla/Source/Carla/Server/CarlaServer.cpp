38ec7d88fdee54be302e021acc7d6509141beaf2
Xavier Solé Nogués <xavisolesoft@gmail.com>
2024-02-19 10:59:13 +0100

    Fix windows build: Uninitialized variable error

--- Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Server/CarlaServer.cpp
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Server/CarlaServer.cpp
@@ -1299,7 +1299,7 @@ BIND_SYNC(is_sensor_enabled_for_ros) << [this](carla::streaming::detail::stream_
       TArray<UActorComponent*> Components;
       CarlaActor->GetActor()->GetComponents(Components);
 
-      USceneComponent* Component;
+      USceneComponent* Component = nullptr;
       for(auto Cmp : Components)
       {
         if(USceneComponent* SCMP = Cast<USceneComponent>(Cmp))
@@ -1343,7 +1343,7 @@ BIND_SYNC(is_sensor_enabled_for_ros) << [this](carla::streaming::detail::stream_
       TArray<UActorComponent*> Components;
       CarlaActor->GetActor()->GetComponents(Components);
 
-      USceneComponent* Component;
+      USceneComponent* Component = nullptr;
       for(auto Cmp : Components)
       {
         if(USceneComponent* SCMP = Cast<USceneComponent>(Cmp))
