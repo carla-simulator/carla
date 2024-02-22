aaaed276b25872ae4a87936a3093902a1aa4523a
Daniel <github.blurry@9ox.net>
2024-02-22 10:56:37 +0100

    Python 2.7 and <3.6 compatible code

--- PythonAPI/carla/agents/tools/hints.py
+++ PythonAPI/carla/agents/tools/hints.py
@@ -1,13 +1,24 @@
-from typing import NamedTuple, Union
 
-from carla import Actor, TrafficLight
+import sys
+if sys.version_info < (3, 6):
+    from collections import namedtuple
+    ObstacleDetectionResult = namedtuple('ObstacleDetectionResult', ['obstacle_was_found', 'obstacle', 'distance'])
+    TrafficLightDetectionResult = namedtuple('TrafficLightDetectionResult', ['traffic_light_was_found', 'traffic_light'])
+    print("Python version is less than 3.6")
+else:
+    #from agents.tools.hints import ObstacleDetectionResult, TrafficLightDetectionResult
+    from typing import NamedTuple, Union
+    from carla import Actor, TrafficLight
+    """
+    class ObstacleDetectionResult(NamedTuple):
+        obstacle_was_found : bool
+        obstacle : Union[Actor, None]
+        distance : float 
+        # distance : Union[float, Literal[-1]] # Python 3.8+ only
 
-class ObstacleDetectionResult(NamedTuple):
-    obstacle_was_found : bool
-    obstacle : Union[Actor, None]
-    distance : float 
-    # distance : Union[float, Literal[-1]] # Python 3.8+ only
-
-class TrafficLightDetectionResult(NamedTuple):
-    traffic_light_was_found : bool
-    traffic_light : Union[TrafficLight, None]
+    class TrafficLightDetectionResult(NamedTuple):
+        traffic_light_was_found : bool
+        traffic_light : Union[TrafficLight, None]
+    """
+    ObstacleDetectionResult = NamedTuple('ObstacleDetectionResult', [('obstacle_was_found', bool), ('obstacle', Union[Actor, None]), ('distance', float)])
+    TrafficLightDetectionResult = NamedTuple('TrafficLightDetectionResult', [('traffic_light_was_found', bool), ('traffic_light', Union[TrafficLight, None])])
