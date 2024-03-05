d01d6672e11fb60d3a7d924d941c851b31788c33
Daniel <github.blurry@9ox.net>
2024-03-05 14:07:55 +0100

    SimpleConfig warns if duplicated key is encoutnered.

--- PythonAPI/carla/agents/conf/agent_settings_backend.py
+++ PythonAPI/carla/agents/conf/agent_settings_backend.py
@@ -83,6 +83,71 @@ class AgentConfig:
             else:
                 print(f"Warning: Key '{key}' not found in {self.__class__.__name__} default options. Consider updating or creating a new class to avoid this message.")
 
+
+    
+class SimpleConfig(object):
+    """
+    A class that allows a more simple way to initialize settings.
+    Initializing an instance changes the type the the given base class, defined via `_base_settings`.
+    
+    :param _base_settings: The base class to port the settings to.
+    
+    TODO: NOTE: This class assumes that there are NO DUPLICATE keys in the underlying base
+    """
+    
+    _base_settings: ClassVar["AgentConfig"] = None
+    
+    def __new__(cls, overwrites:Optional[Dict[str, Union[dict, AgentConfig]]]=None) -> "AgentConfig":
+        """
+        Transforms a SimpleConfig class into the given _base_settings
+        
+        :param overwrites: That allow overwrites during initialization.
+        """
+        if cls._base_settings is None:
+            raise TypeError("{cls.__name__} must have a class set in the `_base_settings` to initialize to.")
+        if cls is SimpleConfig:
+            raise TypeError("SimpleConfig class may not be instantiated")
+        simple_settings = {k:v for k,v  in cls.__dict__.items() if not k.startswith("_") } # Removes all private attributes
+        if overwrites:
+            simple_settings.update(overwrites) 
+        return super().__new__(cls).to_nested_config(simple_settings) # call from a pseudo instance.
+    
+    @class_or_instance_method
+    def to_nested_config(self, simple_overwrites:dict=None) -> AgentConfig:
+        """
+        Initializes the _base_settings with the given overwrites.
+        
+        Maps the keys of simple_overwrites to the base settings.
+        
+        More specifically builds a overwrites dict that is compatible with the nested configuration versions.
+        
+        NOTE: Assumes unique keys over all settings!
+        # TODO: add a warning if a non-unique key is found in the overwrites.
+        """
+        if isinstance(self, type): # called on class
+            return self()
+        keys = set(simple_overwrites.keys())
+        removed_keys = set() # to check for duplicated keys that cannot be set via SimpleConfig unambiguously
+        overwrites = {}
+        for name, base in self._base_settings.__annotations__.items():
+            if not isinstance(base, type) or not issubclass(base, AgentConfig): # First out non AgentConfig attributes
+                continue
+            matching = keys.intersection(base.__dataclass_fields__.keys()) # keys that match from the simple config to the real nested config
+            if len(matching) > 0:
+                if removed_keys.intersection(matching):
+                    print("WARNING: Ambiguous key", removed_keys.intersection(matching), "in the SimpleConfig", str(self), "that occurs multiple times in its given base", self._base_settings.__name__+".", "Encountered at", name, base) # TODO: remove later, left here for testing.")
+                overwrites[name] = {k: getattr(self, k) for k in matching}
+                keys -= matching
+                removed_keys.update(matching)
+        if len(keys) != 0:
+            overwrites.update({k: v for k,v in simple_overwrites.items() if k in keys})
+            print("Warning: Unmatched keys", keys, "in", self.__class__.__name__, "not contained in base", self._base_settings.__name__) # TODO: remove later, left here for testing.
+        return self._base_settings(overwrites=overwrites)
+        # TODO: could add new keys after post-processing.
+        
+        
+# ---------------------
+
 # ---------------------
 # Live Info
 # ---------------------
@@ -561,65 +626,7 @@ class AutopilotBehavior(AgentConfig):
     update_vehicle_lights : bool = False
     """Sets if the Traffic Manager is responsible of updating the vehicle lights, or not."""
     
-    
-class SimpleConfig(object):
-    """
-    A class that allows a more simple way to initialize settings and port them to a specified
-    base class.
-    
-    :param _base_settings: The base class to port the settings to.
-    
-    TODO: NOTE: This class assumes that there are NO DUPLICATE keys in the underlying base
-    """
-    
-    _base_settings: ClassVar["AgentConfig"] = None
-    
-    def __new__(cls, overwrites:Optional[Dict[str, Union[dict, AgentConfig]]]=None) -> "AgentConfig":
-        """
-        Transforms a SimpleConfig class into the given _base_settings
-        
-        :param overwrites: That allow overwrites during initialization.
-        """
-        if cls is SimpleConfig:
-            raise TypeError("SimpleConfig class may not be instantiated")
-        if cls._base_settings is None:
-            raise TypeError("{cls.__name__} must have a class set in the `_base_settings` to initialize to.")
-        the_inst = super().__new__(cls)
-        simple_settings = {k:v for k,v  in cls.__dict__.items() if not k.startswith("_") } # Removes all private attributes
-        if overwrites:
-            simple_settings.update(overwrites) 
-        return the_inst.to_nested_config(simple_settings)
-    
-    @class_or_instance_method
-    def to_nested_config(self, simple_overwrites:dict=None) -> AgentConfig:
-        """
-        Initializes the _base_settings with the given overwrites.
-        
-        Maps the keys of simple_overwrites to the base settings.
-        
-        NOTE: Assumes unique keys over all settings!
-        # TODO: add a warning if a non-unique key is found in the overwrites.
-        """
-        if isinstance(self, type): # called on class
-            return self()
-        keys = set(simple_overwrites.keys())
-        overwrites = {}
-        for name, base in self._base_settings.__annotations__.items():
-            if not isinstance(base, type) or not issubclass(base, AgentConfig): # First out non AgentConfig attributes
-                continue
-            matching = keys.intersection(base.__dataclass_fields__.keys())
-            print(str(base.__class__.__name__), "matches in", matching)
-            insert = {k: getattr(self, k) for k in matching}
-            if len(insert) > 0:
-                overwrites[name] = {k: getattr(self, k) for k in matching}
-            keys -= matching
-        if len(keys) != 0:
-            overwrites.update({k: v for k,v in simple_overwrites.items() if k in keys})
-            # TODO: turn into a warning and find way to include these keys in the config anyway!
-            #raise ValueError(f"Unmatched keys {keys} in {self.__class__.__name__} not contained in base {self._base_settings.__name__}")
-            print("Warning: Unmatched keys", keys, "in", self.__class__.__name__, "not contained in base", self._base_settings.__name__) # TODO: remove later, left here for testing.
-        return self._base_settings(overwrites=overwrites)
-        # TODO: could add new keys after post-processing.
+
 
 
 # ---------------------
@@ -649,6 +656,7 @@ class BehaviorAgentSettings(AgentConfig):
     controls : BehaviorAgentControllerSettings = field(default_factory=BehaviorAgentControllerSettings, init=False)
     planner : BehaviorAgentPlannerSettings = field(default_factory=BehaviorAgentPlannerSettings, init=False)
     emergency : BehaviorAgentEmergencySettings = field(default_factory=BehaviorAgentEmergencySettings, init=False)
+    avoid_tailgators : bool = True
  
 @dataclass
 class SimpleBasicAgentSettings(SimpleConfig, LiveInfo, BasicAgentSpeedSettings, BasicAgentDistanceSettings, BasicAgentLaneChangeSettings, BasicAgentObstacleSettings, BasicAgentControllerSettings, BasicAgentPlannerSettings, BasicAgentEmergencySettings):
