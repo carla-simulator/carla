07963dba968b818aa67ce08cb18e3a492f6c2755
Marcel Pi <25649656+MarcelPiNacy@users.noreply.github.com>
2024-03-12 23:19:57 +0100

    Make _CRT_SECURE_NO_WARNINGS a global definition. Introduce mechanism to disable warnings.

--- CMake/Warnings.cmake
+++ CMake/Warnings.cmake
@@ -18,3 +18,29 @@ if (ENABLE_ALL_WARNINGS)
     endif ()
   endif ()
 endif ()
+
+macro (carla_try_suppress_cxx_warning NAME FLAG)
+  check_cxx_compiler_flag (
+    -Wno-${FLAG}
+    HAS_${NAME}
+  )
+  if (HAS_${NAME})
+    add_compile_options (
+      $<$<COMPILE_LANGUAGE:CXX>:-Wno-${FLAG}>
+    )
+  endif ()
+endmacro ()
+
+set (
+  CARLA_SUPRESSED_WARNING_LIST
+  macro-redefined
+  incompatible-pointer-types
+)
+
+foreach (WARNING ${CARLA_SUPRESSED_WARNING_LIST})
+  string (MAKE_C_IDENTIFIER "${WARNING}" WARNING_NAME)
+  carla_try_suppress_cxx_warning (
+    ${WARNING_NAME}
+    ${WARNING}
+  )
+endforeach ()
