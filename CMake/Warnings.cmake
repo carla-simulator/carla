0a9437fd10e7afc102c914ec3c09a9024620d3a1
Marcel Pi <25649656+MarcelPiNacy@users.noreply.github.com>
2024-03-13 01:04:27 +0100

    Improve warning suppression.

--- CMake/Warnings.cmake
+++ CMake/Warnings.cmake
@@ -19,25 +19,54 @@ if (ENABLE_ALL_WARNINGS)
   endif ()
 endif ()
 
+if (CMAKE_C_COMPILER_FRONTEND_VARIANT STREQUAL "GNU")
+  set (SUPPRESS_WARNING_DIRECTIVE_PREFIX -Wno-)
+elseif (CMAKE_C_COMPILER_FRONTEND_VARIANT STREQUAL "MSVC")
+  set (SUPPRESS_WARNING_DIRECTIVE_PREFIX /wd)
+endif ()
+
 macro (carla_try_suppress_cxx_warning NAME FLAG)
   check_cxx_compiler_flag (
-    -Wno-${FLAG}
+    ${SUPPRESS_WARNING_DIRECTIVE_PREFIX}${FLAG}
     HAS_${NAME}
   )
   if (HAS_${NAME})
     add_compile_options (
-      $<$<COMPILE_LANGUAGE:CXX>:-Wno-${FLAG}>
-    )
+      $<$<COMPILE_LANGUAGE:CXX>:${SUPPRESS_WARNING_DIRECTIVE_PREFIX}${FLAG}>)
+  endif ()
+endmacro ()
+
+macro (carla_try_suppress_c_warning NAME FLAG)
+  check_c_compiler_flag (
+    ${SUPPRESS_WARNING_DIRECTIVE_PREFIX}${FLAG}
+    HAS_${NAME}
+  )
+  if (HAS_${NAME})
+    add_compile_options (
+      $<$<COMPILE_LANGUAGE:C>:${SUPPRESS_WARNING_DIRECTIVE_PREFIX}${FLAG}>)
   endif ()
 endmacro ()
 
 set (
-  CARLA_SUPRESSED_WARNING_LIST
-  macro-redefined
+  CARLA_C_SUPRESSED_WARNING_LIST
+  macro-redefined 4005
   incompatible-pointer-types
 )
 
-foreach (WARNING ${CARLA_SUPRESSED_WARNING_LIST})
+set (
+  CARLA_CXX_SUPRESSED_WARNING_LIST
+  macro-redefined 4005
+)
+
+foreach (WARNING ${CARLA_C_SUPRESSED_WARNING_LIST})
+  string (MAKE_C_IDENTIFIER "${WARNING}" WARNING_NAME)
+  carla_try_suppress_c_warning (
+    ${WARNING_NAME}
+    ${WARNING}
+  )
+endforeach ()
+
+foreach (WARNING ${CARLA_CXX_SUPRESSED_WARNING_LIST})
   string (MAKE_C_IDENTIFIER "${WARNING}" WARNING_NAME)
   carla_try_suppress_cxx_warning (
     ${WARNING_NAME}
