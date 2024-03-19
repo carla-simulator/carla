29b2bd3af345ba89e6e11fb0ed085c65bbac020a
xavisolesoft <xavisolesoft@gmail.com>
2024-03-19 18:02:07 +0100

    We are already on carlaUE5 repo

--- Setup.sh
+++ Setup.sh
@@ -10,14 +10,9 @@ pip3 install --user -Iv setuptools==47.3.1
 pip3 install --user distro
 pip3 install --user wheel auditwheel
 
-git clone -b ue5-dev https://github.com/carla-simulator/carla.git carlaUE5
-cd carlaUE5
-
 curl -L -O https://github.com/Kitware/CMake/releases/download/v3.28.3/cmake-3.28.3-linux-x86_64.tar.gz
 tar -xvf cmake-3.28.3-linux-x86_64.tar.gz
-cp cmake-3.28.3-linux-x86_64/bin/cmake .
-rm -rf cmake-3.28.3-linux-x86_64.sh
-rm -rf cmake-3.28.3-linux-x86_64
+cp cmake-3.28.3-linux-x86_64/bin/* .
 
-./cmake -G Ninja -S . -B Build --toolchain=CMake/LinuxToolchain.cmake -DLAUNCH_ARGS="-prefernvidia" -DCMAKE_BUILD_TYPE=Release -DENABLE_ROS2=ON -DBUILD_CARLA_UNREAL=ON -DCARLA_UNREAL_ENGINE_PATH=$CARLA_UNREAL_ENGINE_PATH
-./cmake --build Build --target launch
+./cmake-3.28.3-linux-x86_64/bin/cmake -G Ninja -S . -B Build --toolchain=CMake/LinuxToolchain.cmake -DLAUNCH_ARGS="-prefernvidia" -DCMAKE_BUILD_TYPE=Release -DENABLE_ROS2=ON -DBUILD_CARLA_UNREAL=ON -DCARLA_UNREAL_ENGINE_PATH=$CARLA_UNREAL_ENGINE_PATH
+./cmake-3.28.3-linux-x86_64/bin/cmake --build Build --target launch
