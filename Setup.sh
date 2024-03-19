02e60feb3d1786d3ddb4793efa55f4972de60b33
Xavier Solé Nogués <xavisolesoft@gmail.com>
2024-03-19 16:32:11 +0100

    Script is able to install requirements and build Carla

--- Setup.sh
+++ Setup.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 set -e
 
-LAUNCH_COMMAND="retry --until=success --times=5 --delay=20 time"
+y | sudo apt-add-repository "deb http://archive.ubuntu.com/ubuntu focal main universe"
 sudo apt-get update
-sudo apt-get install retry
+sudo apt-get install build-essential retry g++-7 ninja-build libvulkan1 python3 python3-dev python3-pip libpng-dev libtiff5-dev libjpeg-dev tzdata sed curl libtool rsync libxml2-dev git git-lfs
 
-#$LAUNCH_COMMAND sudo apt-get install wget software-properties-common
-#$LAUNCH_COMMAND sudo add-apt-repository ppa:ubuntu-toolchain-r/test
-#$LAUNCH_COMMAND wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add
+pip3 install --user numpy
+pip3 install --user -Iv setuptools==47.3.1
+pip3 install --user distro
+pip3 install --user wheel auditwheel
 
-#$LAUNCH_COMMAND sudo apt-add-repository "deb http://archive.ubuntu.com/ubuntu focal main universe"
-$LAUNCH_COMMAND "sudo apt-get update"
-$LAUNCH_COMMAND "sudo apt-get install build-essential g++-7 ninja-build libvulkan1 python3 python3-dev python3-pip libpng-dev libtiff5-dev libjpeg-dev tzdata sed curl libtool rsync libxml2-dev git git-lfs"
+git clone -b ue5-dev https://github.com/carla-simulator/carla.git carlaUE5
+cd carlaUE5
 
-$LAUNCH_COMMAND "curl -L -O https://github.com/Kitware/CMake/releases/download/v3.29.0-rc2/cmake-3.29.0-rc2.tar.gz"
-sudo apt-get install build-essential libssl-dev tar
-tar xvf cmake-3.29.0-rc2.tar.gz
-cd cmake-3.29.0-rc2
-./configure
-make
-sudo make install
-cd ..
-rm -rf cmake-3.29.0-rc2-linux-aarch64.tar.gz
+curl -L -O https://github.com/Kitware/CMake/releases/download/v3.28.3/cmake-3.28.3-linux-x86_64.tar.gz
+tar -xvf cmake-3.28.3-linux-x86_64.tar.gz
+cp cmake-3.28.3-linux-x86_64/bin/cmake .
+rm -rf cmake-3.28.3-linux-x86_64.sh
+rm -rf cmake-3.28.3-linux-x86_64
+
+./cmake -G Ninja -S . -B Build --toolchain=CMake/LinuxToolchain.cmake -DLAUNCH_ARGS="-prefernvidia" -DCMAKE_BUILD_TYPE=Release -DENABLE_ROS2=ON -DBUILD_CARLA_UNREAL=ON -DCARLA_UNREAL_ENGINE_PATH=$CARLA_UNREAL_ENGINE_PATH
+./cmake --build Build --target launch
