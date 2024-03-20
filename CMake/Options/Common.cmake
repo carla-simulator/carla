a7fea2e360a16a500dfa017bbca7bc916c822849
Marcel Pi <25649656+MarcelPiNacy@users.noreply.github.com>
2024-03-20 20:41:12 +0100

    Simplify UE targets.

--- CMake/Options/Common.cmake
+++ CMake/Options/Common.cmake
@@ -107,7 +107,7 @@ option (
 carla_string_option (
   LIBCARLA_IMAGE_SUPPORTED_FORMATS
   "Semicolon-separated list of supported image formats by LibCarla. Available formats: png, jpeg, tiff"
-  "png"
+  png
 )
 
 carla_string_option (  
