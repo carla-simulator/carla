6b82cc23b9719d009b62f989fc1ec92d67195077
Xavier Solé Nogués <xavisolesoft@gmail.com>
2024-03-25 17:27:07 +0100

    Fix echo " characters

--- Setup.bat
+++ Setup.bat
@@ -1,33 +1,33 @@
-echo "Installing Visual Studion 2022..."
+echo Installing Visual Studion 2022...
 curl -L -O https://aka.ms/vs/17/release/vs_community.exe
 vs_Community.exe --add Microsoft.VisualStudio.Workload.NativeDesktop Microsoft.VisualStudio.Workload.NativeGame Microsoft.VisualStudio.Workload.ManagedDesktop Microsoft.VisualStudio.Component.Windows10SDK.18362  Microsoft.VisualStudio.Component.VC.CMake.Project Microsoft.Net.ComponentGroup.4.8.1.DeveloperTools Microsoft.VisualStudio.Component.VC.Llvm.Clang Microsoft.VisualStudio.Component.VC.Llvm.ClangToolset Microsoft.VisualStudio.ComponentGroup.NativeDesktop.Llvm.Clang --removeProductLang Es-es --addProductLang En-us --installWhileDownloading --passive --wait
 del vs_community.exe
-echo "Visual Studion 2022 Installed!!!"
+echo Visual Studion 2022 Installed!!!
 
-echo "Installing Ninja..."
+echo Installing Ninja...
 curl -L -o %USERPROFILE%\AppData\Local\Microsoft\WindowsApps\ninja-win.zip https://github.com/ninja-build/ninja/releases/download/v1.11.1/ninja-win.zip
 powershell -command "Expand-Archive $env:USERPROFILE\AppData\Local\Microsoft\WindowsApps\ninja-win.zip $env:USERPROFILE\AppData\Local\Microsoft\WindowsApps\ninja-win"
 move %USERPROFILE%\AppData\Local\Microsoft\WindowsApps\ninja-win\ninja.exe %USERPROFILE%\AppData\Local\Microsoft\WindowsApps\ninja.exe
 rmdir /s /q %USERPROFILE%\AppData\Local\Microsoft\WindowsApps\ninja-win
 del /f %USERPROFILE%\AppData\Local\Microsoft\WindowsApps\ninja-win.zip
-echo "Ninja Installed!!!"
+echo Ninja Installed!!!
 
-echo "Starting Content Download..."
+echo Starting Content Download...
 mkdir Unreal\CarlaUnreal\Content
 start cmd /c git -C Unreal/CarlaUnreal/Content clone -b ue5-dev https://bitbucket.org/carla-simulator/carla-content.git Carla
 
-echo "Switching to x64 Native Tools Command Prompt for VS 2022 command line..."
+echo Switching to x64 Native Tools Command Prompt for VS 2022 command line...
 call "%ProgramFiles%\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
 pushd ..
-echo "Cloning CARLA Unreal Engine 5..."
+echo Cloning CARLA Unreal Engine 5...
 git clone -b ue5-dev-carla https://github.com/CarlaUnreal/UnrealEngine.git UnrealEngine5_carla
 pushd UnrealEngine5_carla
 set /A CARLA_UNREAL_ENGINE_PATH=%cd%
-echo "Setup CARLA Unreal Engine 5..."
+echo Setup CARLA Unreal Engine 5...
 Setup.bat
-echo "GenerateProjectFiles CARLA Unreal Engine 5..."
+echo GenerateProjectFiles CARLA Unreal Engine 5...
 GenerateProjectFiles.bat
-echo "Opening Visual Studio 2022..."
+echo Opening Visual Studio 2022...
 powershell write-host -fore Red Please Build UE5 Project following the nextlink instructions:
 echo https://dev.epicgames.com/documentation/en-us/unreal-engine/building-unreal-engine-from-source#runningtheeditor
 start https://dev.epicgames.com/documentation/en-us/unreal-engine/building-unreal-engine-from-source#runningtheeditor
@@ -35,5 +35,10 @@ start "" /B /WAIT "C:\Program Files\Microsoft Visual Studio\2022\Community\Commo
 popd
 popd
 
+echo Configuring CARLA...
 cmake -G Ninja -S . -B Build -DCMAKE_BUILD_TYPE=Release -DBUILD_CARLA_UNREAL=ON -DCARLA_UNREAL_ENGINE_PATH=%CARLA_UNREAL_ENGINE_PATH%
+echo Buiding CARLA...
+cmake --build Build
+echo Build Succesfull :)
+echo Launching Unreal Editor with CARLA...
 cmake --build Build --target launch
