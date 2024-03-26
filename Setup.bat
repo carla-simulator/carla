2d235cc937c7f610b5b65a920c63e8d8f0c6ad73
Xavier Solé Nogués <xavisolesoft@gmail.com>
2024-03-26 08:33:49 +0100

    Add crash on error behaviour to commands

--- Setup.bat
+++ Setup.bat
@@ -4,16 +4,16 @@ start cmd /c git -C Unreal/CarlaUnreal/Content clone -b ue5-dev https://bitbucke
 
 
 echo Installing Visual Studion 2022...
-curl -L -O https://aka.ms/vs/17/release/vs_community.exe
-vs_Community.exe --add Microsoft.VisualStudio.Workload.NativeDesktop Microsoft.VisualStudio.Workload.NativeGame Microsoft.VisualStudio.Workload.ManagedDesktop Microsoft.VisualStudio.Component.Windows10SDK.18362  Microsoft.VisualStudio.Component.VC.CMake.Project Microsoft.Net.ComponentGroup.4.8.1.DeveloperTools Microsoft.VisualStudio.Component.VC.Llvm.Clang Microsoft.VisualStudio.Component.VC.Llvm.ClangToolset Microsoft.VisualStudio.ComponentGroup.NativeDesktop.Llvm.Clang --removeProductLang Es-es --addProductLang En-us --installWhileDownloading --passive --wait
+curl -L -O https://aka.ms/vs/17/release/vs_community.exe || exit /b
+vs_Community.exe --add Microsoft.VisualStudio.Workload.NativeDesktop Microsoft.VisualStudio.Workload.NativeGame Microsoft.VisualStudio.Workload.ManagedDesktop Microsoft.VisualStudio.Component.Windows10SDK.18362  Microsoft.VisualStudio.Component.VC.CMake.Project Microsoft.Net.ComponentGroup.4.8.1.DeveloperTools Microsoft.VisualStudio.Component.VC.Llvm.Clang Microsoft.VisualStudio.Component.VC.Llvm.ClangToolset Microsoft.VisualStudio.ComponentGroup.NativeDesktop.Llvm.Clang --removeProductLang Es-es --addProductLang En-us --installWhileDownloading --passive --wait || exit /b
 del vs_community.exe
 echo Visual Studion 2022 Installed!!!
 
 
 echo Installing Ninja...
-curl -L -o %USERPROFILE%\AppData\Local\Microsoft\WindowsApps\ninja-win.zip https://github.com/ninja-build/ninja/releases/download/v1.11.1/ninja-win.zip
-powershell -command "Expand-Archive $env:USERPROFILE\AppData\Local\Microsoft\WindowsApps\ninja-win.zip $env:USERPROFILE\AppData\Local\Microsoft\WindowsApps\ninja-win"
-move %USERPROFILE%\AppData\Local\Microsoft\WindowsApps\ninja-win\ninja.exe %USERPROFILE%\AppData\Local\Microsoft\WindowsApps\ninja.exe
+curl -L -o %USERPROFILE%\AppData\Local\Microsoft\WindowsApps\ninja-win.zip https://github.com/ninja-build/ninja/releases/download/v1.11.1/ninja-win.zip || exit /b
+powershell -command "Expand-Archive $env:USERPROFILE\AppData\Local\Microsoft\WindowsApps\ninja-win.zip $env:USERPROFILE\AppData\Local\Microsoft\WindowsApps\ninja-win" || exit /b
+move %USERPROFILE%\AppData\Local\Microsoft\WindowsApps\ninja-win\ninja.exe %USERPROFILE%\AppData\Local\Microsoft\WindowsApps\ninja.exe || exit /b
 rmdir /s /q %USERPROFILE%\AppData\Local\Microsoft\WindowsApps\ninja-win
 del /f %USERPROFILE%\AppData\Local\Microsoft\WindowsApps\ninja-win.zip
 echo Ninja Installed!!!
@@ -22,8 +22,8 @@ echo Ninja Installed!!!
 python --version 2>NUL
 if errorlevel 1 (
     echo Installing Python 3.8.10...
-    curl -L -O https://www.python.org/ftp/python/3.8.10/python-3.8.10-amd64.exe
-    python-3.8.10-amd64.exe /quiet AppendPath=1
+    curl -L -O https://www.python.org/ftp/python/3.8.10/python-3.8.10-amd64.exe || exit /b
+    python-3.8.10-amd64.exe /quiet AppendPath=1 || exit /b
     del python-3.8.10-amd64.exe
     echo Python 3.8.10 installed!!!
 ) else (
@@ -33,26 +33,26 @@ if errorlevel 1 (
 
 
 echo Installing Python Pacakges...
-pip install --user numpy
-pip install --user -Iv setuptools==47.3.1
-pip install --user distro
-pip install --user wheel auditwheel
+pip install --user numpy || exit /b
+pip install --user -Iv setuptools==47.3.1 || exit /b
+pip install --user distro || exit /b
+pip install --user wheel auditwheel || exit /b
 echo Python Pacakges Installed...
 
 
 echo Switching to x64 Native Tools Command Prompt for VS 2022 command line...
-call "%ProgramFiles%\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
+call "%ProgramFiles%\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat" || exit /b
 pushd ..
 echo Cloning CARLA Unreal Engine 5...
-git clone -b ue5-dev-carla https://github.com/CarlaUnreal/UnrealEngine.git UnrealEngine5_carla
+git clone -b ue5-dev-carla https://github.com/CarlaUnreal/UnrealEngine.git UnrealEngine5_carla || exit /b
 pushd UnrealEngine5_carla
 set /A CARLA_UNREAL_ENGINE_PATH=cd
 echo Setup CARLA Unreal Engine 5...
-call Setup.bat
+call Setup.bat || exit /b
 echo GenerateProjectFiles CARLA Unreal Engine 5...
-call GenerateProjectFiles.bat
+call GenerateProjectFiles.bat || exit /b
 echo Opening Visual Studio 2022...
-start "" /B /WAIT msbuild Engine\Intermediate\ProjectFiles\UE5.vcxproj /property:Configuration="Development_Editor" /property:Platform="x64"
+msbuild Engine\Intermediate\ProjectFiles\UE5.vcxproj /property:Configuration="Development_Editor" /property:Platform="x64" || exit /b
 popd
 popd
 
@@ -61,7 +61,7 @@ echo Configuring CARLA...
 set COUNT=0
 :cmake_configure_command
 set /A COUNT=%COUNT%+1
-cmake -G Ninja -S . -B Build -DCMAKE_BUILD_TYPE=Release -DBUILD_CARLA_UNREAL=ON -DCARLA_UNREAL_ENGINE_PATH=%CARLA_UNREAL_ENGINE_PATH%
+cmake -G Ninja -S . -B Build -DCMAKE_BUILD_TYPE=Release -DBUILD_CARLA_UNREAL=ON -DCARLA_UNREAL_ENGINE_PATH=%CARLA_UNREAL_ENGINE_PATH% || exit /b
 timeout /t 10
 if errorlevel 1 if %COUNT% LSS 12 goto cmake_configure_command
 
@@ -69,10 +69,10 @@ echo Buiding CARLA...
 set COUNT=0
 :cmake_build_command
 set /A COUNT=%COUNT%+1
-cmake --build Build
+cmake --build Build || exit /b
 timeout /t 10
 if errorlevel 1 if %COUNT% LSS 12 goto cmake_build_command
 
 echo Build Succesfull :)
 echo Launching Unreal Editor with CARLA...
-cmake --build Build --target launch
+cmake --build Build --target launch || exit /b
