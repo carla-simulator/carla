71c4e95a2aa675ff3bd85bd0c76da51f502fce37
berndgassmann <bernd.gassmann@gmx.de>
2024-03-27 17:23:29 +0100

    Some cleanup
    Copied Util/Formatting/clang-format to LibCarla/.clang-format
    and formatted the Exception files (at least) to get rid of formatting
    errors.

--- LibCarla/source/carla/Exception.h
+++ LibCarla/source/carla/Exception.h
@@ -12,28 +12,28 @@
 
 namespace carla {
 
-  /// User define function, similar to Boost throw_exception.
-  ///
-  /// @important Boost exceptions are also routed to this function.
-  ///
-  /// When compiled with LIBCARLA_NO_EXCEPTIONS, this function is left undefined
-  /// in LibCarla, and the modules using LibCarla are expected to supply an
-  /// appropriate definition. Callers of throw_exception are allowed to assume
-  /// that the function never returns; therefore, if the user-defined
-  /// throw_exception returns, the behavior is undefined.
-  [[ noreturn ]] void throw_exception(const std::exception &e);
+/// User define function, similar to Boost throw_exception.
+///
+/// @important Boost exceptions are also routed to this function.
+///
+/// When compiled with LIBCARLA_NO_EXCEPTIONS, this function is left undefined
+/// in LibCarla, and the modules using LibCarla are expected to supply an
+/// appropriate definition. Callers of throw_exception are allowed to assume
+/// that the function never returns; therefore, if the user-defined
+/// throw_exception returns, the behavior is undefined.
+[[noreturn]] void throw_exception(const std::exception &e);
 
-} // namespace carla
+}  // namespace carla
 
 #else
 
 namespace carla {
 
-  template <typename T>
-  [[ noreturn ]] void throw_exception(const T &e) {
-    throw e;
-  }
+template <typename T>
+[[noreturn]] void throw_exception(const T &e) {
+  throw e;
+}
 
-} // namespace carla
+}  // namespace carla
 
-#endif // LIBCARLA_NO_EXCEPTIONS
+#endif  // LIBCARLA_NO_EXCEPTIONS
