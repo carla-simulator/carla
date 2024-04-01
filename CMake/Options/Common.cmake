a48347e40b5d9e8762dc53a9c016958a8efa66e1
Marcel Pi <25649656+MarcelPiNacy@users.noreply.github.com>
2024-04-02 01:14:56 +0200

    Switch to wrapper macro for options.

--- CMake/Options/Common.cmake
+++ CMake/Options/Common.cmake
@@ -8,97 +8,97 @@
 
 ]]
 
-option (
+carla_option (
   BUILD_CARLA_CLIENT
   "Build the CARLA client."
   ON
 )
 
-option (
+carla_option (
   BUILD_CARLA_SERVER
   "Build the CARLA server."
   ON
 )
 
-option (
+carla_option (
   BUILD_EXAMPLES
   "Build CARLA examples."
   ON
 )
 
-option (
+carla_option (
   BUILD_LIBCARLA_TESTS
   "Build LibCarla tests."
   ON
 )
 
-option (
+carla_option (
   ENABLE_PYTORCH
   "Enable PyTorch for CARLA."
   OFF
 )
 
-option (
+carla_option (
   BUILD_PYTHON_API
   "Build the CARLA Python API."
   ON
 )
 
-option (
+carla_option (
   ENABLE_ROS2
   "Enable ROS2."
   OFF
 )
 
-option (
+carla_option (
   ENABLE_OSM2ODR
   "Enable OSM2ODR."
   OFF
 )
 
-option (
+carla_option (
   ENABLE_RSS
   "Enable RSS components (ad-rss-lib)"
   OFF
 )
 
-option (
+carla_option (
   INSTALL_LIBCARLA
   "Install LibCarla and all of its dependencies."
   ON
 )
 
-option (
+carla_option (
   BUILD_OSM_WORLD_RENDERER
   "Build OSM World Renderer."
   OFF
 )
 
-option (
+carla_option (
   ENABLE_RTTI
   "Enable C++ RTTI."
   OFF
 )
 
-option (
+carla_option (
   ENABLE_EXCEPTIONS
   "Enable C++ Exceptions."
   OFF
 )
 
-option (
+carla_option (
   PREFER_CLONE
   "Whether to clone dependencies instead of directly downloading a compressed archive."
   OFF
 )
 
-option (
+carla_option (
   ENABLE_ALL_WARNINGS
   "Whether to emit extra build warnings."
   OFF
 )
 
-option (
+carla_option (
   ENABLE_WARNINGS_TO_ERRORS
   "Whether to convert build warnings to errors."
   OFF
