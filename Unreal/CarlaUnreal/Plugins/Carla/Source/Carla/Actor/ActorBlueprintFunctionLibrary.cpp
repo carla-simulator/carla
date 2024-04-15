3c397a346fbb51b60caad4029326193eb95daa6d
Xavier Solé Nogués <xavisolesoft@gmail.com>
2024-04-15 08:43:13 +0200

    Fix IsValid build error in package mode (#7428)

--- Unreal/CarlaUnreal/Plugins/Carla/Source/Carla/Actor/ActorBlueprintFunctionLibrary.cpp
+++ Unreal/CarlaUnreal/Plugins/Carla/Source/Carla/Actor/ActorBlueprintFunctionLibrary.cpp
@@ -1530,7 +1530,7 @@ FColor UActorBlueprintFunctionLibrary::RetrieveActorAttributeToColor(
   }
 #else
 #  define CARLA_ABFL_CHECK_ACTOR(ActorPtr) \
-  IsValid(ActorPtr));
+  IsValid(ActorPtr);
 #endif // WITH_EDITOR
 
 void UActorBlueprintFunctionLibrary::SetCamera(
