7162db87611b29f60f8c4eeb216f5610a1d4d4c1
Xavier Solé Nogués <xavisolesoft@gmail.com>
2024-04-15 10:32:06 +0200

    Implement package generation. for windows (#7427)
    * Implement package generation for windows
    
    * Prevent unreal editor to ve launched when finish the package

---
+++ Unreal/Package/RemoveUnrealPackageExtraFiles.cmake
@@ -0,0 +1,3 @@
+file(REMOVE ${CARLA_TARGET_PACKAGE_PATH}/Manifest_NonUFSFiles_Linux.txt)
+file(REMOVE ${CARLA_TARGET_PACKAGE_PATH}/Manifest_UFSFiles_Linux.txt)
+file(REMOVE ${CARLA_TARGET_PACKAGE_PATH}/Manifest_DebugFiles_Linux.txt)
