e24cad59b5168b18ee6f19feb28abcb510fe6397
xavisolesoft <xavisolesoft@gmail.com>
2024-04-16 18:30:50 +0100

    Pacial changes

--- Docker/RunDocker.sh
+++ Docker/RunDocker.sh
@@ -1,1 +1,2 @@
+#docker run -it -v ~/programing:/home --privileged --rm --gpus all -it --net=host -e DISPLAY=:0.GPU_ID -v /tmp/.X11-unix:/tmp/.X11-unix:rw carla-ubuntu-22.04 /bin/bash -vulkan
 docker run -it -v ~/programing:/home carla-ubuntu-22.04 /bin/bash
