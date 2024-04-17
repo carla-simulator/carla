8502dbc28dd9cd415e3fad1fa5f35f4a5f0e702e
xavisolesoft <xavisolesoft@gmail.com>
2024-04-17 14:31:17 +0100

    First version that builds, but not launches

--- Docker/BuildDocker.sh
+++ Docker/BuildDocker.sh
@@ -1,1 +1,4 @@
-docker build -t carla-ubuntu-22.04 -f Dockerfile .
+SCRIPT_RELATIVE_PATH=$(basename $PWD)
+pushd ..
+docker build -t carla-ubuntu-22.04 --build-arg LOCALE_LANG=$LANG -f $SCRIPT_RELATIVE_PATH/Dockerfile .
+popd
--- Docker/RunDocker.sh
+++ Docker/RunDocker.sh
@@ -1,2 +1,11 @@
 #docker run -it -v ~/programing:/home --privileged --rm --gpus all -it --net=host -e DISPLAY=:0.GPU_ID -v /tmp/.X11-unix:/tmp/.X11-unix:rw carla-ubuntu-22.04 /bin/bash -vulkan
-docker run -it -v ~/programing:/home carla-ubuntu-22.04 /bin/bash
+CARLA_ROOT=$PWD/..
+WORKSPACE=$CARLA_ROOT/..
+# TODO: Add Unreal engine path as optional argument
+# TODO: runthis command on start: git config --global --add safe.directory /home/xsole/programing/carlaUE5_cmake
+docker run -it \
+    --workdir $CARLA_ROOT \
+    -e CARLA_UNREAL_ENGINE_PATH=$WORKSPACE/UnrealEngine5_carla \
+    -v $WORKSPACE:$WORKSPACE \
+    carla-ubuntu-22.04 \
+    /bin/bash
