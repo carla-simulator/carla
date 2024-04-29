1071001114914a83eed4c1d774d5f8ac89465f3e
xavisolesoft <xavisolesoft@gmail.com>
2024-04-29 18:45:26 +0100

    Fix windows

--- Util/BuildTools/BuildOSM2ODR.bat
+++ Util/BuildTools/BuildOSM2ODR.bat
@@ -93,9 +93,12 @@ if %REMOVE_INTERMEDIATE% == true (
 rem Build OSM2ODR
 if %BUILD_OSM2ODR% == true (
     cd "%OSM2ODR_SOURCE_PATH%"
-    curl --retry 5 --retry-max-time 120 -L -o OSM2ODR.zip https://github.com/carla-simulator/sumo/archive/%CURRENT_OSM2ODR_COMMIT%.zip
-    tar -xf OSM2ODR.zip
-    cd sumo-%CURRENT_OSM2ODR_COMMIT%
+    if not exist "%OSM2ODR_SOURCE_PATH%" (
+        curl --retry 5 --retry-max-time 120 -L -o OSM2ODR.zip https://github.com/carla-simulator/sumo/archive/%CURRENT_OSM2ODR_COMMIT%.zip
+        tar -xf OSM2ODR.zip
+        del OSM2ODR.zip
+        ren sumo-%CURRENT_OSM2ODR_COMMIT% %OSM2ODR_SOURCE_PATH%
+    )
 
     if not exist "%OSM2ODR_VSPROJECT_PATH%" mkdir "%OSM2ODR_VSPROJECT_PATH%"
     cd "%OSM2ODR_VSPROJECT_PATH%"
