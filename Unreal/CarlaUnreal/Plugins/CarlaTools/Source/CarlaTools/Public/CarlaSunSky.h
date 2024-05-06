26a4fae161718561bbd148080a253e3754aece0c
Antonio Mata <artoniodev@gmail.com>
2024-05-06 14:02:58 +0200

    Added comment to explain the code and the class

--- Unreal/CarlaUnreal/Plugins/CarlaTools/Source/CarlaTools/Public/CarlaSunSky.h
+++ Unreal/CarlaUnreal/Plugins/CarlaTools/Source/CarlaTools/Public/CarlaSunSky.h
@@ -6,13 +6,17 @@
 #include "GameFramework/Actor.h"
 #include "CarlaSunSky.generated.h"
 
+/*
+Class created to hold functionality of the "BP_CarlaSky" that is innaccesible for the blueprint editor to access
+*/
+
 UCLASS()
 class CARLATOOLS_API ACarlaSunSky : public AActor
 {
 	GENERATED_BODY()
 	
 public:	
-
+	//Controls wether the skylight component has "bRealTimeCapture" ON or OFF
 	UFUNCTION(BlueprintCallable, Category = "Sun Sky Utilities", meta = (KeyWords = "SunSky, Utils"))
 	void SetRealTimeCapture(USkyLightComponent* SkyLightComp, bool NewRTC);
 };
