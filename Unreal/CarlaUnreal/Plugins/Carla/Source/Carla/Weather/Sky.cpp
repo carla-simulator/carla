7e3255a385ccb330abd9492c3f425dbdb8ff8bf0
Marcel Pi <25649656+MarcelPiNacy@users.noreply.github.com>
2024-05-27 13:40:40 +0200

    Add default subobjects.

--- Unreal/CarlaUnreal/Plugins/Carla/Source/Carla/Weather/Sky.cpp
+++ Unreal/CarlaUnreal/Plugins/Carla/Source/Carla/Weather/Sky.cpp
@@ -1,14 +1,22 @@
 #include "Sky.h"
+#include "Components/PostProcessComponent.h"
+#include "Components/ExponentialHeightFogComponent.h"
+#include "Components/DirectionalLightComponent.h"
+#include "Components/DirectionalLightComponent.h"
+#include "Components/SkyLightComponent.h"
+#include "Components/VolumetricCloudComponent.h"
+#include "Components/SkyAtmosphereComponent.h"
+
 
 ASkyBase::ASkyBase(
 	const FObjectInitializer& ObjectInitializer) :
 	Super(ObjectInitializer),
-	PostProcessComponent(nullptr),
-	ExponentialHeightFogComponent(nullptr),
-	DirectionalLightComponentSun(nullptr),
-	DirectionalLightComponentMoon(nullptr),
-	SkyLightComponent(nullptr),
-	VolumetricCloudComponent(nullptr),
-	SkyAtmosphereComponent(nullptr)
+	PostProcessComponent(CreateDefaultSubobject<UPostProcessComponent>("ASkyBase::PostProcessComponent")),
+	ExponentialHeightFogComponent(CreateDefaultSubobject<UExponentialHeightFogComponent>("ASkyBase::ExponentialHeightFogComponent")),
+	DirectionalLightComponentSun(CreateDefaultSubobject<UDirectionalLightComponent>("ASkyBase::DirectionalLightComponentSun")),
+	DirectionalLightComponentMoon(CreateDefaultSubobject<UDirectionalLightComponent>("ASkyBase::DirectionalLightComponentMoon")),
+	SkyLightComponent(CreateDefaultSubobject<USkyLightComponent>("ASkyBase::SkyLightComponent")),
+	VolumetricCloudComponent(CreateDefaultSubobject<UVolumetricCloudComponent>("ASkyBase::VolumetricCloudComponent")),
+	SkyAtmosphereComponent(CreateDefaultSubobject<USkyAtmosphereComponent>("ASkyBase::SkyAtmosphereComponent"))
 {
 }
