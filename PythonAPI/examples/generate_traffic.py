a38ac46484354cacd445ac799c77329a6fee9847
PabloVD <pablo.villanueva.domingo@gmail.com>
2024-09-30 17:14:14 +0200

    Update comments for generations of vehicles and pedestrians

--- PythonAPI/examples/generate_traffic.py
+++ PythonAPI/examples/generate_traffic.py
@@ -92,7 +92,7 @@ def main():
         '--generationv',
         metavar='G',
         default='All',
-        help='restrict to certain vehicle generation (values: "1","2","All" - default: "All")')
+        help='restrict to certain vehicle generation (values: "2","3","All" - default: "All")')
     argparser.add_argument(
         '--filterw',
         metavar='PATTERN',
@@ -102,7 +102,7 @@ def main():
         '--generationw',
         metavar='G',
         default='2',
-        help='restrict to certain pedestrian generation (values: "1","2","All" - default: "2")')
+        help='restrict to certain pedestrian generation (values: "2","3","All" - default: "2")')
     argparser.add_argument(
         '--tm-port',
         metavar='P',
