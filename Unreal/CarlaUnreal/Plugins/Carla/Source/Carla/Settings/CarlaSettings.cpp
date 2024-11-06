30e17ff5a93c9950f10100b842c57d753974ca5a
MarcelPiNacy-CVC <marpi@cvc.uab.cat>
2024-11-06 11:17:50 +0100

    Upgrade enum to string code.

--- Unreal/CarlaUnreal/Plugins/Carla/Source/Carla/Settings/CarlaSettings.cpp
+++ Unreal/CarlaUnreal/Plugins/Carla/Source/Carla/Settings/CarlaSettings.cpp
@@ -50,12 +50,8 @@ static EQualityLevel QualityLevelFromString(
 
 FString QualityLevelToString(EQualityLevel QualitySettingsLevel)
 {
-  const UEnum *ptr = FindObject<UEnum>(ANY_PACKAGE, TEXT("EQualityLevel"), true);
-  if (!ptr)
-  {
-    return FString("Invalid");
-  }
-  return ptr->GetNameStringByIndex(static_cast<int32>(QualitySettingsLevel));
+  static_assert(TIsEnumClass<EQualityLevel>::Value);
+  return StaticEnum<EQualityLevel>()->GetNameStringByValue((int64)QualitySettingsLevel);
 }
 
 static void LoadSettingsFromConfig(
