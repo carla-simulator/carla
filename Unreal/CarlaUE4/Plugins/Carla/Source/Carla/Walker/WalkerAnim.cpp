36e7819bdc784843a7b27a1e4b26fb352e50f0b7
sxngt <sxngt@icloud.com>
2024-11-21 23:26:52 +0900

    Remove all files except documentation related files

--- Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Walker/WalkerAnim.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2021 Computer Vision Center (CVC) at the Universitat Autonoma
-// de Barcelona (UAB).
-//
-// This work is licensed under the terms of the MIT license.
-// For a copy, see <https://opensource.org/licenses/MIT>.
-
-#include "WalkerAnim.h"
