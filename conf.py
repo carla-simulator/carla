2a50157b5ef9c17ef3886e5a7a260ec3a07d751c
MattRoweEAIF <matt@embodiedaifoundation.org>
2024-11-25 15:53:29 +0100

    removed lightstates from the API

--- conf.py
+++
@@ -1,7 +0,0 @@
-html_theme = 'sphinx_rtd_theme'
-#html_static_path = ['_static']
-html_logo = 'docs/img/logo/carla_logo.png'
-html_theme_options = {
-    'logo_only': True,
-    'display_version': True,
-}
