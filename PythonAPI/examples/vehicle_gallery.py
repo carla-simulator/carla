337efb3a8541a0d4c6453cd150f55d142ecee730
PabloVD <pablo.villanueva.domingo@gmail.com>
2024-12-05 12:40:15 +0100

    - Include description comments at the beginning of each python example - Unify description comments format - Remove some unrequired lines

--- PythonAPI/examples/vehicle_gallery.py
+++ PythonAPI/examples/vehicle_gallery.py
@@ -6,6 +6,7 @@
 # This work is licensed under the terms of the MIT license.
 # For a copy, see <https://opensource.org/licenses/MIT>.
 
+"""Visualize the catalog of vehicles present in CARLA"""
 
 import carla
 
