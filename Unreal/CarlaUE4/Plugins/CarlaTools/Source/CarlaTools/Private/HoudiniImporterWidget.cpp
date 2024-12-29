b3034cf828d664b9b7d32151c410f64311430faf
SakuraHUTB <2165323830@qq.com>
2024-12-29 21:18:59 +0800

    Update HoudiniImporterWidget.cpp

--- Unreal/CarlaUE4/Plugins/CarlaTools/Source/CarlaTools/Private/HoudiniImporterWidget.cpp
+++ Unreal/CarlaUE4/Plugins/CarlaTools/Source/CarlaTools/Private/HoudiniImporterWidget.cpp
@@ -13,188 +13,207 @@
 #include "EditorLevelLibrary.h"
 #include "Components/PrimitiveComponent.h"
 
+// Create sublevels for managing a large map.
+// The implementation is currently empty and needs further development.
 void UHoudiniImporterWidget::CreateSubLevels(ALargeMapManager* LargeMapManager)
 {
-
 }
 
+// Move actors to specific sublevels based on their location in the large map.
 void UHoudiniImporterWidget::MoveActorsToSubLevelWithLargeMap(TArray<AActor*> Actors, ALargeMapManager* LargeMapManager)
 {
-  TMap<FCarlaMapTile*, TArray<AActor*>> ActorsToMove;
-  for (AActor* Actor : Actors)
-  {
-    if (Actor == nullptr) {
-      continue;
-    }
-    UHierarchicalInstancedStaticMeshComponent* Component
-        = Cast<UHierarchicalInstancedStaticMeshComponent>(
-          Actor->GetComponentByClass(
-          UHierarchicalInstancedStaticMeshComponent::StaticClass()));
-    FVector ActorLocation = Actor->GetActorLocation();
-    if (Component)
+    TMap<FCarlaMapTile*, TArray<AActor*>> ActorsToMove;
+
+    // Categorize actors by their respective map tiles.
+    for (AActor* Actor : Actors)
     {
-      ActorLocation = FVector(0);
-      for(int32 i = 0; i < Component->GetInstanceCount(); ++i)
-      {
-        FTransform Transform;
-        Component->GetInstanceTransform(i, Transform, true);
-        ActorLocation = ActorLocation + Transform.GetTranslation();
-      }
-      ActorLocation = ActorLocation / Component->GetInstanceCount();
-    }
-    UE_LOG(LogCarlaTools, Log, TEXT("Actor at location %s"),
-          *ActorLocation.ToString());
-    FCarlaMapTile* Tile = LargeMapManager->GetCarlaMapTile(ActorLocation);
-    if(!Tile)
-    {
-      UE_LOG(LogCarlaTools, Error, TEXT("Error: actor %s in location %s is outside the map"),
-          *Actor->GetName(),*ActorLocation.ToString());
-      continue;
+        if (!Actor) continue;
+
+        // Check for hierarchical instanced mesh components and calculate location.
+        UHierarchicalInstancedStaticMeshComponent* Component = Cast<UHierarchicalInstancedStaticMeshComponent>(
+            Actor->GetComponentByClass(UHierarchicalInstancedStaticMeshComponent::StaticClass()));
+        FVector ActorLocation = Actor->GetActorLocation();
+
+        if (Component)
+        {
+            // Calculate average location for all instances in the mesh.
+            ActorLocation = FVector(0);
+            for (int32 i = 0; i < Component->GetInstanceCount(); ++i)
+            {
+                FTransform Transform;
+                Component->GetInstanceTransform(i, Transform, true);
+                ActorLocation += Transform.GetTranslation();
+            }
+            ActorLocation /= Component->GetInstanceCount();
+        }
+
+        // Log actor location and determine the corresponding map tile.
+        UE_LOG(LogCarlaTools, Log, TEXT("Actor at location %s"), *ActorLocation.ToString());
+        FCarlaMapTile* Tile = LargeMapManager->GetCarlaMapTile(ActorLocation);
+
+        if (!Tile)
+        {
+            UE_LOG(LogCarlaTools, Error, TEXT("Error: actor %s in location %s is outside the map"), *Actor->GetName(), *ActorLocation.ToString());
+            continue;
+        }
+
+        // Update actor coordinates based on tile origin.
+        if (Component)
+        {
+            UpdateInstancedMeshCoordinates(Component, Tile->Location);
+        }
+        else
+        {
+            UpdateGenericActorCoordinates(Actor, Tile->Location);
+        }
+
+        // Add the actor to the corresponding tile's list.
+        ActorsToMove.FindOrAdd(Tile).Add(Actor);
     }
 
-    if(Component)
+    // Move categorized actors to their respective levels.
+    for (auto& Element : ActorsToMove)
     {
-      UpdateInstancedMeshCoordinates(Component, Tile->Location);
+        FCarlaMapTile* Tile = Element.Key;
+        TArray<AActor*> ActorList = Element.Value;
+
+        if (ActorList.Num() == 0) continue;
+
+        UWorld* World = UEditorLevelLibrary::GetEditorWorld();
+        ULevelStreamingDynamic* StreamingLevel = Tile->StreamingLevel;
+
+        StreamingLevel->bShouldBlockOnLoad = true;
+        StreamingLevel->SetShouldBeVisible(true);
+        StreamingLevel->SetShouldBeLoaded(true);
+
+        ULevelStreaming* Level = UEditorLevelUtils::AddLevelToWorld(
+            World, *Tile->Name, ULevelStreamingDynamic::StaticClass(), FTransform());
+
+        int MovedActors = UEditorLevelUtils::MoveActorsToLevel(ActorList, Level, false, false);
+
+        UE_LOG(LogCarlaTools, Log, TEXT("Moved %d actors"), MovedActors);
+
+        // Save and unload the level.
+        FEditorFileUtils::SaveDirtyPackages(false, true, true, false, false, false, nullptr);
+        UEditorLevelUtils::RemoveLevelFromWorld(Level->GetLoadedLevel());
     }
-    else
+
+    // Perform garbage collection and cleanup.
+    GEngine->PerformGarbageCollectionAndCleanupActors();
+    FText TransResetText(FText::FromString("Clean up after Move actors to sublevels"));
+
+    if (GEditor->Trans)
     {
-      UpdateGenericActorCoordinates(Actor, Tile->Location);
+        GEditor->Trans->Reset(TransResetText);
+        GEditor->Cleanse(true, true, TransResetText);
     }
-    ActorsToMove.FindOrAdd(Tile).Add(Actor);
-  }
-
-  for (auto& Element : ActorsToMove)
-  {
-    FCarlaMapTile* Tile = Element.Key;
-    TArray<AActor*> ActorList = Element.Value;
-    if(!ActorList.Num())
-    {
-      continue;
-    }
-
-    UWorld* World = UEditorLevelLibrary::GetEditorWorld();
-    ULevelStreamingDynamic* StreamingLevel = Tile->StreamingLevel;
-    StreamingLevel->bShouldBlockOnLoad = true;
-    StreamingLevel->SetShouldBeVisible(true);
-    StreamingLevel->SetShouldBeLoaded(true);
-    ULevelStreaming* Level =
-        UEditorLevelUtils::AddLevelToWorld(
-        World, *Tile->Name, ULevelStreamingDynamic::StaticClass(), FTransform());
-    int MovedActors = UEditorLevelUtils::MoveActorsToLevel(ActorList, Level, false, false);
-    // StreamingLevel->SetShouldBeLoaded(false);
-    UE_LOG(LogCarlaTools, Log, TEXT("Moved %d actors"), MovedActors);
-    FEditorFileUtils::SaveDirtyPackages(false, true, true, false, false, false, nullptr);
-    UEditorLevelUtils::RemoveLevelFromWorld(Level->GetLoadedLevel());
-  }
-
-  GEngine->PerformGarbageCollectionAndCleanupActors();
-  FText TransResetText(FText::FromString("Clean up after Move actors to sublevels"));
-  if ( GEditor->Trans )
-  {
-    GEditor->Trans->Reset(TransResetText);
-    GEditor->Cleanse(true, true, TransResetText);
-  }
 }
 
-void UHoudiniImporterWidget::ForceStreamingLevelsToUnload( ALargeMapManager* LargeMapManager )
+// Force the unloading of all streaming levels managed by the large map manager.
+void UHoudiniImporterWidget::ForceStreamingLevelsToUnload(ALargeMapManager* LargeMapManager)
 {
-  UWorld* World = UEditorLevelLibrary::GetGameWorld();
+    UWorld* World = UEditorLevelLibrary::GetGameWorld();
+    FIntVector NumTilesInXY = LargeMapManager->GetNumTilesInXY();
 
-  FIntVector NumTilesInXY  = LargeMapManager->GetNumTilesInXY();
-
-  for(int x = 0; x < NumTilesInXY.X; ++x)
-  {
-    for(int y = 0; y < NumTilesInXY.Y; ++y)
+    for (int x = 0; x < NumTilesInXY.X; ++x)
     {
-      FIntVector CurrentTileVector(x, y, 0);
-      FCarlaMapTile CarlaTile = LargeMapManager->GetCarlaMapTile(CurrentTileVector);
-      ULevelStreamingDynamic* StreamingLevel = CarlaTile.StreamingLevel;
-      ULevelStreaming* Level =
-        UEditorLevelUtils::AddLevelToWorld(
-        World, *CarlaTile.Name, ULevelStreamingDynamic::StaticClass(), FTransform());
-      FEditorFileUtils::SaveDirtyPackages(false, true, true, false, false, false, nullptr);
-      UEditorLevelUtils::RemoveLevelFromWorld(Level->GetLoadedLevel());
-    }
-  }
+        for (int y = 0; y < NumTilesInXY.Y; ++y)
+        {
+            FIntVector CurrentTileVector(x, y, 0);
+            FCarlaMapTile CarlaTile = LargeMapManager->GetCarlaMapTile(CurrentTileVector);
 
+            ULevelStreamingDynamic* StreamingLevel = CarlaTile.StreamingLevel;
+            ULevelStreaming* Level = UEditorLevelUtils::AddLevelToWorld(
+                World, *CarlaTile.Name, ULevelStreamingDynamic::StaticClass(), FTransform());
+
+            FEditorFileUtils::SaveDirtyPackages(false, true, true, false, false, false, nullptr);
+            UEditorLevelUtils::RemoveLevelFromWorld(Level->GetLoadedLevel());
+        }
+    }
 }
 
+// Move a list of actors to a specified sublevel.
 void UHoudiniImporterWidget::MoveActorsToSubLevel(TArray<AActor*> Actors, ULevelStreaming* Level)
 {
-  int MovedActors = UEditorLevelUtils::MoveActorsToLevel(Actors, Level, false, false);
+    int MovedActors = UEditorLevelUtils::MoveActorsToLevel(Actors, Level, false, false);
 
-  UE_LOG(LogCarlaTools, Log, TEXT("Moved %d actors"), MovedActors);
-  FEditorFileUtils::SaveDirtyPackages(false, true, true, false, false, false, nullptr);
-  UEditorLevelUtils::RemoveLevelFromWorld(Level->GetLoadedLevel());
-  FText TransResetText(FText::FromString("Clean up after Move actors to sublevels"));
-  if ( GEditor->Trans )
-  {
-    GEditor->Trans->Reset(TransResetText);
-  }
+    UE_LOG(LogCarlaTools, Log, TEXT("Moved %d actors"), MovedActors);
+
+    FEditorFileUtils::SaveDirtyPackages(false, true, true, false, false, false, nullptr);
+    UEditorLevelUtils::RemoveLevelFromWorld(Level->GetLoadedLevel());
+
+    FText TransResetText(FText::FromString("Clean up after Move actors to sublevels"));
+    if (GEditor->Trans)
+    {
+        GEditor->Trans->Reset(TransResetText);
+    }
 }
 
-void UHoudiniImporterWidget::UpdateGenericActorCoordinates(
-    AActor* Actor, FVector TileOrigin)
+// Update actor coordinates to be relative to the given tile origin.
+void UHoudiniImporterWidget::UpdateGenericActorCoordinates(AActor* Actor, FVector TileOrigin)
 {
-  FVector LocalLocation = Actor->GetActorLocation() - TileOrigin;
-  Actor->SetActorLocation(LocalLocation);
-  UE_LOG(LogCarlaTools, Log, TEXT("%s New location %s"),
-    *Actor->GetName(), *LocalLocation.ToString());
+    FVector LocalLocation = Actor->GetActorLocation() - TileOrigin;
+    Actor->SetActorLocation(LocalLocation);
+
+    UE_LOG(LogCarlaTools, Log, TEXT("%s New location %s"), *Actor->GetName(), *LocalLocation.ToString());
 }
 
-void UHoudiniImporterWidget::UpdateInstancedMeshCoordinates(
-    UHierarchicalInstancedStaticMeshComponent* Component,  FVector TileOrigin)
+// Update the transforms of all instances in a hierarchical instanced mesh component.
+void UHoudiniImporterWidget::UpdateInstancedMeshCoordinates(UHierarchicalInstancedStaticMeshComponent* Component, FVector TileOrigin)
 {
-  TArray<FTransform> NewTransforms;
-  for(int32 i = 0; i < Component->GetInstanceCount(); ++i)
-  {
-    FTransform Transform;
-    Component->GetInstanceTransform(i, Transform, true);
-    Transform.AddToTranslation(-TileOrigin);
-    NewTransforms.Add(Transform);
-    UE_LOG(LogCarlaTools, Log, TEXT("New instance location %s"),
-        *Transform.GetTranslation().ToString());
-  }
-  Component->BatchUpdateInstancesTransforms(0, NewTransforms, true, true, true);
+    TArray<FTransform> NewTransforms;
+
+    for (int32 i = 0; i < Component->GetInstanceCount(); ++i)
+    {
+        FTransform Transform;
+        Component->GetInstanceTransform(i, Transform, true);
+        Transform.AddToTranslation(-TileOrigin);
+        NewTransforms.Add(Transform);
+
+        UE_LOG(LogCarlaTools, Log, TEXT("New instance location %s"), *Transform.GetTranslation().ToString());
+    }
+
+    Component->BatchUpdateInstancesTransforms(0, NewTransforms, true, true, true);
 }
 
+// Set actor location to its center of mass for a given list of actors.
 void UHoudiniImporterWidget::UseCOMasActorLocation(TArray<AActor*> Actors)
 {
-  for (AActor* Actor : Actors)
-  {
-    UPrimitiveComponent* Primitive = Cast<UPrimitiveComponent>(
-        Actor->GetComponentByClass(UPrimitiveComponent::StaticClass()));
-    if(Primitive)
+    for (AActor* Actor : Actors)
     {
-      FBodyInstance* BodyInstance = Primitive->GetBodyInstance();
-      FVector CenterOfMass = BodyInstance->COMNudge;
-      Actor->SetActorLocation(CenterOfMass);
+        UPrimitiveComponent* Primitive = Cast<UPrimitiveComponent>(
+            Actor->GetComponentByClass(UPrimitiveComponent::StaticClass()));
+
+        if (Primitive)
+        {
+            FBodyInstance* BodyInstance = Primitive->GetBodyInstance();
+            FVector CenterOfMass = BodyInstance->COMNudge;
+            Actor->SetActorLocation(CenterOfMass);
+        }
     }
-  }
 }
 
-bool UHoudiniImporterWidget::GetNumberOfClusters(
-    TArray<AActor*> ActorList, int& OutNumClusters)
+// Extract the number of clusters based on actor naming convention.
+bool UHoudiniImporterWidget::GetNumberOfClusters(TArray<AActor*> ActorList, int& OutNumClusters)
 {
-
-  for (AActor* Actor : ActorList)
-  {
-    FString ObjectName = UKismetSystemLibrary::GetObjectName(Actor);
-    UE_LOG(LogCarlaTools, Log, TEXT("Searching in string %s"), *ObjectName);
-    if(ObjectName.StartsWith("b"))
+    for (AActor* Actor : ActorList)
     {
-      int Index = ObjectName.Find("of");
-      if(Index == -1)
-      {
-        continue;
-      }
-      UE_LOG(LogCarlaTools, Log, TEXT("found of substr at %d"), Index);
-      FString NumClusterString = ObjectName.Mid(Index+2, ObjectName.Len());
-      OutNumClusters = FCString::Atoi(*NumClusterString);
-      return true;
+        FString ObjectName = UKismetSystemLibrary::GetObjectName(Actor);
+        UE_LOG(LogCarlaTools, Log, TEXT("Searching in string %s"), *ObjectName);
+
+        if (ObjectName.StartsWith("b"))
+        {
+            int Index = ObjectName.Find("of");
+            if (Index == -1) continue;
+
+            UE_LOG(LogCarlaTools, Log, TEXT("found 'of' substring at %d"), Index);
+            FString NumClusterString = ObjectName.Mid(Index + 2, ObjectName.Len());
+            OutNumClusters = FCString::Atoi(*NumClusterString);
+            return true;
+        }
     }
-  }
-  UE_LOG(LogCarlaTools, Warning, TEXT("Num clusters not found"));
-  OutNumClusters = -1;
-  return false;
+
+    UE_LOG(LogCarlaTools, Warning, TEXT("Num clusters not found"));
+    OutNumClusters = -1;
+    return false;
 }
