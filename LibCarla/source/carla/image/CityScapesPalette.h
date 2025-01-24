eaa5122b654ae0b7e13cdb98cee234cf88d48b94
yuanhaorannnnnn <37274406+yuanhaorannnnnn@users.noreply.github.com>
2025-01-24 18:28:18 +0800

    add rock/stone tag (#8598)
    Co-authored-by: yhr <yuanhaoran@synkrotron.ai>

--- LibCarla/source/carla/image/CityScapesPalette.h
+++ LibCarla/source/carla/image/CityScapesPalette.h
@@ -48,7 +48,8 @@ namespace detail {
         { 81u,   0u,  81u},   // ground        =  25u
         {150u, 100u, 100u},   // bridge        =  26u
         {230u, 150u, 140u},   // rail track    =  27u
-        {180u, 165u, 180u}    // guard rail    =  28u
+        {180u, 165u, 180u},   // guard rail    =  28u
+        {180u, 130u,  70u},   // rock          =  29u
       };
 
 } // namespace detail
