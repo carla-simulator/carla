2af7b5aee285e84e3bfd082dd9b0e2ceb5940d3b
Abhishek Dwivedi <abhishekkumardwivedi@users.noreply.github.com>
2025-02-06 20:23:13 +0530

    Update CarlaSetup.sh for code readability

--- CarlaSetup.sh
+++ CarlaSetup.sh
@@ -1,22 +1,21 @@
-#! /bin/bash
+#!/usr/bin/env bash
 
+# Exit immediately if a command exits with a non-zero status.
 set -e
 
+# Initialize variables with default values.
 interactive=0
 skip_prerequisites=0
 launch=0
-python_root=
+python_root=""
 
-workspace_path="$(dirname $(realpath "${BASH_SOURCE[-1]}"))"
-echo "workspace_path=$workspace_path"
+# Determine the workspace path.
+workspace_path="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+echo "workspace_path=${workspace_path}"
 
-options=$(\
-    getopt \
-    -o "i,p,l,pyroot:" \
-    --long "interactive,skip-prerequisites,launch,python-root:" \
-    -n 'CarlaSetup.sh' -- "$@")
-
-eval set -- "$options"
+# Parse command-line options.
+options=$(getopt -o "ipl" --long "interactive,skip-prerequisites,launch,python-root:" -n 'CarlaSetup.sh' -- "$@")
+eval set -- "${options}"
 while true; do
     case "$1" in
         -i|--interactive)
@@ -31,8 +30,8 @@ while true; do
             launch=1
             shift
             ;;
-        -pyroot|--python-root)
-            python_root=$2
+        --python-root)
+            python_root="$2"
             shift 2
             ;;
         --)
@@ -40,109 +39,121 @@ while true; do
             break
             ;;
         *)
+            echo "Invalid option: $1"
+            exit 1
             ;;
     esac
 done
 
-# Check for root privileges:
-if [ -z "$EUID" ]; then
-    EUID=$(id -u)
-fi
-if [ "$EUID" -ne 0 ]; then
-    if [ $interactive -eq 0 ]; then
-        if [ $skip_prerequisites -eq 0 ]; then
-            echo "Please run this script as root. Otherwise pass --interactive to be prompted whenever root privileges or Git credentials are needed."
+# Function to check for root privileges.
+check_root() {
+    if [[ "$EUID" -ne 0 ]]; then
+        if [[ $interactive -eq 0 && $skip_prerequisites -eq 0 ]]; then
+            echo "Please run this script as root or pass --interactive to be prompted for privileges."
             exit 1
         fi
     fi
-fi
+}
 
-# Check for Git credentials:
-if [ -z "$GIT_LOCAL_CREDENTIALS" ]; then
-    if [ $interactive -eq 1 ]; then
-        echo "Warning: git credentials are not set. You may be required to manually enter them later."
+# Function to check for Git credentials.
+check_git_credentials() {
+    if [[ -z "$GIT_LOCAL_CREDENTIALS" ]]; then
+        if [[ $interactive -eq 1 ]]; then
+            echo "Warning: Git credentials are not set. You may be prompted later."
+        else
+            echo "Git credentials are not set; cannot continue in unattended mode."
+            exit 1
+        fi
     else
-        echo "Git credentials are not set, can not continue setup in unattended mode."
-        exit 1
+        echo "Found Git credentials."
     fi
-else
-    echo "Found git credentials."
-fi
+}
 
-# -- PREREQUISITES INSTALL STEP --
-if [ $skip_prerequisites -eq 0 ]; then
-    python_path=python3
-    if [ "$python_root" != "" ]; then
-        python_path=${python_root}/python3
+# Function to install prerequisites.
+install_prerequisites() {
+    local python_path="python3"
+    if [[ -n "$python_root" ]]; then
+        python_path="${python_root}/python3"
     fi
     echo "Installing prerequisites..."
-    sudo -E bash -x Util/SetupUtils/InstallPrerequisites.sh --python-path=$python_path
+    sudo -E bash -x Util/SetupUtils/InstallPrerequisites.sh --python-path="$python_path"
+}
+
+# Function to clone CARLA content.
+clone_carla_content() {
+    if [[ -d "${workspace_path}/Unreal/CarlaUnreal/Content" ]]; then
+        echo "Found CARLA content."
+    else
+        echo "Could not find CARLA content. Downloading..."
+        mkdir -p "${workspace_path}/Unreal/CarlaUnreal/Content"
+        git -C "${workspace_path}/Unreal/CarlaUnreal/Content" clone -b ue5-dev https://bitbucket.org/carla-simulator/carla-content.git Carla
+    fi
+}
+
+# Function to download and build Unreal Engine.
+build_unreal_engine() {
+    export CARLA_UNREAL_ENGINE_PATH="${workspace_path}/UnrealEngine5_carla"
+    echo "CARLA_UNREAL_ENGINE_PATH=${CARLA_UNREAL_ENGINE_PATH}"
+
+    if [[ -d "${CARLA_UNREAL_ENGINE_PATH}" ]]; then
+        echo "Found CARLA Unreal Engine at ${CARLA_UNREAL_ENGINE_PATH}"
+    else
+        echo "Could not find CARLA Unreal Engine, downloading..."
+        pushd ..
+        local ue5_url="https://github.com/CarlaUnreal/UnrealEngine.git"
+        if [[ -n "$GIT_LOCAL_CREDENTIALS" ]]; then
+            IFS='@' read -r git_user git_token <<< "$GIT_LOCAL_CREDENTIALS"
+            ue5_url="https://${git_user}:${git_token}@github.com/CarlaUnreal/UnrealEngine.git"
+        fi
+        git clone -b ue5-dev-carla "$ue5_url" UnrealEngine5_carla
+        pushd UnrealEngine5_carla
+        echo -e '\n# CARLA UnrealEngine5\nexport CARLA_UNREAL_ENGINE_PATH='"$PWD" >> ~/.bashrc
+        export CARLA_UNREAL_ENGINE_PATH="$PWD"
+        echo "Running Unreal Engine pre-build steps..."
+        bash -x Setup.sh
+        bash -x GenerateProjectFiles.sh
+        echo "Building Unreal Engine 5..."
+        make
+        popd
+        popd
+    fi
+}
+
+# Function to build CARLA.
+build_carla() {
+    echo "Configuring the CARLA CMake project..."
+    cmake -G Ninja -S . -B Build \
+        --toolchain="$PWD/CMake/Toolchain.cmake" \
+        -DLAUNCH_ARGS="-prefernvidia" \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DENABLE_ROS2=ON \
+        -DPython_ROOT_DIR="${python_root}" \
+        -DPython3_ROOT_DIR="${python_root}" \
+        -DCARLA_UNREAL_ENGINE_PATH="${CARLA_UNREAL_ENGINE_PATH}"
+    echo "Building CARLA..."
+    cmake --build Build
+    echo "Installing Python API..."
+    cmake --build Build --target carla-python-api-install
+    echo "CARLA Python API build+install succeeded."
+}
+
+# Function to launch CARLA.
+launch_carla() {
+    if [[ $launch -eq 1 ]]; then
+        echo "Launching Carla - Unreal Editor..."
+        cmake --build Build --target launch
+    fi
+}
+
+# Main script execution.
+check_root
+check_git_credentials
+if [[ $skip_prerequisites -eq 0 ]]; then
+    install_prerequisites
 else
     echo "Skipping prerequisites install step."
 fi
-
-# -- CLONE CONTENT --
-if [ -d $workspace_path/Unreal/CarlaUnreal/Content ]; then
-    echo "Found CARLA content."
-else
-    echo "Could not find CARLA content. Downloading..."
-    mkdir -p $workspace_path/Unreal/CarlaUnreal/Content
-    git \
-        -C $workspace_path/Unreal/CarlaUnreal/Content \
-        clone \
-        -b ue5-dev \
-        https://bitbucket.org/carla-simulator/carla-content.git \
-        Carla
-fi
-
-# -- DOWNLOAD + BUILD UNREAL ENGINE --
-if [ ! -z $CARLA_UNREAL_ENGINE_PATH ] && [ -d $CARLA_UNREAL_ENGINE_PATH ]; then
-    echo "Found CARLA Unreal Engine at $CARLA_UNREAL_ENGINE_PATH"
-elif [ -d ../UnrealEngine5_carla ]; then
-    echo "Found CARLA Unreal Engine at $workspace_path/UnrealEngine5_carla. Assuming already built..."
-else
-    echo "Could not find CARLA Unreal Engine, downloading..."
-    pushd ..
-    if [ -z "$GIT_LOCAL_CREDENTIALS" ]
-    then
-        UE5_URL=https://github.com/CarlaUnreal/UnrealEngine.git
-    else
-        GIT_CREDENTIALS_INFO=(${GIT_LOCAL_CREDENTIALS//@/ })
-        GIT_LOCAL_USER=${GIT_CREDENTIALS_INFO[0]}
-        GIT_LOCAL_TOKEN=${GIT_CREDENTIALS_INFO[1]}
-        UE5_URL=https://$GIT_LOCAL_USER:$GIT_LOCAL_TOKEN@github.com/CarlaUnreal/UnrealEngine.git
-    fi
-    git clone -b ue5-dev-carla $UE5_URL UnrealEngine5_carla
-    pushd UnrealEngine5_carla
-    echo -e '\n#CARLA UnrealEngine5\nexport CARLA_UNREAL_ENGINE_PATH='$PWD >> ~/.bashrc
-    export CARLA_UNREAL_ENGINE_PATH=$PWD
-    echo "Running Unreal Engine pre-build steps..."
-    bash -x Setup.sh
-    bash -x GenerateProjectFiles.sh
-    echo "Building Unreal Engine 5..."
-    make
-    popd
-    popd
-fi
-
-# -- BUILD CARLA --
-echo "Configuring the CARLA CMake project..."
-cmake -G Ninja -S . -B Build \
-    --toolchain=$PWD/CMake/Toolchain.cmake \
-    -DLAUNCH_ARGS="-prefernvidia" \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DENABLE_ROS2=ON \
-    -DPython_ROOT_DIR=${python_root} \
-    -DPython3_ROOT_DIR=${python_root} \
-    -DCARLA_UNREAL_ENGINE_PATH=$CARLA_UNREAL_ENGINE_PATH
-echo "Building CARLA..."
-cmake --build Build
-echo "Installing Python API..."
-cmake --build Build --target carla-python-api-install
-echo "CARLA Python API build+install succeeded."
-
-# -- POST-BUILD STEPS --
-if [ $launch -eq 1 ]; then
-    echo "Launching Carla - Unreal Editor..."
-    cmake --build Build --target launch
-fi
+clone_carla_content
+build_unreal_engine
+build_carla
+launch_carla
