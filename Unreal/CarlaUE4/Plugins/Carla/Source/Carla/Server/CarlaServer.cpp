ee3a9ad7987564805d8671f4c72a1097b1bcc504
Blyron <53337103+Blyron@users.noreply.github.com>
2025-04-28 09:42:06 +0200

    Return 0exntent BB when actor is destroyed (#8872)

--- Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Server/CarlaServer.cpp
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Server/CarlaServer.cpp
@@ -1348,21 +1348,16 @@ BIND_SYNC(is_sensor_enabled_for_ros) << [this](carla::streaming::detail::stream_
   BIND_SYNC(get_actor_bounding_box) << [this](
       cr::ActorId ActorId) -> R<cr::BoundingBox>
   {
-    return cr::BoundingBox();
+    //return cr::BoundingBox();
     // Commenting it out due to an unknown bug where sometimes the server tryes to act on a destroyed actor, crashing the simulation.
 
-    // REQUIRE_CARLA_EPISODE();
-    // FCarlaActor* CarlaActor = Episode->FindCarlaActor(ActorId);
-    // if (!CarlaActor)
-    // {
-    //   return RespondError(
-    //       "get_actor_bounding_box",
-    //       ECarlaServerResponse::ActorNotFound,
-    //       " Actor Id: " + FString::FromInt(ActorId));
-    // }
-    // FBoundingBox bounding_box = UBoundingBoxCalculator::GetActorBoundingBox(CarlaActor->GetActor(), 0);
-    // bounding_box.ActorId = CarlaActor->GetActorId();
-    // return cr::BoundingBox(bounding_box);
+    REQUIRE_CARLA_EPISODE();
+    FCarlaActor* CarlaActor = Episode->FindCarlaActor(ActorId);
+    if (!CarlaActor)
+      return cr::BoundingBox();
+    FBoundingBox bounding_box = UBoundingBoxCalculator::GetActorBoundingBox(CarlaActor->GetActor(), 0);
+    bounding_box.ActorId = CarlaActor->GetActorId();
+    return cr::BoundingBox(bounding_box);
   };
 
   BIND_SYNC(get_actor_component_world_transform) << [this](
