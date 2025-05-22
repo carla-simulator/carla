14ebc88e127995ab3d2f71031a67595ab636a3bb
Blyron <53337103+Blyron@users.noreply.github.com>
2025-05-22 15:56:05 +0200

    Update CarlaStatics.h

--- Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Game/CarlaStatics.h
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Game/CarlaStatics.h
@@ -51,12 +51,6 @@ public:
   UFUNCTION(BlueprintPure, Category="CARLA", meta=(WorldContext="WorldContextObject"))
   static ACarlaRecorder* GetRecorder(const UObject *WorldContextObject);
 
-  UFUNCTION(BlueprintPure, Category="CARLA")
-  static TArray<FString> GetAllPluginContentPaths();
-
-  UFUNCTION(BlueprintPure, Category="CARLA")
-  static FString FindMapPath(const FString &MapName);
-
   static CarlaReplayer* GetReplayer(const UObject *WorldContextObject);
 
   UFUNCTION(BlueprintPure, Category="CARLA", meta=(WorldContext="WorldContextObject"))
