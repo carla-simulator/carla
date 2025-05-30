5fa5f4e2e1691b1c6648ff0e53b8514617a44cab
Antonio Mata <124166696+AntonioMata1@users.noreply.github.com>
2025-05-30 11:21:42 +0200

    Geometry importer quick fix (#8949)
    Co-authored-by: Antonio Mata <artoniodev@gmail.com>

--- Unreal/CarlaUnreal/Plugins/Carla/Source/Carla/Util/GeometryImporter.h
+++ Unreal/CarlaUnreal/Plugins/Carla/Source/Carla/Util/GeometryImporter.h
@@ -6,18 +6,17 @@
 #include "GeometryImporter.generated.h"
 
 UCLASS(Blueprintable, BlueprintType)
-class CARLADIGITALTWINSTOOL_API UGeometryImporter : public UBlueprintFunctionLibrary
+class CARLA_API UGeometryImporter : public UBlueprintFunctionLibrary
 {
     GENERATED_BODY()
 
 public:
-
     UFUNCTION(BlueprintCallable, Category = "GeometryImporter")
     TArray<FVector2D> ReadCSVCoordinates(FString Path, FVector2D OriginGeoCoordinates);
 
     UFUNCTION(BlueprintCallable, Category = "GeometryImporter")
-    static USplineComponent* CreateSpline(UWorld* World, const TArray<FVector>& Points, const FString SplineName);
+    static USplineComponent *CreateSpline(UWorld *World, const TArray<FVector> &Points, const FString SplineName);
 
     UFUNCTION(BlueprintCallable, Category = "GeometryImporter")
-    static TArray<USplineComponent*> ImportGeoJsonPolygonsToSplines(UWorld* World, const FString& GeoJsonFilePath, const FVector2D OriginGeoCoordinates);
+    static TArray<USplineComponent *> ImportGeoJsonPolygonsToSplines(UWorld *World, const FString &GeoJsonFilePath, const FVector2D OriginGeoCoordinates);
 };
