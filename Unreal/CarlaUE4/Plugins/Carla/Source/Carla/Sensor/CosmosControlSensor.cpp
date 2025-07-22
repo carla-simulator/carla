90f4fcc9a38de6fb09d9986b71205738dc522592
Jorge Virgos <jorgevirgos.dev@gmail.com>
2025-07-22 10:18:05 +0200

    Uploading empty Sensor class for testing

---
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Sensor/CosmosControlSensor.cpp
@@ -0,0 +1,5 @@
+// Copyright (c) 2017 Computer Vision Center (CVC) at the Universitat Autonoma de Barcelona (UAB). This work is licensed under the terms of the MIT license. For a copy, see <https://opensource.org/licenses/MIT>.
+
+
+#include "Sensor/CosmosControlSensor.h"
+
