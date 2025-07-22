51fb32c9e05d7120084b8e7c09f1afd75a951522
Jorge Virgos <jorgevirgos.dev@gmail.com>
2025-07-22 10:55:55 +0200

    Standard functions added to the sensor

--- Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Sensor/CosmosControlSensor.cpp
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Sensor/CosmosControlSensor.cpp
@@ -1,5 +1,66 @@
-// Copyright (c) 2017 Computer Vision Center (CVC) at the Universitat Autonoma de Barcelona (UAB). This work is licensed under the terms of the MIT license. For a copy, see <https://opensource.org/licenses/MIT>.
+// Copyright (c) 2025 Computer Vision Center (CVC) at the Universitat Autonoma
+// de Barcelona (UAB).
+//
+// This work is licensed under the terms of the MIT license.
+// For a copy, see <https://opensource.org/licenses/MIT>.
 
-
+#include "Carla.h"
 #include "Sensor/CosmosControlSensor.h"
+#include "Carla/Actor/ActorBlueprintFunctionLibrary.h"
 
+#include "Carla/Sensor/PixelReader.h"
+
+#include "Components/SceneCaptureComponent2D.h"
+
+
+FActorDefinition ACosmosControlSensor::GetSensorDefinition()
+{
+  return UActorBlueprintFunctionLibrary::MakeCameraDefinition(TEXT("cosmos_visualization"));
+}
+
+ACosmosControlSensor::ACosmosControlSensor(
+    const FObjectInitializer &ObjectInitializer)
+  : Super(ObjectInitializer)
+{
+  AddPostProcessingMaterial(TEXT("Material'/Carla/PostProcessingMaterials/PhysicLensDistortion.PhysicLensDistortion'"));
+  // TODO: Setup OnActorSpawnHandler so we can refresh components
+  // World->AddOnActorSpawnedHandler(FOnActorSpawned::FDelegate::CreateRaw(this, &ACosmosControlSensor::OnActorSpawned));
+}
+
+void ACosmosControlSensor::SetUpSceneCaptureComponent(USceneCaptureComponent2D &SceneCapture)
+{
+  Super::SetUpSceneCaptureComponent(SceneCapture);
+
+  ApplyViewMode(VMI_Unlit, true, SceneCapture.ShowFlags);
+
+  SceneCapture.ShowFlags.SetAtmosphere(false);
+
+  SceneCapture.PrimitiveRenderMode = ESceneCapturePrimitiveRenderMode::PRM_UseShowOnlyList;
+
+//   TArray<UObject *> StaticMeshComps;
+//   GetObjectsOfClass(UStaticMeshComponent::StaticClass(), StaticMeshComps, false, EObjectFlags::RF_ClassDefaultObject, EInternalObjectFlags::AllFlags);
+
+//   TArray<UPrimitiveComponent *> ShowOnlyComponents;
+//   for (UObject *Object : StaticMeshComps) {
+//     UPrimitiveComponent *Component = Cast<UStaticMeshComponent>(Object);
+//     SceneCapture.ShowOnlyComponents.Emplace(Component);
+//   }
+}
+
+void ACosmosControlSensor::PostPhysTick(UWorld *World, ELevelTick TickType, float DeltaSeconds)
+{
+  TRACE_CPUPROFILER_EVENT_SCOPE(ACosmosControlSensor::PostPhysTick);
+
+  USceneCaptureComponent2D* SceneCapture = GetCaptureComponent2D();
+//   TArray<UObject *> StaticMeshComps;
+//   GetObjectsOfClass(UStaticMeshComponent::StaticClass(), StaticMeshComps, false, EObjectFlags::RF_ClassDefaultObject, EInternalObjectFlags::AllFlags);
+
+//   TArray<UPrimitiveComponent *> ShowOnlyComponents;
+//   for (UObject *Object : StaticMeshComps) {
+//     UPrimitiveComponent *Component = Cast<UStaticMeshComponent>(Object);
+//     SceneCapture.ShowOnlyComponents.Emplace(Component);
+//   }
+
+  FPixelReader::SendPixelsInRenderThread<ACosmosControlSensor, FColor>(*this);
+
+}
--- Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Sensor/CosmosControlSensor.h
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Sensor/CosmosControlSensor.h
@@ -1,17 +1,32 @@
-// Copyright (c) 2017 Computer Vision Center (CVC) at the Universitat Autonoma de Barcelona (UAB). This work is licensed under the terms of the MIT license. For a copy, see <https://opensource.org/licenses/MIT>.
+// Copyright (c) 2025 Computer Vision Center (CVC) at the Universitat Autonoma
+// de Barcelona (UAB).
+//
+// This work is licensed under the terms of the MIT license.
+// For a copy, see <https://opensource.org/licenses/MIT>.
 
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Sensor/Sensor.h"
+#include "Carla/Actor/ActorDefinition.h"
 #include "CosmosControlSensor.generated.h"
 
 /**
- * 
+ * Sensor that produces an input control visualization for Cosmos
  */
 UCLASS()
 class CARLA_API ACosmosControlSensor : public ASensor
 {
 	GENERATED_BODY()
-	
+
+public:
+
+	static FActorDefinition GetSensorDefinition();
+
+  	ACosmosControlSensor(const FObjectInitializer &ObjectInitializer);
+
+protected:
+
+  void SetUpSceneCaptureComponent(USceneCaptureComponent2D &SceneCapture) override;
+  void PostPhysTick(UWorld *World, ELevelTick TickType, float DeltaSeconds) override;
 };
