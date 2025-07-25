1e00fab95002643c3f7a52dbd79a205484c17648
Jorge Virgos <jorgevirgos.dev@gmail.com>
2025-07-25 09:50:01 +0200

    Reverted Changes to game mode base

--- Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Game/CarlaGameModeBase.cpp
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Game/CarlaGameModeBase.cpp
@@ -20,8 +20,6 @@
 #include "EngineUtils.h"
 #include "Tagger.h"
 #include "TaggedComponent.h"
-#include "Carla/Traffic/TrafficLightBase.h"
-#include "carla/Traffic/RoutePlanner.h"
 
 #include <compiler/disable-ue4-macros.h>
 #include "carla/opendrive/OpenDriveParser.h"
@@ -37,8 +35,6 @@
 #include "DrawDebugHelpers.h"
 #include "Kismet/KismetSystemLibrary.h"
 
-#include "carla/Game/Tagger.h"
-#include "carla/road/Map.h"
 
 namespace cr = carla::road;
 namespace crp = carla::rpc;
@@ -401,145 +397,6 @@ void ACarlaGameModeBase::Tick(float DeltaSeconds)
   {
     Recorder->Tick(DeltaSeconds);
   }
-
-  //static bool first_rendering = false;
-  //int depth_prio = 0;
-
-  //// if(first_rendering == false)
-  //// {
-  ////   for (TActorIterator<AActor> it(GetWorld()); it; ++it) {
-  ////     (*it)->SetActorHiddenInGame(true);
-  ////   }
-  ////   first_rendering = true;
-  //// }
-
-  //int dist_alpha = 255;
-  //float cutoff_dist = 3000.0f;
-
-  //if (!GetWorld()) return;
-  ////TODO: Move to sensor once it's able to be spawned
-  //TArray<UObject*> CosmosRelevantComponents;
-  //GetObjectsOfClass(UStaticMeshComponent::StaticClass(), CosmosRelevantComponents, false, EObjectFlags::RF_ClassDefaultObject, EInternalObjectFlags::AllFlags);
-
-  //for (UObject* Object : CosmosRelevantComponents) {
-  //  UStaticMeshComponent* mesh_component = Cast<UStaticMeshComponent>(Object);
-  //  if (mesh_component->GetOwner() == nullptr) continue;
-
-  //  FVector box_origin, box_extent;
-  //  UKismetSystemLibrary::GetActorBounds(mesh_component->GetOwner(), box_origin, box_extent);
-
-  //  const carla::rpc::CityObjectLabel Tag = ATagger::GetTagOfTaggedComponent(*mesh_component);
-
-  //  if (mesh_component->GetStaticMesh() == nullptr) continue;
-  //  FBoxSphereBounds bounds = mesh_component->GetStaticMesh()->GetBounds();
-
-  //  FColor vis_color;
-  //  switch (Tag) {
-  //  case carla::rpc::CityObjectLabel::TrafficLight:
-  //    vis_color = FColor(109, 144, 136);
-  //    break;
-  //  case carla::rpc::CityObjectLabel::TrafficSigns:
-  //    vis_color = FColor(214, 144, 137);
-  //    break;
-  //  case carla::rpc::CityObjectLabel::Poles:
-  //    vis_color = FColor(58, 4, 124);
-  //    break;
-  //  case carla::rpc::CityObjectLabel::Car:
-  //  case carla::rpc::CityObjectLabel::Bicycle:
-  //  case carla::rpc::CityObjectLabel::Bus:
-  //  case carla::rpc::CityObjectLabel::Motorcycle:
-  //    vis_color = FColor::Red;
-  //    break;
-  //  case carla::rpc::CityObjectLabel::Pedestrians:
-  //    vis_color = FColor::Green;
-  //    break;
-  //  }
-
-  //  if (Tag == carla::rpc::CityObjectLabel::TrafficLight || Tag == carla::rpc::CityObjectLabel::TrafficSigns)
-  //  {
-  //    //DrawDebugSolidBox(GetWorld(), box_origin, box_extent, mesh_component->GetComponentRotation().Quaternion(), FColor::Cyan);
-  //    DrawDebugBox(GetWorld(), mesh_component->GetComponentLocation(), bounds.BoxExtent, mesh_component->GetOwner()->GetActorRotation().Quaternion(), vis_color.WithAlpha(dist_alpha), false, -1, depth_prio, 10);
-  //    //DrawDebugSolidBox(GetWorld(), box_origin, bounds.BoxExtent, mesh_component->GetOwner()->GetActorRotation().Quaternion(), vis_color, false, -1.0f, 10U);
-  //    //DrawDebugSolidPlane(GetWorld(), FPlane(FVector(0.0f, 1.0f, 0.0f), 1.0f), box_origin, FVector2D(bounds.BoxExtent.X, bounds.BoxExtent.Z), vis_color);
-  //  }
-  //  else if (Tag == carla::rpc::CityObjectLabel::Car ||
-  //    Tag == carla::rpc::CityObjectLabel::Bicycle ||
-  //    Tag == carla::rpc::CityObjectLabel::Bus ||
-  //    Tag == carla::rpc::CityObjectLabel::Motorcycle ||
-  //    Tag == carla::rpc::CityObjectLabel::Pedestrians ||
-  //    Tag == carla::rpc::CityObjectLabel::Train ||
-  //    Tag == carla::rpc::CityObjectLabel::Truck)
-  //  {
-  //    DrawDebugBox(GetWorld(), box_origin, bounds.BoxExtent, mesh_component->GetOwner()->GetActorRotation().Quaternion(), vis_color.WithAlpha(dist_alpha), false, -1, depth_prio, 10);
-  //  }
-  //  else if (Tag == carla::rpc::CityObjectLabel::Poles)
-  //  {
-  //    //Filtering out horizontal poles
-  //    if (fmax(bounds.BoxExtent.X, bounds.BoxExtent.Y) > bounds.BoxExtent.Z) continue;
-
-  //    float half_height = bounds.BoxExtent.Z;
-  //    DrawDebugCapsule(GetWorld(), mesh_component->GetComponentLocation() + FVector(0.0f, 0.0f, half_height), half_height, 0.1f, FQuat::Identity, vis_color.WithAlpha(dist_alpha), false, -1, depth_prio, 20);
-  //  }
-  //}
-
-  ////Stop Lines
-  //TArray<AActor*> TrafficLights;
-  //UGameplayStatics::GetAllActorsOfClass(GetWorld(), ATrafficLightBase::StaticClass(), TrafficLights);
-
-  //for (AActor* traffic_light : TrafficLights)
-  //{
-  //  UBoxComponent* stop_box_collider = Cast<UBoxComponent>(traffic_light->GetComponentByClass(UBoxComponent::StaticClass()));
-  //  //DrawDebugCapsule(GetWorld(), stop_box_collider->GetComponentLocation() - FVector(0.0f, 0.0f, stop_box_collider->GetScaledBoxExtent().Z), stop_box_collider->GetScaledBoxExtent().X, 0.1f, stop_box_collider->GetRightVector()., FColor::Red, false, -1, 0, 20);
-  //  //DrawDebugLine(const UWorld * InWorld, FVector const& LineStart, FVector const& LineEnd, FColor const& Color, bool bPersistentLines = false, float LifeTime = -1.f, uint8 DepthPriority = 0, float Thickness = 0.f) {}
-  //  DrawDebugLine(GetWorld(), 
-  //    stop_box_collider->GetComponentLocation() - FVector(0.0f, 0.0f, stop_box_collider->GetScaledBoxExtent().Z) + -stop_box_collider->GetScaledBoxExtent().X * stop_box_collider->GetForwardVector() - 710.0f * stop_box_collider->GetRightVector(),
-  //    stop_box_collider->GetComponentLocation() - FVector(0.0f, 0.0f, stop_box_collider->GetScaledBoxExtent().Z) +  stop_box_collider->GetScaledBoxExtent().X * stop_box_collider->GetForwardVector() - 710.0f * stop_box_collider->GetRightVector(),
-  //    FColor::Red.WithAlpha(dist_alpha), false, -1, depth_prio, 20);
-  //}
-
-  ////RouteSplines
-  //TArray<AActor*> RouteSplines;
-  //UGameplayStatics::GetAllActorsOfClass(GetWorld(), ARoutePlanner::StaticClass(), RouteSplines);
-  //for(AActor* RouteSpline : RouteSplines)
-  //{
-  //  ARoutePlanner* route_planner = Cast<ARoutePlanner>(RouteSpline);
-  //  
-  //  //route_planner->DrawRoutes();
-  //  for (int i = 0, lenRoutes = route_planner->Routes.Num(); i < lenRoutes; ++i)
-  //  {
-  //    for (int j = 0, lenNumPoints = route_planner->Routes[i]->GetNumberOfSplinePoints() - 1; j < lenNumPoints; ++j)
-  //    {
-  //      const FVector p0 = route_planner->Routes[i]->GetLocationAtSplinePoint(j + 0, ESplineCoordinateSpace::World);
-  //      const FVector p1 = route_planner->Routes[i]->GetLocationAtSplinePoint(j + 1, ESplineCoordinateSpace::World);
-
-  //      static const float MinThickness = 3.f;
-  //      static const float MaxThickness = 15.f;
-
-  //      const float Dist = (float)j / (float)lenNumPoints;
-  //      const float OneMinusDist = 1.f - Dist;
-  //      //const float Thickness = OneMinusDist * MaxThickness + MinThickness;
-
-  //      if (!route_planner->bIsIntersection)
-  //      {
-  //        DrawDebugLine(GetWorld(), p0, p1, FColor(71, 134, 183).WithAlpha(dist_alpha), false, -1.f, depth_prio, 20.0f);
-  //      }
-  //    }
-  //  }
-  //}
-
-  ////Crosswalks
-  //std::vector<carla::geom::Location> crosswalks_points = Map->GetAllCrosswalkZones();
-  //carla::geom::Location first_in_loop = crosswalks_points[0];
-  //for (int i = 1; i < crosswalks_points.size(); ++i)
-  //{
-  //  DrawDebugLine(GetWorld(), crosswalks_points[i-1], crosswalks_points[i], FColor(202, 132, 58).WithAlpha(dist_alpha), false, -1.f, depth_prio, 20.0f);
-
-  //  if (crosswalks_points[i] == first_in_loop) 
-  //  {
-  //    ++i;
-  //    if(i < crosswalks_points.size()) first_in_loop = crosswalks_points[i];
-  //  }
-  //}
 }
 
 void ACarlaGameModeBase::EndPlay(const EEndPlayReason::Type EndPlayReason)
