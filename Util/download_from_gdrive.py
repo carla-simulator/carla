7e9887145fb2cde7c86d2b021d32542dd5601837
Marcel Pi <169088301+MarcelPiNacy-CVC@users.noreply.github.com>
2025-09-18 11:09:52 +0200

    Update all copyright notices.

--- Util/download_from_gdrive.py
+++ Util/download_from_gdrive.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright (c) 2024 Computer Vision Center (CVC) at the Universitat Autonoma de
+# Copyright (c) 2025 Computer Vision Center (CVC) at the Universitat Autonoma de
 # Barcelona (UAB).
 #
 # This work is licensed under the terms of the MIT license.
