32ddaa0000c519ac00670d5ca3eb7bfb3651ee32
Aperiss <perissanchezantonio@gmail.com>
2025-09-18 18:36:05 +0200

    Export Road Marks

---
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Cosmos/Exporter/RoadMarkingExporter.h
@@ -0,0 +1,40 @@
+// RoadMarkingExporter.h
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CosmosStaticExporter.h"
+#include "Serialization/JsonWriter.h"
+#include "Serialization/JsonSerializer.h"
+#include <carla/road/Map.h>
+#include "RoadMarkingExporter.generated.h"
+
+UCLASS()
+class CARLA_API URoadMarkingExporter : public UCosmosStaticExporter
+{
+	GENERATED_BODY()
+
+public:
+
+  UFUNCTION(BlueprintCallable, Category = "Cosmos Static Exporter")
+  static bool ExportCosmosRoadMarkings(UWorld* World, const FString& SessionId, const FString& OutFilePath, FString& OutError);
+
+private:
+
+  static bool ParseSessionIdParts(const FString& In, FString& OutUuid, FString& OutStart, FString& OutEnd);
+  static FString MakeStableLabelId(const FString& Uuid, const TArray<FVector>& Vertices);
+  static void AppendRoadMarkingLabel(
+      TArray<TSharedPtr<FJsonValue>>& LabelsArr,
+      const FString& Uuid,
+      const FString& StartTs,
+      const TArray<FVector>& PolygonMeters,
+      const FString& SensorName,
+      const FString& LabelClassNamespace,
+      const FString& LabelClassIdentifier,
+      const FString& LabelClassVersion,
+      const FString& CoordinateFrame,
+      const FString& LabelNameEnum,
+      const FString& OptionalAssetRefUri,
+      const FString& OptionalFeatureId,
+      const FString& OptionalFeatureVer,
+      const FString& OptionalClipVerId);
+};
