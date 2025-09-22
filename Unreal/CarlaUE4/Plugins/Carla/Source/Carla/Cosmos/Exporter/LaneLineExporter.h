3caaaffd81f98ee449a352f0786363a31e5cd40b
Aperiss <perissanchezantonio@gmail.com>
2025-09-22 16:29:03 +0200

    Fix LaneLine exporter

---
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Cosmos/Exporter/LaneLineExporter.h
@@ -0,0 +1,26 @@
+// LaneLineExporter.h
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CosmosStaticExporter.h"
+#include "Serialization/JsonWriter.h"
+#include "Serialization/JsonSerializer.h"
+#include "LaneLineExporter.generated.h"
+
+UCLASS()
+class CARLA_API ULaneLineExporter : public UCosmosStaticExporter
+{
+	GENERATED_BODY()
+
+public:
+
+  static bool ExportCosmosLaneLines(UWorld* World, const FString& SessionId, const FString& OutFilePath, FString& OutError);
+
+private:
+
+  static bool ParseSessionIdParts(const FString& In, FString& OutUuid, FString& OutStart, FString& OutEnd);
+  static void AppendLaneLineLabel(
+      TArray<TSharedPtr<FJsonValue>>& LabelsArr,
+      const FString& StartTs,
+      const TArray<FVector>& PolyMeters);
+};
