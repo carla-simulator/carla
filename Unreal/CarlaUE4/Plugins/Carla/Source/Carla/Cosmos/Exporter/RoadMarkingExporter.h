eb6723ea020852792b60881e639d273203b1dde2
Aperiss <perissanchezantonio@gmail.com>
2025-09-22 16:29:27 +0200

    Add RoadMarking exporter

---
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Cosmos/Exporter/RoadMarkingExporter.h
@@ -0,0 +1,26 @@
+// RoadMarkingExporter.h
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CosmosStaticExporter.h"
+#include "Serialization/JsonWriter.h"
+#include "Serialization/JsonSerializer.h"
+#include "RoadMarkingExporter.generated.h"
+
+UCLASS()
+class CARLA_API URoadMarkingExporter : public UCosmosStaticExporter
+{
+	GENERATED_BODY()
+
+public:
+
+  static bool ExportCosmosRoadMarkings(UWorld* World, const FString& SessionId, const FString& OutFilePath, FString& OutError);
+
+private:
+
+  static bool ParseSessionIdParts(const FString& In, FString& OutUuid, FString& OutStart, FString& OutEnd);
+  static void AppendRoadMarkingLabel(
+      TArray<TSharedPtr<FJsonValue>>& LabelsArr,
+      const FString& StartTs,
+      const TArray<FVector>& SurfaceVertices);
+};
