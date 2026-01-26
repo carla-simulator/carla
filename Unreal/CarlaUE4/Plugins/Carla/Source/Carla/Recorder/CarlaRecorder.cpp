3595011fba909a18dd018e92db028c23472f44ce
glopezdiest <glopez@cvc.uab.cat>
2026-01-26 10:05:41 +0100

    Added simulateneous record and replay

--- Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Recorder/CarlaRecorder.cpp
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/Recorder/CarlaRecorder.cpp
@@ -150,7 +150,7 @@ void ACarlaRecorder::Ticking(float DeltaSeconds)
     // write all data for this frame
     Write(DeltaSeconds);
   }
-  else if (Episode->GetReplayer()->IsEnabled())
+  if (Episode->GetReplayer()->IsEnabled())
   {
     // replayer
     Episode->GetReplayer()->Tick(DeltaSeconds);
@@ -437,9 +437,6 @@ std::string ACarlaRecorder::Start(
   FString MapName,
   bool AdditionalData)
 {
-  // stop replayer if any in course
-  if (Replayer.IsEnabled())
-    Replayer.Stop();
 
   // stop recording
   Stop();
